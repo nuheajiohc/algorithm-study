f4db6d8519ff353d22c6ba7f73782b63fc6a660b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-01-04 09:25:59 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,7 @@
+const year = require("fs").readFileSync("/dev/stdin").toString();
+
+if (((year % 4 == 0) && (year % 100 != 0)) || year % 400 == 0) {
+    console.log("1");
+} else {
+    console.log("0");
+}
