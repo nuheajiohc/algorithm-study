b3b1cdcbaaebfe5d7cf1a7453d542944d15d4d62
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-01-21 22:39:32 +0900

    [Bronze III] Title: 오븐 시계, Time: 136 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -1,12 +1,6 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-
-const current = input[0].split(" ");
-
-const h = parseInt(current[0]);
-const m = parseInt(current[1]);
-
-const sum_m = (h*60) + m + parseInt(input[1]);
-const new_h = parseInt((sum_m/60)%24);
-const new_m = sum_m%60;
-
-console.log(new_h,new_m);
+const [time, plus] = require("fs").readFileSync(0).toString().trim().split("\n");
+const [A, B] = time.split(" ").map(Number);
+const totalMinute = A * 60 + B + Number(plus);
+const H = Math.floor(totalMinute / 60) % 24;
+const M = totalMinute % 60;
+console.log(H, M);
