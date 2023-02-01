e34ed92450d0631d598b33ce8a60a83ecaa85cea
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-02-01 21:05:03 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.67 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [unrated] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086#) 
 
 ### 성능 요약
 
-메모리: 38.3 MB, 시간: 5.54 ms
+메모리: 37.8 MB, 시간: 4.67 ms
 
 ### 구분
 
--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,13 +1,27 @@
+// function solution(s) {
+//     var answer = [];
+//     const alphabet= new Array(26).fill(0);
+//     for(let i=0;i<s.length;i+=1){
+//         if(alphabet[s.charCodeAt(i)-97]===0){
+//             answer.push(-1);
+//         }else{
+//             answer.push(i-alphabet[s.charCodeAt(i)-97][1]);
+//         }
+//         alphabet[s.charCodeAt(i)-97]=[alphabet[s.charCodeAt(i)-97],i];
+//     }
+//     return answer;
+// }
+
 function solution(s) {
     var answer = [];
-    const alphabet= new Array(26).fill(0);
+    const alphabet= new Array(26);
     for(let i=0;i<s.length;i+=1){
-        if(alphabet[s.charCodeAt(i)-97]===0){
+        if(alphabet[s.charCodeAt(i)-97]===undefined){
             answer.push(-1);
         }else{
-            answer.push(i-alphabet[s.charCodeAt(i)-97][1]);
+            answer.push(i-alphabet[s.charCodeAt(i)-97]);
         }
-        alphabet[s.charCodeAt(i)-97]=[alphabet[s.charCodeAt(i)-97],i];
+        alphabet[s.charCodeAt(i)-97]=i;
     }
     return answer;
 }
