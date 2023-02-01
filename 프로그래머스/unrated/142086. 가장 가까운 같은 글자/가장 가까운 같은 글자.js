e3bd7adc24f3c0a0a4f228606698044faaebd816
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-02-01 21:03:46 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 5.54 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = [];
+    const alphabet= new Array(26).fill(0);
+    for(let i=0;i<s.length;i+=1){
+        if(alphabet[s.charCodeAt(i)-97]===0){
+            answer.push(-1);
+        }else{
+            answer.push(i-alphabet[s.charCodeAt(i)-97][1]);
+        }
+        alphabet[s.charCodeAt(i)-97]=[alphabet[s.charCodeAt(i)-97],i];
+    }
+    return answer;
+}
