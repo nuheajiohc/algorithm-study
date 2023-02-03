1441725531b564ca683930173a3366f56498f6f9
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-02-03 19:20:23 +0900

    [Silver IV] Title: 좋은 단어, Time: 256 ms, Memory: 20460 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.js
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.js
@@ -1,44 +1,12 @@
-const input = require("fs").readFileSync(0).toString().trim().split("\n");
+const input = require("fs").readFileSync(0).toString().split("\n");
 let result = 0;
 for (let i = 1; i <= Number(input[0]); i += 1) {
   const stack = [];
-  let [a, b] = [0, 0];
   input[i].split("").forEach(el => {
-    if (stack.length === 0) {
-      if (el === "A") {
-        a = 1;
-      } else {
-        b = 1;
-      }
-      stack.push(el);
+    if (stack.at(-1) === el) {
+      stack.pop();
     } else {
-      if (el === "A") {
-        if (a > 0) {
-          if (stack.at(-1) === "A") {
-            stack.pop();
-            a += 1;
-          } else {
-            stack.push(el);
-            a += 1;
-          }
-        } else {
-          stack.push(el);
-          a += 1;
-        }
-      } else {
-        if (b > 0) {
-          if (stack.at(-1) === "B") {
-            stack.pop();
-            b += 1;
-          } else {
-            stack.push(el);
-            b += 1;
-          }
-        } else {
-          stack.push(el);
-          b = 1;
-        }
-      }
+      stack.push(el);
     }
   });
   if (stack.length === 0) {
