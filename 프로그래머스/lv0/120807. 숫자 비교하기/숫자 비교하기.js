b40eed3ee5a4dc0869535e1e5779dba61f39a59b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-02-08 16:55:04 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,18 +1,4 @@
 function solution(num1, num2) {
-    const answer = compareNum(num1,num2);
+    var answer = num1===num2?1:-1;
     return answer;
 }
-
-function compareNum(num1,num2){
-    if(num1!==num2){
-        return -1;
-    }else{
-    return 1;
-    }
-}
-
-
-// function solution(num1,num2){
-//     const answer = ((num1===num2)-0.5)*2;
-//     return answer;
-// }
