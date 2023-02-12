3628a12fc17ffa200c405f45cd03c568c85aa041
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-02-12 18:14:26 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,8 @@
+function solution(age) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
+    const answer = age.toString().split("").map(a=>alphabet[a]).join("");
+    return answer;
+}
