53e58c48079cfdbfd7cb835d7c8a8bb45a9402f7
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-03-01 10:57:20 +0900

    [Silver V] Title: 배열 합치기, Time: 1896 ms, Memory: 428848 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.js
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -1,4 +1,30 @@
 let [_, first, second] = require("fs").readFileSync(0).toString().trim().split("\n");
+const result = [];
 first = first.split(" ").map(Number);
 second = second.split(" ").map(Number);
-console.log([...first, ...second].sort((a, b) => a - b).join(" "));
+let i = 0;
+let j = 0;
+for (let order = 0; order < first.length + second.length; order += 1) {
+  if (
+    first[i] !== undefined &&
+    second[j] != undefined &&
+    first[i] <= second[j]
+  ) {
+    result.push(first[i]);
+    i += 1;
+  } else if (
+    first[i] !== undefined &&
+    second[j] != undefined &&
+    first[i] > second[j]
+  ) {
+    result.push(second[j]);
+    j += 1;
+  } else if (first[i] === undefined && second[j] != undefined) {
+    result.push(second[j]);
+    j += 1;
+  } else {
+    result.push(first[i]);
+    i += 1;
+  }
+}
+console.log(result.join(" "));
