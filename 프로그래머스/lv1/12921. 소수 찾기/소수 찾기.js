3e13914846a5b2d25bca51b7566d642f6b356e80
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-03-26 12:24:19 +0900

    [level 1] Title: 소수 찾기, Time: 54.10 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,7 +1,8 @@
 function solution(n) {
-    const arr = new Array(n+1).fill(1).fill(0,0,2);
+    const arr = new Array(n+1).fill(-1).fill(0,2,0);
     for(let i=2;i<=n;i+=1){
-        if(arr[i]===0) continue;
+        if(arr[i]!==-1) continue;
+        arr[i]=1;
         for(let j=i+i;j<=n;j+=i){
             arr[j]=0;
         }
