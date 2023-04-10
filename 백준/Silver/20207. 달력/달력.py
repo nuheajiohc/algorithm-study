1f28c39f29d0bc445a986f4ed93ffe1ee3ac1343
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-04-10 14:32:42 +0900

    [Silver I] Title: 달력, Time: 120 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/20207. 달력/달력.py
+++ 백준/Silver/20207. 달력/달력.py
@@ -1,34 +1,25 @@
-N = int(input())
-schedule = []
-calender=[[0]*367 for _ in range(N)]
-size = [0]*367
+N= int(input())
+calender = [0]*367
+
 for i in range(N):
-    schedule.append(list(map(int,input().split())))
+    st,en=map(int,input().split())
+    for j in range(st,en+1):
+        calender[j]+=1
 
-schedule.sort(key=lambda x:(x[0],-x[1]))
-
-for st,en in schedule:
-    i=0
-    while True:
-        if calender[i][st:en+1].count(0)==en-st+1:
-            calender[i][st:en+1] = [1]*(en-st+1)
-            for j in range(st,en+1):
-                size[j] = max(size[j],i+1)
-            break
-        i+=1
-result = 0
+result=0
 temp=0
 width=0
 height=0
-for i in size:
+
+for i in calender:
     if i==0:
-        temp = width*height
+        temp=width*height
         result+=temp
+        temp=0
         width=0
         height=0
-        temp=0
-        continue
     else:
         width+=1
         height=max(height,i)
+
 print(result)
