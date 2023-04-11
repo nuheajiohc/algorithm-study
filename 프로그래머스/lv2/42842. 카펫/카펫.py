28111e4411a2a1b9733466e3c7e4950e16d30ac8
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-04-11 20:54:06 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -2,25 +2,19 @@ import math
 
 def solution(brown, yellow):
     answer = []
-    s=brown+yellow
-    for i in range(int(math.sqrt(s)),s+1):
-        if s%i==0:
-            width=i
-            height=s//i
-            if width<height:
-                continue
-            if (2*width)+2*(height-2)==brown and int(yellow/(width-2))==(yellow/(width-2)):
-                answer.append(width)
-                answer.append(height)
-                break
+    area=brown+yellow
+    height=0
+    width=0
+    for i in range(math.ceil(math.sqrt(area)),area):
+        width=i
+        height=area//width
+        if brown==(width*2)+(height-2)*2 and yellow/(height-2) ==yellow//(height-2):
+            break
+    answer.append(width)
+    answer.append(height)
     return answer
 
 
-# 전체 크기는 brown+yellow = 가로 x 세로 
+# brown = 가로*2 + (세로-2)*2
+# yellow = (세로-2)* x
 # 가로>=세로
-# a+a+(2+b)*(세로-2)
-
-# 2(가로)+2(세로-2)= brown
-# b(세로-2) =yellow
-
-# brown+yellow를 구하고 가로가 세로보다 크거나 같은 약수들을 구하면서 위조건에 맞는 값을 구한다.
