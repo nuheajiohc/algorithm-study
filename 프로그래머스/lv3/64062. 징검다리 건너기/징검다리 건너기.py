ec8c6a85a85a861115380c564f4e33f9bb58f3e8
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-04-13 23:28:13 +0900

    [level 3] Title: 징검다리 건너기, Time: 65.93 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,38 +1,20 @@
-# def solution(stones, k):
-#     answer = 0
-#     while True:
-#         jump = 0
-#         flag=True
-#         for i in range(len(stones)):
-#             if stones[i] > 0 and jump<k:
-#                 stones[i]-=1
-#                 jump=0
-#             elif stones[i]==0 and jump>=k:
-#                 flag=False
-#                 break
-#             else :
-#                 jump+=1
-#         if flag==True:
-#             answer+=1
-#         else:
-#             break
-#     return answer
-
-def solution (stones,k):
-    st = min(stones)
-    en = max(stones)
-    
-    while st<=en:
-        mid = (st+en)//2
+def solution(stones, k):
+    max_stone = max(stones)
+    min_stone = min(stones)
+    while min_stone<=max_stone:
+        mid =(min_stone+max_stone)//2
         cnt=0
-        for stone in stones:
-            if stone <= mid:
+        for i in stones:
+            if mid>=i:
                 cnt+=1
             else:
                 cnt=0
             if cnt>=k:
-                en =mid-1
+                max_stone=mid-1
                 break
         else:
-            st=mid+1
-    return st
+            min_stone=mid+1
+    return min_stone
+
+
+
