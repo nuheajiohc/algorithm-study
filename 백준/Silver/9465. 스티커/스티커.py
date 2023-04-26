6646c97082d98a4fe8e62e07da478145a3859728
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-04-26 21:12:55 +0900

    [Silver I] Title: 스티커, Time: 756 ms, Memory: 47880 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,15 +1,11 @@
-T =int(input())
+T = int(input())
 for _ in range(T):
     n = int(input())
-    sticker = []
-    sticker.append(list(map(int,input().split())))
-    sticker.append(list(map(int,input().split())))
-
-    for i in range(1,n):
-        if i==1:
-            sticker[0][1]+=sticker[1][0]
-            sticker[1][1]+=sticker[0][0]
-            continue
-        sticker[0][i]+=max(sticker[1][i-1],sticker[1][i-2])
-        sticker[1][i]+=max(sticker[0][i-1],sticker[0][i-2])
-    print(max(sticker[0][n-1],sticker[1][n-1]))
+    stk=[list(map(int,input().split())) for __ in range(2)]
+    if n>1:
+        stk[0][1]+=stk[1][0]
+        stk[1][1]+=stk[0][0]
+    for i in range(2,n):
+        stk[1][i]+= max(stk[0][i-2],stk[0][i-1])
+        stk[0][i]+= max(stk[1][i-2],stk[1][i-1])
+    print(max(stk[0][n-1],stk[1][n-1]))
