22a262e7663055fef5a25c654bc8b99dcd4d21bd
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-05-03 14:50:48 +0900

    [Silver II] Title: 사탕 게임, Time: 4720 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,43 +1,43 @@
-def check(data):
-    max_cnt = 1
-    for i in range(N):
-        cnt = 1
-        for j in range(1, N):
-            if data[i][j] == data[i][j-1]:
-                cnt += 1
-            else:
-                cnt = 1
-            max_cnt = max(max_cnt, cnt)
-
-        cnt = 1
-        for j in range(1, N):
-            if data[j][i] == data[j-1][i]:
-                cnt += 1
-            else:
-                cnt = 1
-            max_cnt = max(max_cnt, cnt)
-    
-    return max_cnt
-
 N = int(input())
-data = [list(input()) for _ in range(N)]
-ans = 0
+board = [list(input()) for _ in range(N)]
+
+dx=[0,1]
+dy=[1,0]
+max_candy=0
+
+def check(board):
+    temp=0
+    for i in range(N):
+        st=0
+        en=0
+        while en<N:
+          if board[i][st]==board[i][en]:
+              en+=1
+          else:
+              temp=max(temp,en-st)
+              st=en
+        temp=max(temp,en-st)
+    for i in range(N):
+        st=0
+        en=0
+        while en<N:
+          if board[st][i]==board[en][i]:
+              en+=1
+          else:
+              temp=max(temp,en-st)
+              st=en
+        temp=max(temp,en-st)
+    return temp
 
 for i in range(N):
     for j in range(N):
-        if j + 1 < N: # 열 범위 체크
-            # 인접한 것끼리 바꿔주기
-            data[i][j], data[i][j+1] = data[i][j+1], data[i][j]
-            cnt = check(data)
-            ans = max(ans, cnt)
-            # 바꾼 것 원래대로 돌려놓기
-            data[i][j], data[i][j+1] = data[i][j+1], data[i][j]
+        for dir in range(2):
+            nx=i+dx[dir]
+            ny=j+dy[dir]
+            if nx>=N or ny>=N:
+                continue
+            board[i][j],board[nx][ny]=board[nx][ny],board[i][j]
+            max_candy=max(max_candy,check(board))
+            board[i][j],board[nx][ny]=board[nx][ny],board[i][j]
 
-        if i + 1 < N: # 행 범위 체크
-            data[i][j], data[i+1][j] = data[i+1][j], data[i][j]
-            cnt = check(data)
-            ans = max(ans, cnt)
-            # 바꾼 것 원래대로 돌려놓기
-            data[i][j], data[i+1][j] = data[i+1][j], data[i][j]
-
-print(ans)
+print(max_candy)
