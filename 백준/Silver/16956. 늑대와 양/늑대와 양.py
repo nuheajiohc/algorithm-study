50f8a808f491521abfbf6fd0ed62b077d87ca329
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-05-09 10:43:21 +0900

    [Silver III] Title: 늑대와 양, Time: 232 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Silver/16956. 늑대와 양/늑대와 양.py
+++ 백준/Silver/16956. 늑대와 양/늑대와 양.py
@@ -1,27 +1,24 @@
-from collections import deque
 import sys
 
 R,C = map(int,input().split())
 pasture = [list(input()) for _ in range(R)]
-queue =deque()
+
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
+
 for i in range(R):
     for j in range(C):
-        if pasture[i][j]==".":
-            pasture[i][j]="D"
         if pasture[i][j]=="S":
-              queue.append((i,j))
-while queue:
-    x,y=queue.popleft()
-    for dir in range(4):
-        nx=x+dx[dir]
-        ny=y+dy[dir]
-        if nx<0 or ny<0 or nx>=R or ny>=C:
-            continue
-        if pasture[nx][ny]=="W":
-            print(0)
-            sys.exit()
+            for dir in range(4):
+                nx=i+dx[dir]
+                ny=j+dy[dir]
+                if nx<0 or ny<0 or nx>=R or ny>=C:
+                    continue
+                if pasture[nx][ny]=="W":
+                    print(0)
+                    sys.exit()
+                if pasture[nx][ny]==".":
+                    pasture[nx][ny]="D"
 else:
     print(1)
     for i in range(R):
