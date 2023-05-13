0cf8de4293f38b01d13caef255a6b80b1ca9376b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-05-13 22:20:31 +0900

    [Gold IV] Title: 샘터, Time: 204 ms, Memory: 58464 KB -BaekjoonHub

--- 백준/Gold/18513. 샘터/샘터.py
+++ 백준/Gold/18513. 샘터/샘터.py
@@ -1,28 +1,27 @@
 from collections import deque
 
 n,k = map(int,input().split())
-water=list(map(lambda x:int(x)+150000000,input().split()))
-vis=[0]*300000000
-water_set=set()
-for v in water:
-    water_set.add(v)
-ans=0
-cnt=k
-def bfs(cnt,ans):
-    queue = deque(water)
-    while queue:
-        x=queue.popleft()
-        for dir in [1,-1]:
-            nx=x+dir
-            if nx in water_set:
-                continue
-            if vis[nx]:
-                continue
-            vis[nx]=vis[x]+1
-            ans+=vis[nx]
-            cnt-=1
-            queue.append(nx)
-            if cnt==0:
-                return ans
+n_list = list(map(int,input().split()))
+visit=set()
+queue=deque()
+for i in n_list:
+    queue.append((i,0))
+    visit.add(i)
 
+ans = 0
+cnt = k
+def bfs(cnt,ans):
+    while queue:
+        x,d=queue.popleft()
+        for dir in [-1,1]:
+            nx=x+dir
+            if nx in visit:
+                continue
+            queue.append((nx,d+1))
+            visit.add(nx)
+            ans+=d+1
+            cnt-=1
+            if not cnt:
+                return ans
+        
 print(bfs(cnt,ans))
