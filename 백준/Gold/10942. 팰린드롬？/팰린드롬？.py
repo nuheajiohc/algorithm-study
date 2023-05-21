50368fdf7da50261b06a94f51c895622c9467212
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-05-21 10:48:20 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2876 ms, Memory: 250444 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -6,9 +6,7 @@ m = int(input())
 m_list = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(m)]
 d=[[0]*(n+1) for _ in range(n+1)]
 
-for i in range(1,n+1):
-    st=i
-    en=i
+def check(st,en):
     while st>0 and en<=n:
         if n_list[st-1]==n_list[en-1]:
             d[st][en]=1
@@ -16,15 +14,14 @@ for i in range(1,n+1):
             break
         st-=1
         en+=1
-    if i<n:
-        st=i
-        en=i+1
-        while st>0 and en<=n:
-            if n_list[st-1]==n_list[en-1]:
-                d[st][en]=1
-            else:
-                break
-            st-=1
-            en+=1
+
+for i in range(1,n+1):
+    st=i
+    en=i
+    check(st,en)
+    st=i
+    en=i+1
+    check(st,en)
+
 for (s,e) in m_list:
     print(d[s][e])
