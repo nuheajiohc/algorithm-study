fd7d54426fd2d86e0ae8705a45373bef6a179baa
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-05-30 22:29:48 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,15 +1,18 @@
-import sys
 a,b = map(int,input().split())
-
-def change(a,b,n):
-    if a==b:
-        print(n)
-        sys.exit()
-    if a>b:
-        return
-    change(a*2,b,n+1)
-    temp=int(str(a)+"1")
-    change(temp,b,n+1) 
-
-change(a,b,1)
-print(-1)
+cnt=1
+temp=b
+while a<=temp:
+    if a==temp:
+        print(cnt)
+        break
+    if temp%2==0:
+        temp//=2
+    else:
+        if str(temp)[-1]=="1":
+            temp=int(str(temp)[:-1])
+        else:
+            print(-1)
+            break
+    cnt+=1
+else:
+    print(-1)
