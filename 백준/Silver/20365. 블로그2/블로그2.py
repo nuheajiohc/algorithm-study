78b1551641a8d6387c376e3a520319f61917eed4
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-05-30 22:06:29 +0900

    [Silver III] Title: 블로그2, Time: 48 ms, Memory: 32236 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -1,17 +1,4 @@
-n =int(input())
-problem = input()
-st=0
-en=len(problem)-1
-cnt=1
-temp_color=problem[0]
-
-while st<=en:
-    if problem[st]==temp_color:
-        st+=1
-    else:
-        if problem[en]!=temp_color:
-            cnt+=1
-            temp_color=problem[st]
-            st+=1
-        en-=1
-print(cnt)
+input()
+problem=input()
+check= "BR" if problem[0]=="B" else "RB"
+print(problem.count(check)+1)
