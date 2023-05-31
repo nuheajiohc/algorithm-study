5e2262d4d7c769b18d3e9be577ca73fd3097074f
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-05-31 09:30:51 +0900

    [Silver II] Title: 민겸 수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.py
+++ 백준/Silver/21314. 민겸 수/민겸 수.py
@@ -1,21 +1,18 @@
 s=input()
-cnt=0
 min_val=""
 max_val=""
-m_cnt=0
+cnt=0
 for i in range(len(s)):
-    if s[i]=="M":
-        m_cnt+=1
-    else:
-        if m_cnt:
-            max_val+="5"+"0"*(m_cnt)
-            min_val+="1"+"0"*(m_cnt-1)+"5"
+    cnt+=1
+    if s[i]=="K":
+        max_val+="5"+"0"*(cnt-1)
+        if cnt==1:
+          min_val+="5"
         else:
-            max_val+="5"
-            min_val+="5"
-        m_cnt=0
-if m_cnt:
-    max_val+="1"*m_cnt
-    min_val+=str(int(10**(m_cnt-1)))
-print(max_val)
-print(min_val)
+          min_val+=str(10**(cnt-2))+"5"
+        cnt=0
+
+if cnt:
+   max_val+="1"*cnt
+   min_val+="1"+"0"*(cnt-1)
+print(max_val,min_val,sep="\n")
