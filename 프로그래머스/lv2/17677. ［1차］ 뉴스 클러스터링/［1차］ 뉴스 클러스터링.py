e86234cd02748fcb3e5564831fb9a0b7083f36b1
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-08 09:34:20 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,42 +1,44 @@
 def solution(str1, str2):
-    answer=0
-    lowerstr1=str1.lower()
-    lowerstr2=str2.lower()
-    list1 = []
-    list2 = []
-    for i in range(len(lowerstr1)-1):
-        if lowerstr1[i:i+2].isalpha():
-            list1.append(lowerstr1[i:i+2])
-
-    for i in range(len(lowerstr2)-1):
-        if lowerstr2[i:i+2].isalpha():
-            list2.append(lowerstr2[i:i+2])
-            
-    list1copy1= list1.copy()
-    list1copy2 = list1.copy()
-    union =list1.copy()
-    intersection = []
-    for i in list2:
-        if i in list1copy1:
-            list1copy1.remove(i)
+    str1=str1.lower()
+    str2=str2.lower()
+    possible_str = set()
+    str1_dict = {}
+    str2_dict = {}
+    intersection=0
+    union=0
+    for i in range(len(str1)-1):
+        if not str1[i:i+2].isalpha():
+            continue
+        possible_str.add(str1[i:i+2])
+        if str1[i:i+2] not in str1_dict:
+            str1_dict[str1[i:i+2]]=0
+        str1_dict[str1[i:i+2]]+=1
+        
+    for i in range((len(str2)-1)):
+        if not str2[i:i+2].isalpha():
+            continue
+        possible_str.add(str2[i:i+2])
+        if str2[i:i+2] not in str2_dict:
+            str2_dict[str2[i:i+2]]=0
+        str2_dict[str2[i:i+2]]+=1
+    
+    possible_str = list(possible_str)
+    
+    for s in possible_str:
+        if s in str1_dict and s in str2_dict:
+            if str1_dict[s]<=str2_dict[s]: 
+                union+=str2_dict[s]
+                intersection+=str1_dict[s]
+            else:
+                union+=str1_dict[s]
+                intersection+=str2_dict[s]
+        elif s in str1_dict:
+            union+=str1_dict[s]
         else:
-            union.append(i)
-    unionlen =len(union)
-    for i in list2:
-        if i in list1copy2:
-            list1copy2.remove(i)
-            intersection.append(i)
-    intersectionlen = len(intersection)
-    if unionlen ==0:
+            union+=str2_dict[s]
+    
+    if union==0:
         answer=65536
     else:
-        answer = intersectionlen*65536/unionlen
-    answer//=1
+        answer = (intersection/union)*65536//1
     return answer
-
-# 합집합
-# list2돌면서 해당 요소가 list1 카피본에 있으면 list1 카피본에서 제거
-# list2돌면서 해당 요소가 list1 카피본에 없으면 union에 추가
-
-# 교집합
-# list2돌면서 해당 요소가 list1 카피본에 있으면 list1카피본에서 제거 intersection에서 추가
