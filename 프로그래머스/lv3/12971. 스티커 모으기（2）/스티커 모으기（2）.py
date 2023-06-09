7021edfa66eb5d17723531f4b912d595329c3889
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-09 20:43:12 +0900

    [level 3] Title: 스티커 모으기(2), Time: 70.87 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,46 +1,38 @@
 # def solution(sticker):
-#     d1=[[sticker[0],sticker[0]]]*3
-#     if len(sticker)>3:
-#         d1.append([sticker[0],sticker[0]+sticker[3]])
-#     for i in range(4,len(sticker)):
-#         d1.append([d1[i-1][1],d1[i-1][0]+sticker[i-1]])
+#     if len(sticker)==1:
+#         return sticker[0]
     
-#     d2=[[0,0]]
+#     d1=[sticker[0]]
 #     if len(sticker)>1:
-#         d2.append([0,sticker[1]])
+#         d1.append(sticker[0])
 #     if len(sticker)>2:
-#         d2.append([sticker[1],sticker[2]])
-#     for i in range(3,len(sticker)):
-#         d2.append([d2[i-1][1],max(d2[i-2])+sticker[i]])
-#     answer = max(*d1[-1],*d2[-1])    
+#         d1.append(sticker[0])
+#     if len(sticker)>3:
+#         d1.append(sticker[0]+sticker[2])
+#     if len(sticker)>4:
+#         d1.append(sticker[0]+sticker[3])
+#     for i in range(5,len(sticker)):
+#         d1.append(max(d1[i-2],d1[i-3])+sticker[i-1])
+    
+#     d2=[0]
+#     if len(sticker)>1:
+#         d2.append(sticker[1])
+#     if len(sticker)>2:
+#         d2.append(sticker[2])
+#     if len(sticker)>3:
+#         d2.append(sticker[1]+sticker[3])
+#     for i in range(4,len(sticker)):
+#         d2.append(max(d2[i-2],d2[i-3])+sticker[i])
 
+#     answer= max(d1[-1],d1[-2],d2[-1],d2[-2])
 #     return answer
 
 def solution(sticker):
-    if len(sticker)==1:
-        return sticker[0]
-    
-    d1=[sticker[0]]
-    if len(sticker)>1:
-        d1.append(sticker[0])
+    d1=[sticker[0],sticker[0],sticker[0]]
+    d2=[0,sum(sticker[:2])-sticker[0]]
     if len(sticker)>2:
-        d1.append(sticker[0])
-    if len(sticker)>3:
-        d1.append(sticker[0]+sticker[2])
-    if len(sticker)>4:
-        d1.append(sticker[0]+sticker[3])
-    for i in range(5,len(sticker)):
-        d1.append(max(d1[i-2],d1[i-3])+sticker[i-1])
-    
-    d2=[0]
-    if len(sticker)>1:
-        d2.append(sticker[1])
-    if len(sticker)>2:
-        d2.append(sticker[2])
-    if len(sticker)>3:
-        d2.append(sticker[1]+sticker[3])
-    for i in range(4,len(sticker)):
-        d2.append(max(d2[i-2],d2[i-3])+sticker[i])
-
-    answer= max(d1[-1],d1[-2],d2[-1],d2[-2])
-    return answer
+        d2.append(max(sticker[1:3]))
+    for i in range(3,len(sticker)):
+        d1.append(max(d1[i-1],d1[i-2]+sticker[i-1]))
+        d2.append(max(d2[i-1],d2[i-2]+sticker[i]))
+    return max(d1[-1],d2[-1])
