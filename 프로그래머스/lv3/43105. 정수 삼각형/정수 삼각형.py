e00053f957573a93e569d4bdaa173a779da7cd78
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-09 21:58:56 +0900

    [level 3] Title: 정수 삼각형, Time: 37.68 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -5,5 +5,5 @@ def solution(triangle):
         for j in range(1,len(triangle[i])-1):
             triangle[i][j]+=max(triangle[i-1][j-1:j+1])
         triangle[i][-1]+=triangle[i-1][-1]
-        answer=max(answer,*triangle[i])
+    answer=max(triangle[-1])
     return answer
