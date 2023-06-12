f3b0569542af6c3f4c3cddc96496eebaec7c6aa0
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-12 20:52:40 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 626.95 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/lv2/86052. 빛의 경로 사이클/빛의 경로 사이클.py
+++ 프로그래머스/lv2/86052. 빛의 경로 사이클/빛의 경로 사이클.py
@@ -1,26 +1,24 @@
 def solution(grid):
     answer = []
-    r,c=len(grid),len(grid[0])
-    board=[[[] for _ in range(c)] for _ in range(r)]
-    direction ={"L":[0,-1],"R":[0,1],"D":[1,0],"U":[-1,0]}
-    
+    r,c= len(grid),len(grid[0])
+    board=[[[0,0,0,0] for _ in range(c)] for _ in range(r)]
+    dx=[1,0,-1,0]#하우상좌
+    dy=[0,1,0,-1]
     for x in range(r):
         for y in range(c):
-            for d in 'ULDR':
+            for dir in range(4):
                 cnt=0
-                while d not in board[x][y]:
-                    board[x][y].append(d)
+                while not board[x][y][dir]:
+                    board[x][y][dir]=1
                     cnt+=1
+                    x=(x+dx[dir])%r
+                    y=(y+dy[dir])%c
                     
-                    x = (x+direction[d][0]) % r
-                    y = (y+direction[d][1]) % c
-                    
-                    if grid[x][y] =="L":
-                        idx = "ULDR".index(d)
-                        d = "ULDR"[(idx+1)%4]
-                    elif grid[x][y] =="R":
-                        idx = "RDLU".index(d)
-                        d="RDLU"[(idx+1)%4]
+                    if grid[x][y]=="L":
+                        dir=(dir-1)%4
+                    elif grid[x][y]=="R":
+                        dir=(dir+1)%4
                 if cnt:
                     answer.append(cnt)
+                    
     return sorted(answer)
