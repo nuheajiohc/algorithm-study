f2c44e2d27eba8f40709a330fc6aa9e6d1c9151b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-14 14:01:46 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -3,37 +3,33 @@ from collections import deque
 dx=[0,0,1,-1]
 dy=[1,-1,0,0]
 
-
 def bfs(case):
+    vis=[[-1]*5 for _ in range(5)]
     for i in range(5):
         for j in range(5):
             if case[i][j]=="P":
-                q=deque()
-                q.append((i,j))
-                vis=[[-1]*5 for _ in range(5)]
+                q = deque([(i,j)])
                 vis[i][j]=0
                 while q:
                     x,y=q.popleft()
                     for dir in range(4):
-                        nx= x+dx[dir]
-                        ny= y+dy[dir]
+                        nx=x+dx[dir]
+                        ny=y+dy[dir]
                         if nx<0 or ny<0 or nx>=5 or ny>=5:
                             continue
                         if vis[nx][ny]==0:
                             continue
-                        if case[nx][ny]=="P":
-                            return 0
                         if case[nx][ny]=="X":
                             continue
+                        if case[nx][ny]=="P":
+                            return 0
                         vis[nx][ny]=vis[x][y]+1
                         if vis[nx][ny]==2:
                             continue
                         q.append((nx,ny))
     return 1
-
 def solution(places):
     answer = []
     for place in places:
         answer.append(bfs(place))
     return answer
-
