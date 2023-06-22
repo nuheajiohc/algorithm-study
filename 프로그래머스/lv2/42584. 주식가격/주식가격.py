0217b92f0fd3cfec3b8e0dba5421f0f336a9df41
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-22 21:18:07 +0900

    [level 2] Title: 주식가격, Time: 19.14 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,10 +1,24 @@
+# def solution(prices):
+#     answer = []
+#     for i in range(len(prices)):
+#         temp=0
+#         for j in range(i+1, len(prices)):
+#             temp+=1
+#             if prices[i]>prices[j]:
+#                 break
+#         answer.append(temp)
+#     return answer
+
 def solution(prices):
-    answer = []
-    for i in range(len(prices)):
-        temp=0
-        for j in range(i+1, len(prices)):
-            temp+=1
-            if prices[i]>prices[j]:
-                break
-        answer.append(temp)
-    return answer
+    length = len(prices)
+    # 모든 가격 max값으로 세팅
+    result = [ i for i in range (length - 1, -1, -1)]
+
+    # 주식 가격이 떨어지는 경우를 찾아 수정
+    stack = []
+    for i in range (length):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            result[j] = i - j
+        stack.append(i)
+    return result
