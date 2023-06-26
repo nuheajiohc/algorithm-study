338747bedff40d92d38b1ffc1ee5e4e20a52d755
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-26 20:45:40 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.py
+++ 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 def solution(lines):
     answer = 1
     times = []
@@ -15,21 +13,22 @@ def solution(lines):
         st_time = en_time+1-(duration_time*1000)
         
         times.append([st_time,en_time])
- 
-    # for i in range(len(times)):
-    #     temp=1
-    #     en = times[i][1]
-    #     en_plus_one  = en+1000
-    #     j=i+1
-    #     while j<len(times) and en_plus_one>times[j][0]:
-    #         temp+=1
-    #         j+=1
-    #     answer=max(answer,temp)
+    
+    times.sort(key=lambda x:(x[1],x[0]))
     for i in range(len(times)):
-        en = times[i][1]
         temp=0
-        for j in range(i,len(times)):
-            if en+1000 > times[j][0]:
+        en = times[i][1]
+        j=i
+        while j<len(times):
+            if en+1000>times[j][0]:
                 temp+=1
-        answer = max(temp,answer)
+            j+=1
+        answer=max(answer,temp)
+    # for i in range(len(times)):
+    #     en = times[i][1]
+    #     temp=0
+    #     for j in range(i,len(times)):
+    #         if en+1000 > times[j][0]:
+    #             temp+=1
+    #     answer = max(temp,answer)
     return answer
