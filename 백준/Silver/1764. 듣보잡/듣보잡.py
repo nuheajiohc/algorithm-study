5afa0e6ce1c2e4444d2f0d68739debe0c5aa13bb
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-27 22:52:12 +0900

    [Silver IV] Title: 듣보잡, Time: 3728 ms, Memory: 44060 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,6 +1,6 @@
 n,m = map(int, input().split())
-no_hear = set([input() for i in range(n)])
-no_see = set([input() for i in range(m)])
+no_hear = {input() for i in range(n)}
+no_see = {input() for i in range(m)}
 
 intersection = sorted(no_hear & no_see)
 print(len(intersection))
