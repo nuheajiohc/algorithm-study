f2701e058e6f6f252f3608fdd73a2b461ed7b724
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-28 15:11:52 +0900

    [Silver IV] Title: 듣보잡, Time: 84 ms, Memory: 44060 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,7 +1,10 @@
+import sys
+input = sys.stdin.readline
 n,m = map(int, input().split())
 no_hear = {input() for i in range(n)}
 no_see = {input() for i in range(m)}
 
 intersection = sorted(no_hear & no_see)
+
 print(len(intersection))
-print("\n".join(intersection))
+print("".join(intersection))
