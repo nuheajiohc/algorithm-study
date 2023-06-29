d9a7547c737991044a902be2b318ef2f09b0a377
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-29 16:01:42 +0900

    [Silver IV] Title: ZOAC 3, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/20436. ZOAC 3/ZOAC 3.py
@@ -0,0 +1,31 @@
+left,right = input().split()
+s = input()
+keys_dict = {}
+keys = ["qwertyuiop","asdfghjkl","zxcvbnm"]
+for i in range(len(keys)):
+    for j in range(len(keys[i])):
+        keys_dict[keys[i][j]]=(i,j)
+
+cons=set(["q","w","e","r","t","a","s","d","f","g","z","x","c","v"])
+
+x1,y1 = keys_dict[left]
+x2,y2 = keys_dict[right]
+
+cnt=0
+for v in s:
+    if v in cons:
+        next_x,next_y=keys_dict[v]
+        if (next_x,next_y)==(x1,y1):
+            cnt+=1
+        else:
+            cnt+=abs(next_x-x1)+abs(next_y-y1)+1
+            x1,y1= next_x,next_y
+    else:
+        next_x,next_y=keys_dict[v]
+        if (next_x,next_y)==(x2,y2):
+            cnt+=1
+        else:
+            cnt+=abs(next_x-x2)+abs(next_y-y2)+1
+            x2,y2= next_x,next_y
+
+print(cnt)
