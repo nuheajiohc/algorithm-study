3cd6ef2e5fe0ad19b90cf99bb5cd6d836c7fd204
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-29 21:48:17 +0900

    [Silver I] Title: 후보 추천하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -1,17 +1,14 @@
 n = int(input())
 m = int(input())
 nums = list(map(int, input().split()))
-check=[]
 check_dict={}
 for i,num in enumerate(nums):
     if num in check_dict:
         check_dict[num][0]+=1
     else:
-        if len(check)==n:
+        if len(check_dict)==n:
             min_v=sorted(check_dict.items(),key=lambda x:(x[1][0],x[1][1]))[0]
-            check.remove(min_v[0])
             del check_dict[min_v[0]]
-        check.append(num)
         check_dict[num]=[1,i]
 
 result=sorted(check_dict.items(),key=lambda x:x[0])
