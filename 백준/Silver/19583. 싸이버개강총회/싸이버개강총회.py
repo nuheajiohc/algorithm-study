e2987e7007d597b0aec4c2570db95722020ac72a
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-29 15:17:39 +0900

    [Silver II] Title: 싸이버개강총회, Time: 168 ms, Memory: 43548 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
@@ -7,17 +7,15 @@ quit_time = int(q[:2]+q[3:])
 cnt = 0
 
 records = []  
+check=set()
 for line in sys.stdin:
     time,name = line.split()
-    records.append((int(time[:2]+time[3:]),name))
-
-check=set()
-for record in records:
-    if record[0]<=start_time:
-        check.add(record[1])
-    if quit_time>=record[0]>=end_time:
-        if record[1] in check:
-            check.remove(record[1])
-            cnt+=1
-
+    time=int(time[:2]+time[3:])
+    if time<=start_time:
+        check.add(name)
+    if quit_time>=time>=end_time:
+        if name in check:
+            check.remove(name)
+            cnt+=1    
+            
 print(cnt)
