1610e7951647d7cb1d25903fbb8b582af5c27a90
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-06-29 12:30:34 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 64 ms, Memory: 34292 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+def func():
+    s = input()
+    alphabet = defaultdict(int)
+    for v in s:
+        alphabet[v]+=1
+    
+    cnt=0
+    temp=""
+    for v in alphabet:
+        if alphabet[v]%2==1:
+            cnt+=1
+            temp=v
+        if cnt>1:
+            print("I'm Sorry Hansoo")
+            return
+
+    sorted_alphabet=sorted(alphabet.items())
+    new_s = ""
+    
+    for (v,i) in sorted_alphabet:
+        new_s+=v*(i//2)
+
+    reverse_new_s = new_s[::-1]
+    if temp:
+        new_s+=temp
+
+    new_s+=reverse_new_s
+    if sorted(new_s) != sorted(s):
+        print("I'm Sorry Hansoo")
+    else:
+        print(new_s)
+
+func()
