902b025944bfd9fcfadb0b318fd0e97bf3a13032
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-01 18:41:29 +0900

    [Gold V] Title: 톱니바퀴, Time: 68 ms, Memory: 34308 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -3,54 +3,34 @@ from collections import deque
 gear= []
 for _ in range(4):
     gear.append(deque(input()))
-
 k = int(input())
+
+def check_right(x,d):
+    if x>3 or gear[x-1][2]==gear[x][6]:
+        return
+    
+    if gear[x-1][2]!=gear[x][6]:
+        check_right(x+1,-d)
+        gear[x].rotate(d)
+
+def check_left(x,d):
+    if x<0 or gear[x][2] == gear[x+1][6]:
+        return
+
+    if gear[x][2]!=gear[x+1][6]:
+        check_left(x-1,-d)
+        gear[x].rotate(d) 
+    
+
 for _ in range(k):
     num,direction = map(int,input().split())
-    dir_temp = [0,0,0,0]
-    if num==1:
-        dir_temp[0]=direction
-        if gear[0][2]!=gear[1][6]:
-            dir_temp[1]=direction*(-1)
-            if gear[1][2] != gear[2][6]:
-                dir_temp[2]=dir_temp[1]*(-1)
-                if gear[2][2]!=gear[3][6]:
-                    dir_temp[3]=dir_temp[2]*(-1)
-    elif num==2:
-        dir_temp[1]=direction
-        if gear[1][2]!=gear[2][6]:
-            dir_temp[2]=direction*(-1)
-            if gear[2][2]!=gear[3][6]:
-                dir_temp[3]=dir_temp[2]*(-1)
-        
-        if gear[0][2]!=gear[1][6]:
-            dir_temp[0]=direction*(-1)
 
-    elif num==3:
-        dir_temp[2]=direction
-        if gear[2][2]!=gear[3][6]:
-            dir_temp[3]=direction*(-1)
-        
-        if gear[1][2]!=gear[2][6]:
-            dir_temp[1]=direction*(-1)
-            if gear[0][2]!=gear[1][6]:
-                dir_temp[0]=dir_temp[1]*(-1)
-
-    else:
-        dir_temp[3]=direction
-        if gear[2][2]!=gear[3][6]:
-            dir_temp[2]=direction*(-1)
-            if gear[1][2]!=gear[2][6]:
-                dir_temp[1]=dir_temp[2]*(-1)
-                if gear[0][2]!=gear[1][6]:
-                    dir_temp[0]=dir_temp[1]*(-1)
-
-    for i in range(len(dir_temp)):
-        gear[i].rotate(dir_temp[i])
+    check_right(num,-direction)
+    check_left(num-2,-direction)
+    gear[num-1].rotate(direction)
 
 answer=0
 for i in range(len(gear)):
-    if gear[i][0]=="1":
-        answer+=2**(i)
+    answer+=(2**(i)) * int(gear[i][0])
 
 print(answer)
