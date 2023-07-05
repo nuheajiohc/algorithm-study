2a5adba0d5a89c5e5f89af2bc15f1df5989df597
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-05 14:30:41 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,17 @@
 n,m = map(int,input().split())
+arr = [False]*m
+isused = [False]*(n+1)
 
-vis= [0]*9
-arr= [0]*8
-def dfs(k):
+def func(k):
     if k==m:
-        print(*arr[:m],sep=" ")
+        print(*arr)
+        return
+    
+    for i in range(1,n+1):
+        if not isused[i]:
+            arr[k]=i
+            isused[i]=True
+            func(k+1)
+            isused[i]=False
 
-    for i in range(1, n+1):
-        if vis[i]:
-            continue
-        arr[k]=i
-        vis[i]=1
-        dfs(k+1)
-        vis[i]=0
-dfs(0)
+func(0)
