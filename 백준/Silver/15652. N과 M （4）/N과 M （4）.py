f21067cf938e90429e0371ab8185da9000b7e7d7
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-05 16:46:16 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+n,m = map(int,input().split())
+
+arr = [False] * m
+vis = [False] * (n+1)
+
+def func(k):
+    if k==m:
+        print(*arr)
+        return
+    
+    for i in range(1,n+1):
+        if k>0 and arr[k-1]>i:
+            continue
+        arr[k]=i
+        func(k+1)
+
+func(0)
