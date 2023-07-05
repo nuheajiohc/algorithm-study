ce10d09cda3e34fd1c08c6c94eb31344aea562a0
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-05 16:04:56 +0900

    [Silver III] Title: N과 M (3), Time: 2004 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+arr = [False] * m
+visited = [False] * (n+1)
+
+def func(k):
+    if k==m:
+        print(*arr)
+        return
+    
+    for i in range(1,n+1):
+        arr[k]=i
+        func(k+1)
+
+func(0)
