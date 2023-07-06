e2045ccad051206271b3483e84aba5bea77086bb
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-06 10:26:25 +0900

    [Silver II] Title: 촌수계산, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,29 +1,25 @@
-from collections import defaultdict
-from collections import deque
-
 n = int(input())
-x,y = map(int,input().split())
+a,b = map(int,input().split())
 m = int(input())
-relationship = defaultdict(list)
-vis = [0]*(n+1)
-count=0
-q = deque()
+graph = [[] for _ in range(n+1)]
+vis = [False] * (n+1)
+
 for _ in range(m):
-    a,b = map(int,input().split())
-    relationship[a].append(b)
-    relationship[b].append(a)
+    x,y = map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-vis[x]=1
-q.append(x)
-
-while q:
-    temp = q.popleft()
-    for i in relationship[temp]:
+def dfs(v,cnt):
+    if v==b:
+        print(cnt)
+        return
+    
+    for i in graph[v]:
         if not vis[i]:
-            q.append(i)
-            vis[i]=vis[temp]+1
+            vis[i]=True
+            dfs(i,cnt+1)
 
-if vis[y]:
-    print(vis[y]-1)
-else:
+vis[a]=True
+dfs(a,0)
+if not vis[b]:
     print(-1)
