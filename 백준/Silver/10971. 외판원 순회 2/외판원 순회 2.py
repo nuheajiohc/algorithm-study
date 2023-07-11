c86be62e2d04cffde146eff4bf93951331a0a34f
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-11 22:01:12 +0900

    [Silver II] Title: 외판원 순회 2, Time: 568 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,29 +1,22 @@
 n = int(input())
-network = []
-for _ in range(n):
-    network.append(list(map(int,input().split())))
-
+network = [list(map(int,input().split())) for _ in range(n)]
 min_cost = 1000000*n
-arr = [0]
-def func(cost):
+vis = [0]*n
+vis[0]=1
+
+def func(depth,st,cost):
     global min_cost
-    if len(arr)==n:
-        if network[arr[-1]][arr[0]]==0:
-            return
-        cost+=network[arr[-1]][arr[0]]
-        min_cost = min(min_cost,cost)
+    if depth == n and network[st][0]:
+        min_cost = min(min_cost,cost+network[st][0])
         return
     
-    for i in range(1,n):
-        if i not in arr:
-            arr.append(i)
-            if network[arr[-2]][arr[-1]]==0:
-                arr.pop()
-                continue
-            cost += network[arr[-2]][arr[-1]]
-            func(cost)
-            cost -= network[arr[-2]][arr[-1]]
-            arr.pop()
+    for en in range(n):
+        if not vis[en] and network[st][en]:
+            cost+=network[st][en]
+            vis[en]=1
+            func(depth+1,en,cost)
+            cost-=network[st][en]
+            vis[en]=0
 
-func(0)
+func(1,0,0)
 print(min_cost)
