58bc7c5c779d920a3db900195780b53bdcb12dd8
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-11 18:40:20 +0900

    [Silver II] Title: 로또, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,15 +1,16 @@
+def func(n):
+    if len(arr)==6:
+        print(*arr)
+    
+    for i in range(n,k):
+        arr.append(s[i])
+        func(i+1)
+        arr.pop()
+
 while True:
     k,*s = map(int,input().split())
     if k==0:
         break
     arr = []
-    def func(n):
-        if len(arr)==6:
-            print(*arr)
-        
-        for i in range(n,k):
-            arr.append(s[i])
-            func(i+1)
-            arr.pop()
     func(0)
     print()
