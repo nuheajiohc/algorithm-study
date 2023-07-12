93a761540dc8c071b9a27b8506cf74ee99ee5cae
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-12 17:15:31 +0900

    [Silver I] Title: 안전 영역, Time: 756 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,51 @@
+from collections import deque
+
+n = int(input())
+board = [list(map(int,input().split())) for _ in range(n)]
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+max_count=0
+
+def check_land():
+    cnt=0
+    for i in range(n):
+        for j in range(n): 
+            if board[i][j]==0:
+                cnt+=1
+                continue
+            board[i][j]-=1
+    if cnt==n*n:
+        return False
+    else:
+        return True
+
+def bfs():
+    vis = [[0]*n for _ in range(n)]
+    count=0
+    for i in range(n):
+        for j in range(n):
+            if not vis[i][j] and board[i][j]:
+                count+=1
+                vis[i][j]=1
+                q = deque([(i,j)])
+                while q:
+                    x,y=q.popleft()
+                    for dir in range(4):
+                        nx = x + dx[dir]
+                        ny = y + dy[dir]
+                        if nx<0 or ny<0 or nx>=n or ny>=n:
+                            continue
+                        if vis[nx][ny] or not board[nx][ny]:
+                            continue
+                        vis[nx][ny]=1
+                        q.append((nx,ny))
+    return count
+
+for i in range(1,101):
+    if check_land():
+        max_count= max(max_count,bfs())
+    else:
+        max_count= max(max_count,1)
+        break
+print(max_count)
