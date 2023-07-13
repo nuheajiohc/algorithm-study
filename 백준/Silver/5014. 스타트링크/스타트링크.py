9fea169837e86245810ceb65de1e8c351dfd2538
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-13 14:31:55 +0900

    [Silver I] Title: 스타트링크, Time: 544 ms, Memory: 137856 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -5,17 +5,18 @@ vis = {}
 answer = "use the stairs"
 vis[s]=0
 q= deque([s])
-while q:
-    x=q.popleft()
-    for dir in [u,-d]:
-        nx=x+dir
-        if nx<1 or nx>f:
-            continue
-        if nx in vis:
-            continue
-        vis[nx]=vis[x]+1
-        q.append(nx)
-
+def bfs():
+    while q:
+        x=q.popleft()
+        for dir in [u,-d]:
+            nx=x+dir
+            if nx<1 or nx>f:
+                continue
+            if nx in vis:
+                continue
+            vis[nx]=vis[x]+1
+            q.append(nx)
+bfs()
 if g in vis:
     answer=vis[g]
 print(answer)
