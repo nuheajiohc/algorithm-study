efbda07d9797048cd72ab2920f0fab1556c1ed16
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-15 08:44:33 +0900

    [Gold V] Title: 암호 만들기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,20 @@
+l,c = map(int,input().split())
+alpha = sorted(input().split())
+moeum = {"a","e","i","o","u"}
+arr=[]
+
+def func(k,moeum_num,jaeum_num):
+    if len(arr)==l and moeum_num>0 and jaeum_num>1:
+        print("".join(arr))
+        return
+    
+    for i in range(k,c):
+        if alpha[i] not in arr:
+            arr.append(alpha[i])
+            if alpha[i] in moeum:
+                func(i+1,moeum_num+1,jaeum_num)
+            else:
+                func(i+1,moeum_num,jaeum_num+1)
+            arr.pop()
+
+func(0,0,0)
