441d19377cebf56bd9911e78384ebee42c6af526
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-15 09:07:34 +0900

    [Gold V] Title: 암호 만들기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,20 +1,19 @@
 l,c = map(int,input().split())
 alpha = sorted(input().split())
 moeum = {"a","e","i","o","u"}
-vis = [0]*c
+arr=[]
 
-def func(depth,k,moeum_num,jaeum_num,result):
-    if depth==l and moeum_num>0 and jaeum_num>1:
-        print(result)
+def func(k,moeum_num,jaeum_num):
+    if len(arr)==l and moeum_num>0 and jaeum_num>1:
+        print("".join(arr))
         return
     
     for i in range(k,c):
-        if not vis[i]:
-            vis[i]=1
-            if alpha[i] in moeum:
-                func(depth+1,i+1,moeum_num+1,jaeum_num,result+alpha[i])
-            else:
-                func(depth+1,i+1,moeum_num,jaeum_num+1,result+alpha[i])
-            vis[i]=0
+        arr.append(alpha[i])
+        if alpha[i] in moeum:
+            func(i+1,moeum_num+1,jaeum_num)
+        else:
+            func(i+1,moeum_num,jaeum_num+1)
+        arr.pop()
 
-func(0,0,0,0,"")
+func(0,0,0)
