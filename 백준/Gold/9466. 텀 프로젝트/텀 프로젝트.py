cf26d344cd9f76a783665f8bdadae1f76d438a80
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-20 16:02:45 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2624 ms, Memory: 61632 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,30 +1,27 @@
-import sys
+import sys 
 sys.setrecursionlimit(10**6)
+
 t = int(input())
 
 def dfs(i):
-    while i not in temp_vis:
-        if vis[i]:
-            return False
-        vis[i]=-1
-        temp_vis.add(i)
-        i = students[i]
-    return i
-        
-def check(i,k):
-    if not vis[k]==1:
-        vis[k]=1
-        check(i,students[k])
+    global count
+    vis[i]=1
+    cycle_list.append(i)
+    if vis[stu[i]]:
+        if stu[i] in cycle_list:
+            count -= len(cycle_list[cycle_list.index(stu[i]):])
+        return
+    else:
+        dfs(stu[i])
 
 for _ in range(t):
     n = int(input())
-    students = [0]+list(map(int,sys.stdin.readline().split()))
+    stu = [0]+list(map(int,sys.stdin.readline().split()))
     vis = [0]*(n+1)
+    count=n
     for i in range(1,n+1):
         if not vis[i]:
-            temp_vis = set()
-            temp = dfs(i)
-            if temp:
-                check(temp,temp)
-
-    print(vis.count(-1))
+            cycle_list = []
+            dfs(i)
+    
+    print(count)
