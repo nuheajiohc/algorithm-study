93a66167f84a2a3c4fc6216a4c8abf23fad9716b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-21 23:29:06 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 3936 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -1,45 +1,31 @@
 n = int(input())
 eggs = [list(map(int,input().split())) for _ in range(n)]
 max_num = 0
-vis = [0]*n
-
-def dfs(k):
+def dfs(k,cnt):
     global max_num
     if k==n:
-        max_num=max(max_num,vis.count(1))
+        max_num = max(max_num,cnt)
         return
     
-    if vis[k]:
-        dfs(k+1)
+    if eggs[k][0]<=0:
+        dfs(k+1,cnt)
         return
-
+    
+    if cnt==n-1:
+        max_num = max(max_num,cnt)
+        return
+    
     for i in range(n):
-        if not vis[i] and i!=k:
-            if eggs[k][0]<=eggs[i][1]:
-                vis[k]=1
-                if eggs[i][0]<=eggs[k][1]:
-                    vis[i]=1
-                    dfs(k+1)
-                    vis[i]=0
-                else:
-                    eggs[i][0]-=eggs[k][1]
-                    dfs(k+1)
-                    eggs[i][0]+=eggs[k][1]
-                vis[k]=0
-            else:
-                eggs[k][0]-=eggs[i][1]
-                if eggs[i][0]<=eggs[k][1]:
-                    vis[i]=1
-                    dfs(k+1)
-                    vis[i]=0
-                else:
-                    eggs[i][0]-=eggs[k][1]
-                    dfs(k+1)
-                    eggs[i][0]+=eggs[k][1]
-                eggs[k][0]+=eggs[i][1]
-
-    if k==n-1:
-        max_num=max(max_num,vis.count(1))
-        return
-dfs(0)
+        if eggs[i][0]>0 and i!=k:
+            eggs[i][0]-=eggs[k][1]
+            eggs[k][0]-=eggs[i][1]
+            temp=0
+            if eggs[i][0]<=0:
+                temp+=1
+            if eggs[k][0]<=0:
+                temp+=1
+            dfs(k+1,cnt+temp)
+            eggs[i][0]+=eggs[k][1]
+            eggs[k][0]+=eggs[i][1]
+dfs(0,0)
 print(max_num)
