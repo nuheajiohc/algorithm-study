db4ef0e28316bf374428e2476b5e2725db53acd8
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-22 23:43:52 +0900

    [Gold IV] Title: 스도쿠, Time: 3668 ms, Memory: 203272 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,39 @@
+import sys
+
+board = [list(map(int,input().split())) for _ in range(9)]
+blank_arr=[]
+for i in range(9):
+    for j in range(9):
+        if board[i][j]==0:
+            blank_arr.append((i,j))
+
+def check_possible_num(x,y):
+    num = {i for i in board[x]}
+
+    for i in range(9):
+        num.add(board[i][y])
+
+    r = int(x/3)*3
+    w = int(y/3)*3
+    for i in range(r,r+3):
+        for j in range(w,w+3):
+            num.add(board[i][j])
+
+    possible_num={i for i in range(1,10)}
+    return possible_num-num
+
+def backtracking(k):
+    if k==len(blank_arr):
+        for i in range(9):
+            print(*board[i])
+        sys.exit()
+
+    x,y=blank_arr[k]
+    possible_num = check_possible_num(x,y)
+    
+    for i in possible_num:
+        board[x][y]=i
+        backtracking(k+1)
+        board[x][y]=0
+
+backtracking(0)
