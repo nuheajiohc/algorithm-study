6b2769a4cc860fbf8612ed96dc97f29b6a9edd98
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-07-24 15:16:43 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 3888 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -1,31 +1,27 @@
 n = int(input())
 eggs = [list(map(int,input().split())) for _ in range(n)]
 max_num = 0
-def dfs(k,cnt):
+def dfs(k,count):
     global max_num
     if k==n:
-        max_num = max(max_num,cnt)
+        max_num = max(max_num,count)
+        return
+    
+    if count==n-1:
+        max_num = max(max_num,n-1)
         return
     
     if eggs[k][0]<=0:
-        dfs(k+1,cnt)
+        dfs(k+1,count)
         return
-    
-    if cnt==n-1:
-        max_num = max(max_num,cnt)
-        return
-    
+
     for i in range(n):
         if eggs[i][0]>0 and i!=k:
-            eggs[i][0]-=eggs[k][1]
             eggs[k][0]-=eggs[i][1]
-            temp=0
-            if eggs[i][0]<=0:
-                temp+=1
-            if eggs[k][0]<=0:
-                temp+=1
-            dfs(k+1,cnt+temp)
-            eggs[i][0]+=eggs[k][1]
+            eggs[i][0]-=eggs[k][1]
+            dfs(k+1,count+(eggs[k][0]<=0)+(eggs[i][0]<=0))
             eggs[k][0]+=eggs[i][1]
+            eggs[i][0]+=eggs[k][1]
+
 dfs(0,0)
 print(max_num)
