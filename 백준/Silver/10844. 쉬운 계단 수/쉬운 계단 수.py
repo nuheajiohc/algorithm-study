85466cdbb9a1529fa897351ece9123a703f75913
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-08-01 14:33:35 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,15 +1,14 @@
-N =int(input())
-memo=[[0]*10 for _ in range(N+1)]
-for i in range(1,10):
-    memo[1][i]=1
+n = int(input())
+d = [[0]*10 for _ in range(n+1)]
+d[1]=[0]+[1]*9
 
-for i in range(2,N+1):
+for i in range(2,n+1):
     for j in range(10):
         if j==0:
-            memo[i][j]=memo[i-1][j+1]
+            d[i][j] = d[i-1][1]
         elif j==9:
-            memo[i][j]=memo[i-1][j-1]
+            d[i][j] = d[i-1][8]
         else:
-            memo[i][j]=memo[i-1][j-1]+memo[i-1][j+1]
-
-print(sum(memo[N])%1000000000)
+            d[i][j] = d[i-1][j-1] + d[i-1][j+1]
+    
+print(sum(d[n])%1000000000)
