e528848e830411fdb0c121e0bc46b9e1ffada06b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-08-01 13:38:07 +0900

    [Silver I] Title: 포도주 시식, Time: 408 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,14 +1,13 @@
 n = int(input())
-nums = [0]+[int(input()) for _ in range(n)]
-d=[[0,0] for _ in range(n+1)]
-
-d[1]=[nums[1],nums[1]]
+nums = [0] + [int(input()) for _ in range(n)]
+d = [0]*(n+1)
+d[1] = nums[1]
 if n>1:
-    d[2][0] = nums[2]
-    d[2][1] = nums[2] + nums[1] 
+    d[2] = nums[1] + nums[2]
+if n>2:
+    d[3] = max(nums[3]+nums[1],nums[3]+nums[2],d[2])
 
-for i in range (3,n+1):
-    d[i][0] = nums[i] + max(*d[i-2],*d[i-3])
-    d[i][1] = nums[i] + d[i-1][0]
+for i in range(4,n+1):
+    d[i] = max(nums[i]+nums[i-1]+d[i-3],nums[i]+d[i-2],d[i-1])
 
-print(max(*d[n],*d[n-1]))
+print(d[n])
