6c10f72c0ff9285df123da27412309dddfb6a957
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-08-03 14:47:14 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 1408 ms, Memory: 93548 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -3,9 +3,11 @@ input = sys.stdin.readline
 
 n,m = map(int,input().split())
 d = [1]*(n+1)
-nums = [list(map(int,input().split())) for _ in range(m)]
-
-nums.sort(key=lambda x:(x[0],x[1]))
+nums = []
+for i in range(m):
+    a,b = map(int,input().split())
+    nums.append((a,b))
+nums.sort()
 for a,b in nums:
     if d[b]<=1+d[a]:
         d[b]=1+d[a]
