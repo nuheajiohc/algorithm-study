bb25936626cceb0ea10f89a22128e67171db4f03
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-08-03 15:00:38 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 1344 ms, Memory: 93548 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -1,6 +1,5 @@
 import sys
 input = sys.stdin.readline
-
 n,m = map(int,input().split())
 d = [1]*(n+1)
 nums = [tuple(map(int,input().split())) for _ in range(m)]
@@ -8,5 +7,4 @@ nums.sort()
 for a,b in nums:
     if d[b]<=1+d[a]:
         d[b]=1+d[a]
-
 print(*d[1:])
