4172d95f8e35e36e7810886653d1acf336b2bd39
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-08-10 13:18:37 +0900

    [Silver II] Title: 나무 자르기, Time: 2176 ms, Memory: 150336 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,20 @@
+n,m = map(int,input().split())
+nums = list(map(int,input().split()))
+
+def binary_search():
+    st = 0
+    en = max(nums)
+
+    while st<=en:
+        mid=(st+en)//2
+        length=0
+        for num in nums:
+            if mid<num:
+                length+=num-mid
+        if length<m:
+            en=mid-1
+        else:
+            st=mid+1
+    return en
+
+print(binary_search())
