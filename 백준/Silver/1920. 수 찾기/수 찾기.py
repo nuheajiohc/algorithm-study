60c9c3a1605957dc020487641fd4cec93a909d11
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-08-11 15:23:28 +0900

    [Silver IV] Title: 수 찾기, Time: 420 ms, Memory: 47268 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,23 +1,27 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-nums = list(map(int,input().split()))
-nums.sort()
+arr_n = list(map(int, input().split()))
+arr_n.sort()
+
 m = int(input())
-check = list(map(int,input().split()))
+arr_m = tuple(map(int, input().split()))
 
-def binary_search(i):
-    st = 0
-    en = n-1
-    while st<=en:
-      mid=(st+en)//2
-      if nums[mid]<=i:
-        st=mid+1
-      else: 
-        en=mid-1
-    
-    if nums[en]==i:
-       return 1
-    else:
-       return 0
+def binary_search(num, start, end):
+    while start <= end:
+        mid = (start + end) // 2
 
-for i in check:
-    print(binary_search(i))
+        if arr_n[mid] == num:
+            return 1
+
+        elif arr_n[mid] > num:
+            end = mid - 1
+
+        else: # arr_n[mid] < num
+            start = mid + 1
+
+    return 0
+
+for num in arr_m:
+    print(binary_search(num, 0, n-1))
