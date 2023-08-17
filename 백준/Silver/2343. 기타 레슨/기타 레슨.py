0163b114794622c91a6c020e6135722995e799a6
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-08-17 15:54:09 +0900

    [Silver I] Title: 기타 레슨, Time: 316 ms, Memory: 42340 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,33 @@
+n,m = map(int,input().split())
+lectures = list(map(int,input().split()))
+
+def check_size(mid):
+    s=0
+    cnt=0
+    for i in range(n):
+      if s+lectures[i]>mid:
+        cnt+=1
+        s=0
+      s+=lectures[i]
+
+    if s:
+      cnt+=1
+      
+    if cnt>m:
+      return False
+    else:
+      return True
+
+def binary_search():
+    st=max(lectures)
+    en=sum(lectures)
+    while st<=en:
+      mid=(st+en)//2
+      status=check_size(mid)
+      if status:
+        en=mid-1
+      else:
+        st=mid+1
+    return st
+
+print(binary_search())
