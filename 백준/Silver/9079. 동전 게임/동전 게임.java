94ff378d2efdc729f9c4cff0d6bd720a7b0ee0f3
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2023-12-18 00:52:11 +0900

    [Silver II] Title: 동전 게임, Time: 160 ms, Memory: 16384 KB -BaekjoonHub

---
+++ 백준/Silver/9079. 동전 게임/동전 게임.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N;
+    private static Queue<Integer> queue;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+
+    private static int[] flipCase = {7, 7 << 3, 7 << 6, 73, 73 << 1, 73 << 2, 273, 84};
+    private static int[] vis;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(testcase());
+        }
+    }
+
+    public static int testcase() throws IOException {
+        vis= new int[512];
+        queue = new LinkedList<>();
+
+        int start = convertToInt();
+        if(start ==0 || start == 511){
+            return 0;
+        }
+        vis[start]=1;
+        queue.offer(start);
+        while(!queue.isEmpty()){
+            int cur=queue.poll();
+            for(int dir : flipCase){
+                int nowCur=cur^dir;
+                if(nowCur ==0 || nowCur == 511){
+                    return vis[cur];
+                }
+                if(vis[nowCur]==0){
+                    vis[nowCur]=vis[cur]+1;
+                    queue.add(nowCur);
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static int convertToInt() throws IOException {
+        String str = (br.readLine() + br.readLine() + br.readLine()).replaceAll(" ", "").replaceAll("H", "0")
+                .replaceAll("T", "1");
+        return Integer.parseInt(str, 2);
+    }
+}
