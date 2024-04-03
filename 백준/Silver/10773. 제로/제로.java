f17e31d122086493e9c502db3ae49628bd345918
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-03 23:26:12 +0900

    [Silver IV] Title: 제로, Time: 248 ms, Memory: 26296 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+        Long count=0L;
+        for(int i=0; i<K;i++){
+            stack.offerLast(Integer.parseInt(br.readLine()));
+            if(stack.peekLast()==0){
+                stack.pollLast();
+                stack.pollLast();
+            }
+        }
+        for(int n:stack){
+            count+=n;
+        }
+        System.out.println(count);
+    }
+}
