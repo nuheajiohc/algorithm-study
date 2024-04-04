48aec4404e88410acb6113e7fc579aa6625a79ff
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-05 01:49:45 +0900

    [Gold V] Title: 신기한 소수, Time: 132 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+
+    private static int N;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        backtracking(0,"");
+        System.out.println(sb.toString());
+    }
+
+    public static boolean isPrime(int n){
+        if(n==1){
+            return false;
+        }
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void backtracking(int depth,String str){
+        if(depth==N){
+            if(isPrime(Integer.parseInt(str))){
+                sb.append(str).append("\n");
+            }
+            return;
+        }
+
+        for(int i=1; i<10; i++){
+            String temp = str + String.valueOf(i);
+            if(!isPrime(Integer.parseInt(temp))){
+                continue;
+            }
+            backtracking(depth+1, temp);
+        }
+    }
+}
