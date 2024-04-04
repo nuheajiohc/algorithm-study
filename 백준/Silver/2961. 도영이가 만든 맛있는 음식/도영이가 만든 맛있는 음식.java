868780c0c776c1e4a71080e1148b7a4192dd9364
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-04 20:21:04 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N;
+    private static int[][] arr;
+    private static long ans = Long.MAX_VALUE;
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        backtracking(0,0,1,0);
+        System.out.println(ans);
+    }
+
+    public static void backtracking(int k, int depth,int a, int b){
+        if(depth == N){
+            return;
+        }
+
+        for(int i=k; i<N; i++){
+            a*=arr[i][0];
+            b+=arr[i][1];
+            ans = Math.min(ans,Math.abs(a-b));
+            backtracking(i+1,depth+1,a,b);
+            a/=arr[i][0];
+            b-=arr[i][1];
+        }
+    }
+}
