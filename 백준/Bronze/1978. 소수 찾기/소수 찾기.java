8abc794dca6c698f93ec2894376ed8927dbc1d56
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-04 23:47:54 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count=0;
+        while(N-->0){
+            if(judge(Integer.parseInt(st.nextToken()))){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static boolean judge(int n){
+        if(n==1){
+            return false;
+        }
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
