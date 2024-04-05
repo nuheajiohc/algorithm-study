237615ce9361321159861caf5fb34a881a7cbf19
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-05 13:18:23 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 396 ms, Memory: 39164 KB -BaekjoonHub

---
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int th = Integer.parseInt(st.nextToken());
+            if(command == 1){
+                int sit = Integer.parseInt(st.nextToken());
+                arr[th-1] = arr[th-1] | (1 << (sit-1));
+            }else if(command == 2){
+                int sit = Integer.parseInt(st.nextToken());
+                arr[th-1] = arr[th-1] & ~(1 << (sit-1));
+            }else if(command ==3){
+                arr[th-1] = (arr[th-1] << 1) & ~(1<<20);
+            }else{
+                arr[th-1] = arr[th-1] >> 1;
+            }
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int n : arr){
+            set.add(n);
+        }
+        System.out.println(set.size());
+    }
+}
