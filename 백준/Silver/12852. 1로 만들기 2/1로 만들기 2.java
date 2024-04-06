af8cf0c19b2252999314bde6f00c2f3d3b778fae
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-06 12:20:09 +0900

    [Silver I] Title: 1로 만들기 2, Time: 152 ms, Memory: 22012 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+1];
+        int[] trace = new int[N+1];
+        
+        for(int i=2; i<=N; i++){
+            dp[i] = dp[i-1]+1;
+            trace[i] = i-1;
+            if(i%2==0 && dp[i]>dp[i/2]+1){
+                dp[i] = dp[i/2]+1;
+                trace[i] = i/2;
+            }
+            if(i%3==0 && dp[i]>dp[i/3]+1){
+                dp[i] = dp[i/3]+1;
+                trace[i] = i/3;
+            }
+        }
+
+        System.out.println(dp[N]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(N).append(" ");
+        while(N>1){
+            sb.append(trace[N]).append(" ");
+            N = trace[N];
+        }
+        System.out.println(sb.toString());
+    }
+}
