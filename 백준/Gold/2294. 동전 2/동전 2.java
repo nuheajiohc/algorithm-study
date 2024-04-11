1d04dc4a57557df7c2c2f15c4492103cb9cbbdb2
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-11 10:35:02 +0900

    [Gold V] Title: 동전 2, Time: 148 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -15,21 +15,13 @@ public class Main {
         Collections.sort(uniCoins);
 
         int[] dp = new int[k+1];
+        Arrays.fill(dp,Integer.MAX_VALUE-1);
+        dp[0]=0;
         for(int i=0; i<uniCoins.size(); i++){
-            if(uniCoins.get(i)<=k)
-                dp[uniCoins.get(i)]=1; 
-            for(int j=uniCoins.get(i)+1; j<=k; j++){
-                if(dp[j]>0 && dp[j-uniCoins.get(i)]>0)
-                    dp[j] = Math.min(dp[j],dp[j-uniCoins.get(i)]+1);
-                else if(dp[j]==0 && dp[j-uniCoins.get(i)]>0)
-                    dp[j] = dp[j-uniCoins.get(i)]+1;
+            for(int j=uniCoins.get(i); j<=k; j++){
+                dp[j] = Math.min(dp[j],dp[j-uniCoins.get(i)]+1);
             }
         }
-
-        if(dp[k]==0){
-            System.out.println(-1);
-            return;
-        }
-        System.out.println(dp[k]);
+        System.out.println(dp[k]==Integer.MAX_VALUE-1?-1:dp[k]);
     }
 }
