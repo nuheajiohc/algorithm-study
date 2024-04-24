01505804f6173b8eaf49f4892dcbcd992a10eb70
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-04-24 23:30:21 +0900

    [Bronze V] Title: A+B - 9, Time: 128 ms, Memory: 14316 KB, Score: 75 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Long.parseLong(st.nextToken())+Long.parseLong(st.nextToken()));
+    }
+}
