c901db76e43ef412127ae8d741ccdc21f2e5afff
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-05-03 12:28:14 +0900

    [Bronze V] Title: 사파리월드, Time: 204 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.abs(sc.nextLong()-sc.nextLong()));
+    }
+}
