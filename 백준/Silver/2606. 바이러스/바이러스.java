8c9a01247941f4eea184f49e5ef180f145e8baa4
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-05-06 10:16:43 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N,M,count;
+    private static List<Integer>[] computers;
+    private static boolean[] vis;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        computers = new ArrayList[N+1];
+        for(int i=1; i<=N; i++){
+            computers[i] = new ArrayList<>();
+        }
+
+        while(M-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            computers[u].add(v);
+            computers[v].add(u);
+        }
+
+        bfs();
+        System.out.println(count);
+    }
+
+    public static void bfs(){
+        vis = new boolean[N+1];
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(1);
+        vis[1]= true;
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            for(int next: computers[cur]){
+                if(vis[next]) continue;
+                vis[next] = true;
+                count+=1;
+                queue.offer(next);
+            }
+        }
+    }
+}
