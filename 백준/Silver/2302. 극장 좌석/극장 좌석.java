80b2a9b2cee547072c2b35e013fe89d5b8c94555
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-05-16 09:14:56 +0900

    [Silver I] Title: 극장 좌석, Time: 116 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.java
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.java
@@ -3,23 +3,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];
+        int N=Integer.parseInt(br.readLine());
+        int M=Integer.parseInt(br.readLine());
+        int[] dp=new int[N+1];
         dp[0]=1;
         dp[1]=1;
         for(int i=2; i<=N; i++){
             dp[i] = dp[i-1]+dp[i-2];
         }
 
-        int answer = 1;
+        int answer=1;
         int prev=0;
         for(int i=0; i<M; i++){
-            int cur = Integer.parseInt(br.readLine());
-            answer *= dp[cur-prev-1];
+            int cur=Integer.parseInt(br.readLine());
+            answer*=dp[cur-prev-1];
             prev=cur;
         }
-        if(N>prev) answer *= dp[N-prev];
+        answer*=dp[N-prev];
         System.out.println(answer);
     }
 }
