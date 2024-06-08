79d4d3a02049837f6ce2f66c213dc7464967a077
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-06-08 13:08:27 +0900

    [level 1] Title: 예산, Time: 0.36 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            if(budget>=d[i]){
+                budget-=d[i];
+                answer++;
+            }else break;
+        }
+        return answer;
+    }
+}
