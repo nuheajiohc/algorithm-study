3da516d93c5883524d3ecc7018e91ac6e212317d
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-06-24 11:12:48 +0900

    [Silver V] Title: 이름 궁합, Time: 224 ms, Memory: 16108 KB -BaekjoonHub

---
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String B = br.readLine();
+
+        int[] alphabet = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i=0; i<A.length(); i++){
+            queue.offer(alphabet[A.charAt(i)-'A']);
+            queue.offer(alphabet[B.charAt(i)-'A']);
+        }
+
+        while(queue.size()>2){
+            int qSize=queue.size()-1;
+            while(qSize-->0){
+                queue.offer((queue.poll()+queue.peek())%10);
+            }
+            queue.poll();
+        }
+
+        int result = queue.poll()*10 + queue.poll();
+        System.out.println(result<10 ?"0"+result: result);
+    }
+}
