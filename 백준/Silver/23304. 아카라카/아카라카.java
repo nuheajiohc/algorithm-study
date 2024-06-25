ae31835f773711ce96495eb7aa5ba44b5beee403
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-06-25 13:42:54 +0900

    [Silver II] Title: 아카라카, Time: 516 ms, Memory: 160792 KB -BaekjoonHub

---
+++ 백준/Silver/23304. 아카라카/아카라카.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        if(isAkarakaPalindrome(word)){
+            System.out.println("AKARAKA");
+        }else{
+            System.out.println("IPSELENTI");
+        }
+    }
+
+    public static boolean isAkarakaPalindrome(String word){
+        if(word.length()==1) return true;
+        String whole = word;
+        int len = word.length();
+        String head = word.substring(0,len/2);
+        String tail = word.substring(len-len/2);
+        
+        if(isPalindrome(whole) && isPalindrome(head) && isPalindrome(tail)){
+            return isAkarakaPalindrome(head) && isAkarakaPalindrome(tail);
+        }
+        return false;
+    }
+
+    public static boolean isPalindrome(String s){
+        for(int i=0; i<=s.length()/2; i++){
+            if(s.charAt(i) != s.charAt(s.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
