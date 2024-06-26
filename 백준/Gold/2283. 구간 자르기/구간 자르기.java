26c401ba60a155d934b7c93f2240ec0862df2e90
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-06-26 10:47:39 +0900

    [Gold II] Title: 구간 자르기, Time: 240 ms, Memory: 47132 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.java
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.java
@@ -8,14 +8,13 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        Map<Integer, Integer> sPoint = new HashMap<>();
-        Map<Integer, Integer> ePoint = new HashMap<>();
+        Map<Integer, Integer> point = new HashMap<>();
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            sPoint.put(a,sPoint.getOrDefault(a,0)+1);
-            ePoint.put(b,ePoint.getOrDefault(b,0)+1);
+            point.put(a,point.getOrDefault(a,0)+1);
+            point.put(b,point.getOrDefault(b,0)-1);
         }
 
         int sum=0;
@@ -26,13 +25,11 @@ public class Main {
         while(e<1000001){
             if(sum<K){
                 e++;
-                eCount+=sPoint.getOrDefault(e-1,0);
-                eCount-=ePoint.getOrDefault(e-1,0);
+                eCount+=point.getOrDefault(e-1,0);
                 sum+=eCount;
             }else if(sum>K){
                 s++;
-                sCount+=sPoint.getOrDefault(s-1,0);
-                sCount-=ePoint.getOrDefault(s-1,0);
+                sCount+=point.getOrDefault(s-1,0);
                 sum-=sCount;
             }else{
                 System.out.println(s+" "+e);
