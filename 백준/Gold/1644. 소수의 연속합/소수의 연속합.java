f2270595f65bf8c2171201034a102ac9fb4f1720
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-07-01 11:16:41 +0900

    [Gold III] Title: 소수의 연속합, Time: 220 ms, Memory: 30200 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -7,17 +7,15 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         boolean[] nums = new boolean[N+1];
         Arrays.fill(nums,true);
-        
+        List<Integer> primes = new ArrayList<>();
         for(int i=2; i<=N; i++){
             if(!nums[i]) continue;
+            primes.add(i);
             for(int j=i+i; j<=N; j+=i){
                 nums[j]=false;
             }
         }
-        List<Integer> primes = new ArrayList<>();
-        for(int i=2; i<=N; i++){
-            if(nums[i]) primes.add(i);
-        }
+
         int s=0;
         long sum=0;
         int count=0;
