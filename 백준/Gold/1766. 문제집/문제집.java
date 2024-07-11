e3310386978947d9dc5ca08f94068fef1e48ed65
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-07-11 09:06:03 +0900

    [Gold II] Title: 문제집, Time: 504 ms, Memory: 45496 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N,M;
+    private static List<Integer>[] adj;
+    private static int[] deg;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        adj = new ArrayList[N+1];
+        for(int i=1; i<=N; i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        deg = new int[N+1];
+        while(M-->0){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adj[a].add(b);
+            deg[b]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new PriorityQueue<>();
+        for(int i=1; i<=N; i++){
+            Collections.sort(adj[i]);
+            if(deg[i]==0) queue.offer(i);
+        }
+
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            sb.append(cur).append(" ");
+            for(int next : adj[cur]){
+                deg[next]--;
+                if(deg[next]==0){
+                    queue.offer(next);
+                }
+            }
+        }   
+        System.out.println(sb.toString());
+    }
+}
