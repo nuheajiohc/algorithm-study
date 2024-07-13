2d6bf0436e2a32402e4aa0f27144f3f0a833e24a
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-07-13 12:12:28 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -4,29 +4,31 @@ import java.util.*;
 public class Main {
 
     private static int N;
-    private static int[][] arr;
-    private static long ans = Long.MAX_VALUE;
-    public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][2];
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-        backtracking(0,0,1,0);
-        System.out.println(ans);
+    private static int[][] foods;
+    private static long minDiff=Long.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    N = Integer.parseInt(br.readLine());
+    foods = new int[N][2];
+    for(int i=0; i<N; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        foods[i] = new int[]{a,b};
+    }
+    findMinDiff(0,1,0);
+    System.out.println(minDiff);
     }
 
-    public static void backtracking(int k, int depth,int a, int b){
-        for(int i=k; i<N; i++){
-            a*=arr[i][0];
-            b+=arr[i][1];
-            ans = Math.min(ans,Math.abs(a-b));
-            backtracking(i+1,depth+1,a,b);
-            a/=arr[i][0];
-            b-=arr[i][1];
+    public static void findMinDiff(int i, long a, long b){
+        if(i==N){
+            long diff = Math.abs(a-b);
+            if(b!=0) minDiff = Math.min(diff,minDiff);
+            return;
         }
+        findMinDiff(i+1,a*foods[i][0],b+foods[i][1]);
+        findMinDiff(i+1,a,b);
     }
 }
