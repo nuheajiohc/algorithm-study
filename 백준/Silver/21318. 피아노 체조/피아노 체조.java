f263cd2d8d2580f5db71ad9ce943ffe94502d209
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-07-15 07:46:00 +0900

    [Silver I] Title: 피아노 체조, Time: 672 ms, Memory: 57520 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.java
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -5,27 +5,24 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int[] paper = new int[N+1];
-        int[] mistake = new int[N+1];
         for(int i=1; i<=N; i++){
-            paper[i]=Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
+        int[] mistakes = new int[N+1];
         for(int i=1; i<N; i++){
-            if(paper[i]>paper[i+1]){
-                mistake[i]= mistake[i-1]+1;
-            }else{
-                mistake[i] = mistake[i-1];
-            }
+            if(nums[i]>nums[i+1]) mistakes[i+1] = mistakes[i]+1;
+            else mistakes[i+1]=mistakes[i];
         }
-
+        
+        StringBuilder sb = new StringBuilder();
         int Q = Integer.parseInt(br.readLine());
         for(int i=0; i<Q; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            sb.append(mistake[end-1]-mistake[start-1]).append("\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(mistakes[b]-mistakes[a]).append("\n");
         }
         System.out.println(sb.toString());
     }
