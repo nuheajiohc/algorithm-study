aa0d9e1f1faa581882e49d9c0aeb52c0ae589964
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-07-17 11:19:13 +0900

    [Gold V] Title: 퇴사 2, Time: 704 ms, Memory: 304320 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -4,24 +4,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        int[] T = new int[N+1];
-        int[] P = new int[N+1];
         int[] dp = new int[N+2];
-        for(int i=1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            T[i] = Integer.parseInt(st.nextToken());
-            P[i] = Integer.parseInt(st.nextToken());
-        }
 
         for(int i=1; i<=N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int T = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
             dp[i+1] = Math.max(dp[i+1],dp[i]);
-            if(i+T[i]>N+1){
-                continue;
-            }
-            dp[i+T[i]] = Math.max(dp[i+T[i]], dp[i]+P[i]); 
+            if(i+T>N+1) continue;
+            dp[i+T] = Math.max(dp[i+T],dp[i]+P);
         }
-        Arrays.sort(dp);
+
         System.out.println(dp[N+1]);
-    }   
+    }
 }
