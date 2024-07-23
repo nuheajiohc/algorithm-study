4da2e8a989ef068878a21ee6245b442d0bcfdade
최재훈 <jaehun341012@gmail.com>
2024-07-23 09:34:08 +0900

    [Lv1] Title: 효도의 시작

---
+++ 소프티어/Lv1/효도의 시작/README.md
@@ -0,0 +1,3 @@
+# Lv1 효도의 시작
+
+[문제링크](https://softeer.ai/practice/7724)
---
+++ 소프티어/Lv1/효도의 시작/효도의 시작.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("엄마 아빠 사랑해요!");
+    }
+}
