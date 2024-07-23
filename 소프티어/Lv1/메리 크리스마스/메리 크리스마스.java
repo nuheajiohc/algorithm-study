f2f9d69e38b11cb288fde6ee5b841a866f4b6f5e
최재훈 <jaehun341012@gmail.com>
2024-07-23 11:03:37 +0900

    [Lv1] Title: 메리 크리스마스

---
+++ 소프티어/Lv1/메리 크리스마스/메리 크리스마스.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("##  ##     ##     #####    #####    ##  ##            ##  ##            ##   ##    ##      ####");
+        System.out.println("##  ##    ####    ##  ##   ##  ##   ##  ##            ##  ##            ### ###   ####    ##  ##");
+        System.out.println("##  ##   ##  ##   ##  ##   ##  ##   ##  ##             ####             #######  ##  ##   ##    ");
+        System.out.println("######   ######   #####    #####     ####               ##     ######   ## # ##  ######    #### ");
+        System.out.println("##  ##   ##  ##   ##       ##         ##               ####             ##   ##  ##  ##       ##");
+        System.out.println("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##   ##  ##");
+        System.out.println("##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##    ####");
+    }
+}
