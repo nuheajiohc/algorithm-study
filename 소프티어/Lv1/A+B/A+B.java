ca55745867c7319e77980854b5597d7a4a5b9cb4
최재훈 <jaehun341012@gmail.com>
2024-07-23 10:07:23 +0900

    [Lv1] Title: A+B

---
+++ 소프티어/Lv1/A+B/A+B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append("Case #").append(i).append(": ").append(a+b).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ 소프티어/Lv1/A+B/README.md
@@ -0,0 +1,3 @@
+# Lv1 A+B
+
+[문제링크](https://softeer.ai/practice/6295)
