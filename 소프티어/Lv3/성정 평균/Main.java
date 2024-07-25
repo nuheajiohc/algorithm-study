ea74c904e9b921b113bed4b8dc42e392a10ca6c6
최재훈 <jaehun341012@gmail.com>
2024-07-26 08:44:39 +0900

    [Lv3] Title: 성적 평균

---
+++ 소프티어/Lv3/성정 평균/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] prefixSum = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            prefixSum[i] = Integer.parseInt(st.nextToken())+prefixSum[i-1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(K-->0){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            double avg =Math.ceil((prefixSum[b]-prefixSum[a-1])*100/(double)((b-a+1)))/(double)100;
+            
+            sb.append(String.format("%.2f",avg)).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ 소프티어/Lv3/성정 평균/README.md
@@ -0,0 +1,3 @@
+# Lv3 성적 평균
+
+[링크](https://softeer.ai/practice/6294)
