69b3c4d6db74ba53a98b4231349c6eb39864f8f6
최재훈 <jaehun341012@gmail.com>
2024-08-21 11:36:08 +0900

    [Lv3] Title: 나무조경 - 다른풀이

--- 소프티어/Lv3/나무조경/Main.java
+++ 소프티어/Lv3/나무조경/Main.java
@@ -59,3 +59,63 @@ public class Main {
         return x<0 || x>=n || y<0 || y>=n;
     }
 }
+
+
+
+
+// 재귀리턴
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int n;
+    private static int[][] board;
+    private static boolean[][] vis;
+    private static int[] dx = {0,1};
+    private static int[] dy = {1,0};
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        vis = new boolean[n][n];
+        board = new int[n][n];
+        for(int i =0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        System.out.println(DFS(0,0,0));
+    }
+
+    public static int DFS(int pairCount, int start, int currentBeauty){
+        if(pairCount == 4){
+            return currentBeauty;
+        }
+
+        int maxBeauty = currentBeauty;
+        for(int i=start; i<n*n; i++){
+            int x = i/n;
+            int y = i%n;
+            if(vis[x][y]) continue;
+            vis[x][y]=true;
+            for(int dir=0; dir<2; dir++){
+                int nx = x+dx[dir];
+                int ny = y+dy[dir];
+                if(isOutOfRange(nx,ny))continue;
+                if(vis[nx][ny]) continue;
+                vis[nx][ny]=true;
+                maxBeauty = Math.max(maxBeauty, DFS(pairCount+1, i+1, currentBeauty+board[x][y]+board[nx][ny]));
+                vis[nx][ny]=false;
+            }
+            vis[x][y]=false;
+        }
+        return maxBeauty;
+    }
+
+    public static boolean isOutOfRange(int x, int y){
+        return x<0 || x>=n || y<0 || y>=n;
+    }
+}
