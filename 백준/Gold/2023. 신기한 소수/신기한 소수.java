926b67504758a7816652e1482cdccee42204c69a
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-23 15:38:03 +0900

    [Gold V] Title: 신기한 소수, Time: 96 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,59 +1,36 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
     private static int N;
-    private static int[] arr;
     private static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
 
-        arr = new int[N];
-
-        findPrimeNumber(0);
+        findPrimeNumber(0,0);
         System.out.println(sb);
     }
 
-    public static void findPrimeNumber(int depth){
-        if(depth==N){
-            int number = convertToNumber();
+    public static void findPrimeNumber(int depth, int number){
+        if(depth == N){
             sb.append(number).append("\n");
             return;
         }
 
         for(int i=1; i<10; i++){
-            arr[depth] = i;
-            int number = convertToNumber(depth);
-            if(!isPrimeNumber(number)) continue;
-            findPrimeNumber(depth+1);
+            int curNumber = number*10 + i;
+            if(!isPrimeNumber(curNumber)) continue;
+            findPrimeNumber(depth+1, curNumber);
         }
     }
 
     public static boolean isPrimeNumber(int number){
-        if(number ==1) return false;
+        if(number==1) return false;
         if(number!=2 && number%2==0) return false;
         for(int i=2; i*i<=number; i++){
             if(number%i==0) return false;
         }
         return true;
     }
-
-
-    public static int convertToNumber(){
-        int number=0;
-        for(int i=0; i<N; i++){
-            number = number*10+arr[i];
-        }
-        return number;
-    }
-
-    public static int convertToNumber(int depth){
-        if(depth==0){
-            return arr[depth]; 
-        }
-        return arr[depth]+10*convertToNumber(depth-1);
-    }
-}
+}   
