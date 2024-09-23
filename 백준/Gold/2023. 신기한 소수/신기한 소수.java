bd56a7bb7f8829c2c7af8e0b3af4d1ca3d3697c9
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-23 15:19:48 +0900

    [Gold V] Title: 신기한 소수, Time: 112 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,41 +1,59 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     private static int N;
+    private static int[] arr;
     private static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
 
-        backtracking(0,"");
-        System.out.println(sb.toString());
+        arr = new int[N];
+
+        findPrimeNumber(0);
+        System.out.println(sb);
     }
 
-    public static boolean isPrime(int n){
-        if(n==1){
-            return false;
-        }
-        for(int i=2; i*i<=n; i++){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void backtracking(int depth,String str){
+    public static void findPrimeNumber(int depth){
         if(depth==N){
-            sb.append(str).append("\n");
+            int number = convertToNumber();
+            sb.append(number).append("\n");
             return;
         }
 
         for(int i=1; i<10; i++){
-            String temp = str + String.valueOf(i);
-            if(!isPrime(Integer.parseInt(temp))){
-                continue;
-            }
-            backtracking(depth+1, temp);
+            arr[depth] = i;
+            int number = convertToNumber(depth);
+            if(!isPrimeNumber(number)) continue;
+            findPrimeNumber(depth+1);
         }
+    }
+
+    public static boolean isPrimeNumber(int number){
+        if(number ==1) return false;
+
+        for(int i=2; i*i<=number; i++){
+            if(number%i==0) return false;
+        }
+        return true;
+    }
+
+
+    public static int convertToNumber(){
+        int number=0;
+        for(int i=0; i<N; i++){
+            number = number*10+arr[i];
+        }
+        return number;
+    }
+
+    public static int convertToNumber(int depth){
+        if(depth==0){
+            return arr[depth]; 
+        }
+        return arr[depth]+10*convertToNumber(depth-1);
     }
 }
