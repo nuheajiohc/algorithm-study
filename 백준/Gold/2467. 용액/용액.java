dc0a31e1cda6d5ba031717341b703a6916fb1006
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-25 20:37:05 +0900

    [Gold V] Title: 용액, Time: 340 ms, Memory: 28684 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,33 +1,35 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] nums = new int[N];
+        int[] values = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
         }
 
-        int minSum=Math.abs(nums[0]+nums[N-1]);
-        int min=nums[0];
-        int max=nums[N-1];
-        int s =0;
-        int e =N-1;
-        while(s<e){
-            if(minSum>Math.abs(nums[s]+nums[e])){
-                minSum = Math.abs(nums[s]+nums[e]);
-                min= nums[s];
-                max= nums[e];
-            }
-            if(nums[s]+nums[e]<0){
-                s++;
-            }else{
-                e--;
+        int minSum = Integer.MAX_VALUE;
+        int minValue=0;
+        int maxValue=0;
+        for(int i=0; i<N-1; i++){
+            int s = i;
+            int e = N;
+            while(s+1<e){
+                int mid = (s+e)/2;
+                if(values[i]+values[mid]<=0) s=mid;
+                else e=mid;
+
+                if(minSum>Math.abs(values[i]+values[mid])){
+                    minValue = values[i];
+                    maxValue = values[mid];
+                    minSum = Math.abs(values[i]+values[mid]);
+                }
             }
         }
-        System.out.println(min+" "+max);
+        System.out.println(minValue + " " + maxValue);
+
     }
 }
