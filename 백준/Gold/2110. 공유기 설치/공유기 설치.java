517cecae80a4504fde714e71875f3a494018b697
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-26 09:23:56 +0900

    [Gold IV] Title: 공유기 설치, Time: 308 ms, Memory: 29240 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,43 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    private static int[] house;
     private static int N,C;
+    private static int[] arr;
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        house = new int[N];
+
+        arr = new int[N];
         for(int i=0; i<N; i++){
-            house[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(house);
+
+        Arrays.sort(arr);
 
         int s=0;
         int e=1000000001;
         while(s+1<e){
             int mid = (s+e)/2;
-            if(check(mid)) s =mid;
-            else e =mid;
+            if(check(mid)) s=mid;
+            else e=mid;
         }
         System.out.println(s);
     }
 
-    public static boolean check(int target){
-        int count=1;
-        int s=0;
-        int e=1;
-        while(e<N){
-            if(house[e]-house[s]>=target){
-                s=e;
+    public static boolean check(int distance){
+        int count = 1;
+        int st =0;
+        for(int en=1; en<N; en++){
+            if(arr[en]-arr[st]>=distance){
+                st=en;
                 count++;
             }
-            e++;
         }
-        return count>=C;
+        if(count<C) return false;
+        return true;
     }
 }
