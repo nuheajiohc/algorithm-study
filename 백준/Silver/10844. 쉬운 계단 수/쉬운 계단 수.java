89c0d212f14f8e7605228fac681d39af875e3174
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-27 18:02:45 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,26 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N][10];
-        Arrays.fill(dp[0],1);
-        dp[0][0]=0;
 
-        for(int i=1; i<N; i++){
-            dp[i][0] = dp[i-1][1]%1000000000;
-            dp[i][9] = dp[i-1][8]%1000000000;
-            for(int j=1; j<9; j++){
-                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%1000000000;
-            }
-        }
-        int sum =0;
-        for(int i=0; i<10; i++){
-            sum+=dp[N-1][i];
-            sum%=1000000000;
-        }
-        System.out.println(sum);
+  private static final int NUM = 1_000_000_000;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    int[][] dp = new int[N+1][10];
+    Arrays.fill(dp[1], 1);
+    dp[1][0]=0;
+    for(int i=2; i<=N; i++) {
+      dp[i][0] = dp[i-1][1]%NUM;
+      dp[i][9] = dp[i-1][8]%NUM;
+      for(int j=1; j<=8; j++) {
+        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%NUM;
+      }
     }
+    int count=0;
+    for(int i=0; i<10; i++){
+      count= (count+dp[N][i])%NUM;
+    }
+    System.out.println(count);
+  }
 }
