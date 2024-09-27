cc849caca2a36b813a8ec3ca391d98ffc8d83750
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-27 15:58:27 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,59 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int[] dx = {1,0,0,-1};
-    private static int[] dy = {0,1,-1,0};
-    private static int[][] board;
 
+    private static int[] dx = {0,0,1,-1};
+    private static int[] dy = {1,-1,0,0};
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<T;i++){
-            int count=0;
+        StringBuilder sb = new StringBuilder();
+        while(T-->0){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int M = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
 
-            board = new int[N][M];
-            for (int a = 0 ; a < K; a++){
-                    st = new StringTokenizer(br.readLine());
-                    int x = Integer.parseInt(st.nextToken());
-                    int y = Integer.parseInt(st.nextToken());
-                    board[y][x] = 1;
+            int[][] board = new int[N][M];
+            for(int i=0; i<K; i++){
+                st = new StringTokenizer(br.readLine());
+                int y = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+                board[x][y]=1;
             }
 
-            for(int a=0; a<N; a++){
-                for(int b=0; b<M; b++){
-                    if(board[a][b]!=1){
-                        continue;
-                    }
+            boolean[][] vis = new boolean[N][M];
+            int count=0;  
+            for(int i=0; i<N; i++){
+                for(int j=0; j<M; j++){
+                    if(board[i][j]==0 || vis[i][j]) continue;
                     count++;
-                    Deque<int[]> q = new ArrayDeque<>();
-                    q.offer(new int[]{a,b});
-                    board[a][b]=2;
-                    while(!q.isEmpty()){
-                        int[] cur=q.poll();
-                        for(int dir=0; dir<4 ;dir++){
-                            int nx=cur[0]+dx[dir];
-                            int ny=cur[1]+dy[dir];
-                            if(nx<0 || ny<0 || nx>=N || ny>=M){
-                                continue;
-                            }
-
-                            if(board[nx][ny]!=1){
-                                continue;
-                            }
-                            board[nx][ny]=2;
-                            q.offer(new int[] {nx,ny});
+                    vis[i][j]=true;
+                    Deque<Point> queue = new ArrayDeque<>();
+                    queue.offer(new Point(i, j));
+                    while(!queue.isEmpty()){
+                        Point p = queue.poll();
+                        for(int dir=0; dir<4; dir++){
+                            int nx = p.x+dx[dir];
+                            int ny = p.y+dy[dir];
+                            if(nx<0|| ny<0 || nx>=N || ny>=M) continue;
+                            if(vis[nx][ny]) continue;
+                            if(board[nx][ny]==0) continue;
+                            vis[nx][ny]=true;
+                            queue.offer(new Point(nx,ny));
                         }
                     }
                 }
             }
             sb.append(count).append("\n");
-        } 
-        System.out.println(sb.toString());
+        }
+        System.out.println(sb);
+    }
+
+    private static class Point{
+        int x;
+        int y;
+
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
