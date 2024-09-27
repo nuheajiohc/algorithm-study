6a9f71f6ed4b2760529b9236e7b743c1a5ce2b0a
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-27 14:09:58 +0900

    [Silver I] Title: 회의실 배정, Time: 504 ms, Memory: 47684 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -14,26 +14,18 @@ public class Main {
             time[i] = new int[]{s,e};
         }
 
-        Arrays.sort(time, (a,b)->{
-            if(a[0] == b[0]){
-                return a[1]-b[1];
+        Arrays.sort(time, (a,b)-> {
+            if(a[1]==b[1]){
+                return a[0]-b[0];
             }
-            return a[0]-b[0];
+            return a[1]-b[1];
         });
         
-
         int count=1;
-        int s = time[0][0];
         int e = time[0][1];
         for(int i=1; i<N; i++){
-            if(e>time[i][1]){
-                s = time[i][0];
-                e = time[i][1];
-                continue;
-            }
             if(e<=time[i][0]){
                 count++;
-                s = time[i][0];
                 e = time[i][1];
             }
         }
