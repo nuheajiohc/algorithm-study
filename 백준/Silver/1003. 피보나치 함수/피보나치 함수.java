0841a969af3f09b91b5cd3d0138c83f6e0654dd6
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-27 13:18:21 +0900

    [Silver III] Title: 피보나치 함수, Time: 96 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,20 +1,24 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+
         int[][] dp = new int[41][2];
-        dp[0] = new int[] {1,0};
-        dp[1] = new int[] {0,1};
+        dp[0] = new int[]{1,0};
+        dp[1] = new int[]{0,1};
         for(int i=2; i<=40; i++){
-            dp[i] = new int[] {dp[i-1][0]+dp[i-2][0], dp[i-1][1]+dp[i-2][1]};
+            dp[i][0] = dp[i-1][0]+dp[i-2][0];
+            dp[i][1] = dp[i-1][1]+dp[i-2][1];
         }
+
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<T; i++){
+        int T = Integer.parseInt(br.readLine());
+        while(T-->0){
             int N = Integer.parseInt(br.readLine());
             sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
-        }
-        System.out.println(sb.toString());
+        }   
+        System.out.println(sb);
     }
 }
