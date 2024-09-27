32fda6979f4765c5435dbec8b69190186431ab3e
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-27 14:36:33 +0900

    [Silver II] Title: 나무 자르기, Time: 512 ms, Memory: 119304 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,36 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     private static int N,M;
-    private static int[] tree;
+    private static int[] arr;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        tree = new int[N];
         for(int i=0; i<N; i++){
-            tree[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int s=0;
-        int e=1000000000;
+        int s = 0;
+        int e = 1000000001;
         while(s+1<e){
             int mid = (s+e)/2;
-            if(check(mid)) s=mid;
-            else e=mid;
+            if(check(mid)) s = mid;
+            else e = mid;
         }
-        System.out.println(s);
+
+       System.out.println(s);
     }
 
-    public static boolean check(int mid){
-        long count=0;
+    public static boolean check(int target){
+        long tree =0;
         for(int i=0; i<N; i++){
-            if(tree[i]>mid){
-                count+=tree[i]-mid;
+            if(target<arr[i]){
+                tree += arr[i]-target;   
             }
         }
-        return count>=M;
+        return M<=tree;
     }
-} 
+}
