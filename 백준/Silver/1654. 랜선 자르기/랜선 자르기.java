eedf5f4cfb13c6f901c8cb8057f7940fb4400435
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-27 12:08:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 17112 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -2,34 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int[] line;
+
     private static int K,N;
+    private static int[] arr;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stnr = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(stnr.nextToken());
-        N = Integer.parseInt(stnr.nextToken());
-        line = new int[K];
-        for(int i=0; i<K; i++){
-            line[i] = Integer.parseInt(br.readLine());
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        long st = 1;
-        long en=(long)1e10 +7;
-        while(st+1<en){
-            long mid = (st+en)/2;
-            if(isOverN(mid)) st = mid;
-            else en = mid;
-        }
-        System.out.println(st);
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-    }   
-    
-    public static boolean isOverN(long x){
-        long sum=0;
+        arr = new int[K];
         for(int i=0; i<K; i++){
-            sum+= line[i]/x;
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        return sum>=N;
-    };
+
+        long s = 1;
+        long e = 1L<<31;
+        while(s+1<e){
+            long mid = (s+e)/2;
+            if(check(mid)) s=mid;
+            else e=mid;
+        }
+        System.out.println(s);
+    }
+
+    public static boolean check(long target){
+        long count=0;
+        for(int i=0; i<K; i++){
+            count += arr[i]/target;
+        }
+        return count>=N;
+    }
 }
