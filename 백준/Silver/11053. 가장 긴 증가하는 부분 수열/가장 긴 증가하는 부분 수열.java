dbb26d4631f29e50b1ad0f5c410fa0ac5b89b16f
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-27 16:47:09 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,26 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] nums = new int[N];
-        int[] dp = new int[N];
-        for(int i=0; i<N; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
 
-        Arrays.fill(dp,1);
-        for(int i=0; i<N; i++){
-            for(int j=0; j<=i; j++){
-                if(nums[i]>nums[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
-            }
-        }
-        Arrays.sort(dp);
-        System.out.println(dp[N-1]);
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i=0; i<N; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    int[] dp = new int[N];
+    Arrays.fill(dp, 1);
+    int max =0;
+    for(int i=0; i<N; i++){
+      for(int j=0; j<i; j++){
+        if(arr[i] > arr[j]){
+          dp[i] = Math.max(dp[i], dp[j]+1);
+        }
+      }
+      max = Math.max(max, dp[i]);
+    }
+    System.out.println(max);
+  }
 }
