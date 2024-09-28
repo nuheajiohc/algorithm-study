b6f83238178baa0f25a129d967d800dedb6ecdcc
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 11:57:10 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 100 ms, Memory: 15304 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,57 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[] arr,op;
-    private static int N;
-    private static int maxVal = Integer.MIN_VALUE;
-    private static int minVal = Integer.MAX_VALUE;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+  private static int N;
+  private static int[] A;
+  private static int minVal = Integer.MAX_VALUE;
+  private static int maxVal = Integer.MIN_VALUE;
 
-        op = new int[4];
-        st= new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++){
-            op[i] = Integer.parseInt(st.nextToken());
-        }
-        backtracking(0,arr[0]);
-        System.out.println(maxVal);
-        System.out.println(minVal);
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    A = new int[N];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i=0; i<N; i++){
+      A[i] = Integer.parseInt(st.nextToken());
+    }
+    st = new StringTokenizer(br.readLine());
+    int plus = Integer.parseInt(st.nextToken());
+    int minus = Integer.parseInt(st.nextToken());
+    int multi = Integer.parseInt(st.nextToken());
+    int div = Integer.parseInt(st.nextToken());
+    btk(plus,minus,multi,div,1, A[0]);
+    System.out.println(maxVal);
+    System.out.println(minVal);
+  }
+
+  private static void btk(int plus, int minus, int multi, int div, int depth, int result){
+    if(plus==0 && minus==0 && multi==0 && div==0){
+      maxVal = Math.max(maxVal,result);
+      minVal = Math.min(minVal,result);
+      return;
     }
 
-    public static void backtracking(int depth,int value){
-        if(depth==N-1){
-            maxVal = Math.max(maxVal,value);
-            minVal = Math.min(minVal,value);
-            return;
-        }
-
-        if(op[0]>0){
-            op[0]--;
-            backtracking(depth+1,value+arr[depth+1]);
-            op[0]++;
-        }
-        if(op[1]>0){
-            op[1]--;
-            backtracking(depth+1,value-arr[depth+1]);
-            op[1]++;  
-        }
-        if(op[2]>0){
-            op[2]--;
-            backtracking(depth+1,value*arr[depth+1]);
-            op[2]++;  
-        }
-        if(op[3]>0){
-            op[3]--;
-            backtracking(depth+1,value/arr[depth+1]);
-            op[3]++;  
-        }
-    }
+    if(plus>0) btk(plus-1, minus, multi, div, depth+1, result+A[depth]);
+    if(minus>0) btk(plus,minus-1, multi, div, depth+1, result-A[depth]);
+    if(multi>0) btk(plus,minus,multi-1, div, depth+1, result*A[depth]);
+    if(div>0) btk(plus,minus,multi,div-1, depth+1, result/A[depth]);
+  }
 }
