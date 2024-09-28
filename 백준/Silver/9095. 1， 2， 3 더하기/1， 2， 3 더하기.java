21b4a8c3815dca514ad273931658cfed4ab031f0
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 10:55:27 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,20 +1,26 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int[] dp = new int[12];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for (int i = 4; i < 12; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-        }
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
-        }
-        System.out.println(sb.toString());
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+
+    int[] dp = new int[12];
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for(int i=4; i<12; i++){
+      dp[i] = dp[i-1] + dp[i-2]+dp[i-3];
     }
+
+    StringBuilder sb = new StringBuilder();
+    while(T-- > 0) {
+      int n = Integer.parseInt(br.readLine());
+      sb.append(dp[n]).append("\n");
+    }
+    System.out.println(sb);
+  }
 }
