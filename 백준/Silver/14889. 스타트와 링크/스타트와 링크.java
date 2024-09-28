1ac911a98f748662bd06459cf087103269e3e5b2
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 21:45:57 +0900

    [Silver I] Title: 스타트와 링크, Time: 276 ms, Memory: 19608 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,51 +1,56 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] vis;
-    static int[][] ability;
-    static int N;
-    static int minValue = Integer.MAX_VALUE;
 
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        ability = new int[N][];
-        vis = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            ability[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-        select(0, 0);
-        System.out.println(minValue);
+  private static int N;
+  private static int[][] ability;
+  private static boolean[] vis;
+  private static int minDiff = Integer.MAX_VALUE;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    N = Integer.parseInt(br.readLine());
+    ability = new int[N][N];
+    for(int i=0; i<N; i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int j=0; j<N; j++){
+        ability[i][j] = Integer.parseInt(st.nextToken());
+      }
     }
 
-    public static void select(int depth, int k) {
-        if (depth == N/2) {
-            minValue = Math.min(minValue, check());
-            return;
-        }
+    vis = new boolean[N];
+    vis[0] = true;
+    btk(0,0);
+    System.out.println(minDiff);
+  }
 
-        for (int i = k; i < N; i++) {
-            if (depth == 0 && k == N / 2) {
-                return;
-            }
-            vis[k] = true;
-            select(depth + 1, i + 1);
-            vis[k] = false;
-        }
+  public static void btk(int depth, int k){
+    if(depth==N/2){
+        minDiff = Math.min(minDiff, getMinDiff());
+        return;
     }
 
-    public static int check() {
-        int sum=0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(vis[i] && vis[j])
-                    sum+=ability[i][j];
-                if(!vis[i] && !vis[j])
-                    sum-=ability[i][j];
-            }
-        }
-        return Math.abs(sum);
+    for(int i=k; i<N; i++){
+      vis[i] = true;
+      btk(depth+1, i+1);
+      vis[i]= false;
     }
+  }
+
+  public static int getMinDiff(){
+    int link = 0;
+    int start =0;
+
+    for(int i=0; i<N; i++){
+      for(int j=0; j<N; j++){
+        if(vis[i] && vis[j]) link+=ability[i][j];
+        if(!vis[i] && !vis[j]) start+=ability[i][j];
+      }
+    }
+    return Math.abs(link-start);
+  }
 }
