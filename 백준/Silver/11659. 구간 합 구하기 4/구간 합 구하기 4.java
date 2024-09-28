bde9bd15cdaebd78ce4e90acc7e63a988282f4fa
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 10:33:22 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 548 ms, Memory: 52672 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,29 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] nums = new int[N];
-        st = new StringTokenizer(br.readLine());
-        nums[0]= Integer.parseInt(st.nextToken());
-        for(int i=1; i<N; i++){
-            nums[i] = Integer.parseInt(st.nextToken())+nums[i-1];
-        }
-        for(int i=0; i<M;i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken())-1;
-            int b = Integer.parseInt(st.nextToken())-1;
-            if(a==0){
-                sb.append(nums[b]).append("\n");
-                continue;
-            }
-            sb.append(nums[b]-nums[a-1]).append("\n");
-        }
-        System.out.println(sb.toString());
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    int[] sum = new int[N + 1];
+    st = new StringTokenizer(br.readLine());
+    for(int i=1; i<=N; i++){
+      sum[i] = sum[i-1]+Integer.parseInt(st.nextToken());
     }
+
+    StringBuilder sb = new StringBuilder();
+    while(M-->0){
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      sb.append(sum[b]-sum[a-1]).append("\n");
+    }
+    System.out.println(sb);
+  }
 }
