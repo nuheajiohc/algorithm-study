e4f3098037e7ba1eef13f8e76429e87748c4f0f3
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 11:01:22 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,19 +1,25 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N][3];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        dp[0] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-        for(int i=1; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            dp[i][0] = Integer.parseInt(st.nextToken()) + Math.min(dp[i-1][1],dp[i-1][2]);
-            dp[i][1] = Integer.parseInt(st.nextToken()) + Math.min(dp[i-1][0],dp[i-1][2]);
-            dp[i][2] = Integer.parseInt(st.nextToken()) + Math.min(dp[i-1][0],dp[i-1][1]);
-        }
-        System.out.println(Arrays.stream(dp[N-1]).min().getAsInt());
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    int[][] dp = new int[N+1][4];
+    for(int i=1; i<=N; i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int r = Integer.parseInt(st.nextToken());
+      int g = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      dp[i][1] = Math.min(dp[i-1][2], dp[i-1][3]) + r;
+      dp[i][2] = Math.min(dp[i-1][1], dp[i-1][3]) + g;
+      dp[i][3] = Math.min(dp[i-1][1], dp[i-1][2]) + b;
     }
+    int min  = Math.min(dp[N][1],dp[N][2]);
+    min = Math.min(min, dp[N][3]);
+    System.out.println(min);
+  }
 }
