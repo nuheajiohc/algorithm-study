6af8f45576e1517208de65350114bbdeaf8b7dff
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 11:23:39 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22428 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int N;
-    private static int M;
-    private static int[] nums;
-    private static boolean[] vis;
-    private static StringBuilder sb = new StringBuilder();
+  private static int N, M;
+  private static int[] arr;
+  private static boolean[] used;
+  private static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        nums = new int[M];
-        vis = new boolean[N+1];
-        dfs(0);
-        System.out.println(sb.toString());
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[M];
+    used = new boolean[N+1];
+    btk(0);
+    System.out.println(sb);
+  }
+
+  public static void btk(int depth){
+    if(depth == M){
+      for(int i : arr){
+        sb.append(i).append(" ");
+      }
+      sb.append("\n");
+      return;
     }
 
-    public static void dfs(int depth){
-        if(depth==M){
-            for(int i=0; i<depth;i++){
-                sb.append(nums[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for(int i=1; i<=N;i++){
-            if(!vis[i]){
-                vis[i]=true;
-                nums[depth]=i;
-                dfs(depth+1);
-                vis[i]=false;
-            }
-        }
+    for(int i=1; i<=N; i++){
+      if(used[i]) continue;
+      used[i] = true;
+      arr[depth]= i;
+      btk(depth+1);
+      used[i] = false;
     }
+  }
 }
