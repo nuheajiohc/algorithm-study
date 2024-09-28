ee225f74c5308a885e19c540232a542c92ef5c6d
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 11:39:26 +0900

    [Silver I] Title: 안전 영역, Time: 252 ms, Memory: 38584 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,60 +1,73 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[][] area;
-    private static boolean[][] safeArea;
-    private static int[] dx = {0,1,-1,0};
-    private static int[] dy = {1,0,0,-1};
+  private static int[] dx = {0,0,1,-1};
+  private static int[] dy = {1,-1,0,0};
+  private static int N;
+  private static int[][] area;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        area = new int[N][N];
-        int height=0;
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                area[i][j] = Integer.parseInt(st.nextToken());
-                height= Math.max(height,area[i][j]);
-            }
-        }
-        
-        int maxCount=0;
-        while(height>=0){
-            safeArea = new boolean[N][N];
-            for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
-                    safeArea[i][j] = area[i][j]>height;
-                }
-            }
-
-            int tempCount=0;
-            for(int i=0; i<N; i++){
-                for(int  j=0; j<N; j++){
-                    if(!safeArea[i][j]) continue;
-                    tempCount++;
-                    safeArea[i][j]=false;
-                    Queue<int[]> queue =new ArrayDeque<>();
-                    queue.offer(new int[]{i,j});
-                    while(!queue.isEmpty()){
-                        int[] cur=queue.poll();
-                        for(int dir=0; dir<4; dir++){
-                            int nx = cur[0]+dx[dir];
-                            int ny = cur[1]+dy[dir];
-                            if(nx<0 || ny<0 || nx>=N || ny>=N) continue;
-                            if(!safeArea[nx][ny]) continue;
-                            safeArea[nx][ny] = false;
-                            queue.offer(new int[]{nx,ny});
-                        }
-                    }
-                }
-            }
-            maxCount = Math.max(maxCount,tempCount);
-            height--;
-        }
-        System.out.println(maxCount);
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    area = new int[N][N];
+    for(int i = 0; i < N; i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int j = 0; j < N; j++){
+        area[i][j] = Integer.parseInt(st.nextToken());
+      }
     }
+
+    int max=0;
+    for(int h=0; h<=100; h++){
+      max = Math.max(max, bfs(h));
+    }
+    System.out.println(max);
+  }
+
+  private static int bfs(int h){
+    boolean[][] vis = new boolean[N][N];
+
+    int count =0;
+    for(int i=0; i<N; i++){
+      for(int j=0; j<N; j++){
+        if(vis[i][j] || area[i][j]<=h) continue;
+        count++;
+        vis[i][j] = true;
+        Deque<Point> queue = new ArrayDeque<>();
+        queue.offer(new Point(i,j));
+
+        while(!queue.isEmpty()){
+          Point point = queue.poll();
+          for(int dir=0; dir<4; dir++){
+            int nx = point.x + dx[dir];
+            int ny = point.y + dy[dir];
+            if(isOutOfRange(nx, ny)) continue;
+            if(vis[nx][ny] || area[nx][ny]<=h) continue;
+            vis[nx][ny] =true;
+            queue.offer(new Point(nx,ny));
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  private static class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  private static boolean isOutOfRange(int x, int y){
+    return x<0 || x>=N || y<0 || y>=N;
+  }
 }
