b32b8dc43a9d13f33393bead8a4e7c8ff94bafd0
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-28 10:05:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 528 ms, Memory: 141952 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,44 +1,53 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] vis = new int[N+1];
-        List<Integer>[] adj = new ArrayList[N+1];
-        for(int i=1; i<=N; i++){
-            adj[i] = new ArrayList<>();
-        }
 
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        while(M-->0){
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            adj[u].add(v);
-            adj[v].add(u);
-        }
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
 
-        int count=0;
-        for(int i=1; i<=N; i++){
-            if(vis[i]==1) continue;
-            count++;
-            vis[i] = 1;
-            Queue<Integer> queue = new ArrayDeque<>();
-            queue.offer(i);
-            while(!queue.isEmpty()){
-                int cur = queue.poll();
-                for(int next: adj[cur]){
-                    if(vis[next]==1) continue;
-                    vis[next] = 1;
-                    queue.offer(next);
-                }
-            }
-        }
-        System.out.println(count);
-        
+    List<Integer>[] adj = new ArrayList[N+1];
+
+    for(int i=1; i<=N; i++){
+      adj[i] =  new ArrayList<>();
     }
+
+    for(int i=1; i<=M; i++){
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      adj[a].add(b);
+      adj[b].add(a);
+    }
+
+    boolean[] vis = new boolean[N+1];
+
+    int count=0;
+    for(int i=1; i<=N; i++){
+      if(vis[i]) continue;
+      count++;
+      Deque<Integer> q = new ArrayDeque<>();
+      vis[i] =true;
+      q.offer(i);
+
+      while(!q.isEmpty()){
+        int cur = q.poll();
+        for(int next : adj[cur]){
+          if(vis[next]) continue;
+          vis[next] = true;
+          q.offer(next);
+        }
+      }
+    }
+    System.out.println(count);
+  }
 }
