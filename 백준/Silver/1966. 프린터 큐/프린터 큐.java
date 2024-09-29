90bf6c0e137e0b22a3179217c501c4de2bd44c08
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-29 18:17:43 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,41 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        while(T-->0){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            List<Integer> list = new ArrayList<>();
-            Deque<int[]> q = new ArrayDeque<>();
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++){
-                int num = Integer.parseInt(st.nextToken());
-                q.offer(new int[]{num,i});
-                list.add(num);
-            }
 
-            Collections.sort(list,(a,b)->b-a);
-            int count=0;
-            Deque<Integer> uq= new ArrayDeque<>(list);
-            while(true){
-                if(uq.peek()>q.peek()[0]){
-                    q.offer(q.poll());
-                }else{
-                    int[] num = q.poll();
-                    uq.poll();
-                    count++;
-                    if(num[1]==M){
-                        sb.append(count).append("\n");
-                        break;
-                    }
-                }
-            }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+    StringBuilder sb= new StringBuilder();
+    while(T-->0){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
+      Integer[] arr = new Integer[N];
+      st = new StringTokenizer(br.readLine());
+      for(int i = 0; i < N; i++){
+        arr[i] = Integer.parseInt(st.nextToken());
+      }
+      Queue<Document> q = new ArrayDeque<>();
+      for(int i = 0; i < N; i++){
+        q.offer(new Document(i,arr[i]));
+      }
+
+      int count=0;
+      Arrays.sort(arr,(a,b)->b-a);
+      for(int num : arr){
+        while(q.peek().important!=num){
+          q.offer(q.poll());
         }
-        System.out.println(sb.toString());
+        if(q.peek().important==num){
+          count++;
+          if(q.peek().idx==M) break;
+          q.poll();
+        }
+      }
+      sb.append(count).append("\n");
     }
+    System.out.println(sb);
+  }
+
+  public static class Document{
+    int idx;
+    int important;
+
+    public Document(int idx, int important) {
+      this.idx = idx;
+      this.important = important;
+    }
+  }
 }
