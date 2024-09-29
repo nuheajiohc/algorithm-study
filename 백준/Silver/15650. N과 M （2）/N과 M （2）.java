eeb1b2645f801a3bf66d73c5fe71b9a27d565bd7
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-29 17:02:50 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,35 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    private static int[] arr;
-    private static int N,M;
-    private static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+  private static int N,M;
+  private static int[] arr;
+  private static StringBuilder sb = new StringBuilder();
 
-        arr = new int[M];
-        backtracking(1,0);
-        System.out.println(sb.toString());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[M];
+    btk(0,1);
+    System.out.println(sb);
+  }
+
+  public static void btk(int depth, int k) {
+    if(depth==M){
+      for(int n: arr){
+        sb.append(n).append(" ");
+      }
+      sb.append("\n");
+      return;
     }
 
-    public static void backtracking(int k, int depth){
-        if(depth==M){
-            for(int n : arr){
-                sb.append(n).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for(int i=k; i<=N;i++){
-            arr[depth] =i;
-            backtracking(i+1,depth+1);
-        }   
+    for(int i=k; i<=N; i++){
+      arr[depth] = i;
+      btk(depth+1,i+1);
     }
+  }
 }
