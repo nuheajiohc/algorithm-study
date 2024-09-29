e500d8e57f376642ae894010442e62be162ca480
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-29 15:21:58 +0900

    [Silver II] Title: 부분수열의 합, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,35 +1,38 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int N,S;
-    private static int[] nums;
-    private static int count;
+  private static int count=0;
+  private static int[] arr;
+  private static int N,S;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        nums = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        backtracking(0,0);
-        System.out.println(S==0?count-1:count);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    S = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    arr = new int[N];
+    for(int i=0; i<N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    public static void backtracking(int depth, int sum){
-        if(depth==N){
-            if(sum==S){
-                count++;
-            }
-            return;
-        } 
-        backtracking(depth+1,sum);
-        backtracking(depth+1,sum+nums[depth]);
+    if(S==0) count=-1;
+    permutation(0,0);
+    System.out.println(count);
+  }
 
+  public static void permutation(int depth, int sum){
+    if(depth == N){
+      if(sum ==S) count++;
+      return;
     }
+
+    permutation(depth+1, sum+arr[depth]);
+    permutation(depth+1, sum);
+  }
 }
