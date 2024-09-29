10d1eca6ae453463bb3b7a5eaff17aef8833ad88
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-29 17:07:08 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 26828 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,26 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Queue<Integer> heap = new PriorityQueue<>((a,b)->b-a);
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<N; i++){
-            int num = Integer.parseInt(br.readLine());
-            if(num==0){
-                if(heap.isEmpty()){
-                    sb.append(0);
-                }else{
-                    sb.append(heap.poll());
-                }
-                sb.append("\n");
-            }else{
-                heap.offer(num);
-            }
-        }
-        System.out.println(sb.toString());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    StringBuilder sb = new StringBuilder();
+    Queue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+    while(N-->0){
+      int x = Integer.parseInt(br.readLine());
+      if(x==0){
+        if(pq.isEmpty()){sb.append(0).append("\n");}
+        else sb.append(pq.poll()).append("\n");
+      }else{
+        pq.offer(x);
+      }
     }
+    System.out.println(sb);
+  }
 }
