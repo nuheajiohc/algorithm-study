3614b24a31e0637a848b1b522546fe815ccc4ce6
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-09-30 10:40:07 +0900

    [Silver I] Title: 나이트의 이동, Time: 324 ms, Memory: 72260 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,62 +1,58 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[][] d = {{2,1},{2,-1},{1,2},{1,-2},{-1,-2},{-2,-1},{-2,1},{-1,2}};
-    private static StringBuilder sb = new StringBuilder();
-    private static int[] cur,des;
+  private static int[] dx ={1,2,1,2,-1,-1,-2,-2};
+  private static int[] dy = {2,1,-2,-1,2,-2,-1,1};
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testcase = Integer.parseInt(br.readLine());
-        for(int i=0; i<testcase; i++){
-            int I = Integer.parseInt(br.readLine());
-            int[][] board = new int[I][I];
-            for(int j=0; j<I; j++){
-                Arrays.fill(board[j], -1);
-            }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            cur = new int[2];
-            cur[0] = Integer.parseInt(st.nextToken());
-            cur[1] = Integer.parseInt(st.nextToken());
+    StringBuilder sb = new StringBuilder();
+    while(T-->0){
+      int N  = Integer.parseInt(br.readLine());
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int cur_x = Integer.parseInt(st.nextToken());
+      int cur_y = Integer.parseInt(st.nextToken());
 
-            st = new StringTokenizer(br.readLine());
-            des = new int[2];
-            des[0] = Integer.parseInt(st.nextToken());
-            des[1] = Integer.parseInt(st.nextToken());
+      st = new StringTokenizer(br.readLine());
+      int next_x = Integer.parseInt(st.nextToken());
+      int next_y = Integer.parseInt(st.nextToken());
 
-            if(Arrays.equals(cur,des)){
-                sb.append(0).append("\n");
-                continue;
-            }
+      int[][] vis = new int[N][N];
+      vis[cur_x][cur_y] = 1;
 
-            board[cur[0]][cur[1]]=0;
-            bfs(board,I);
+      Queue<Point> q = new ArrayDeque<>();
+      q.add(new Point(cur_x,cur_y));
+
+      while(!q.isEmpty()){
+        Point p = q.poll();
+        for(int dir=0; dir<8; dir++){
+          int nx = p.x + dx[dir];
+          int ny = p.y + dy[dir];
+          if(nx<0 || ny<0 || nx>=N || ny>=N || vis[nx][ny]>0) continue;
+          vis[nx][ny] = vis[p.x][p.y] + 1;
+          q.add(new Point(nx,ny));
         }
-
-        System.out.println(sb.toString());
+      }
+      sb.append(vis[next_x][next_y]-1).append("\n");
     }
+    System.out.println(sb);
+  }
 
-    public static void bfs(int[][] board, int I){
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{cur[0],cur[1],0});
-        while(!q.isEmpty()){
-            int[] tempCur=q.poll();
-            for(int[] dir : d){
-                int nx = tempCur[0]+dir[0];
-                int ny = tempCur[1]+dir[1];
-                if(nx<0 || nx>=I || ny<0 || ny>=I) continue;
-                if(board[nx][ny]==-1){
-                    board[nx][ny] = board[tempCur[0]][tempCur[1]]+1;
-                    q.offer(new int[]{nx,ny,board[nx][ny]});
-                    if(Arrays.equals(des,new int[]{nx,ny})){
-                        sb.append(board[nx][ny]).append("\n");
-                        return;
-                    }
-                }
-            }
-        }
+  public static class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
     }
+  }
 }
