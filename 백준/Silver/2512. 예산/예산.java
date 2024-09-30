d7a30e241a9d95e6b94a7402beca957128a6ed24
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-01 07:43:45 +0900

    [Silver II] Title: 예산, Time: 148 ms, Memory: 15216 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,41 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[] regions;
-    private static int upperBound;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        regions = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int maxRequest = 0;
-        for(int i=0; i<N; i++){
-            regions[i] = Integer.parseInt(st.nextToken());
-            maxRequest = Math.max(regions[i],maxRequest);
-        }
-        upperBound = Integer.parseInt(br.readLine());
+  private static int N,M;
+  private static int[] arr;
 
-        int s=1;
-        int e=maxRequest+1;
-        while(s+1<e){
-            int mid = (s+e)/2;
-            if(check(mid)) s=mid;
-            else e=mid;
-        }
-        System.out.println(s);
-    }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    arr = new int[N];
 
-    public static boolean check(int target){
-        int budget = 0;
-        for(int region : regions){
-            if(target<region){
-                budget += target;
-            }else{
-                budget += region;
-            }
-        }
-        return budget <= upperBound;
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int max =0;
+    for(int i=0; i<N; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+      max = Math.max(max, arr[i]);
     }
+    M = Integer.parseInt(br.readLine());
+
+    int s = 1;
+    int e = max+1;
+    while(s+1<e){
+      int mid = (s+e)/2;
+      if(check(mid)) s=mid;
+      else e=mid;
+    }
+    System.out.println(s);
+  }
+
+  public static boolean check(int target){
+    int budget=0;
+    for(int i=0; i<N; i++){
+      if(arr[i]>target){
+        budget+=target;
+      }else{
+        budget+=arr[i];
+      }
+    }
+    return M>=budget;
+  }
+
 }
