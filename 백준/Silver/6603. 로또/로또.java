4d5f73eb6448ed1d03566bf28b681220eb100b79
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-01 07:04:31 +0900

    [Silver II] Title: 로또, Time: 116 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,45 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    private static int[] arr;
-    private static int[] answer;
-    private static int k;
-    private static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(true){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            k = Integer.parseInt(st.nextToken());
-            arr = new int[k];
-            answer = new int[6];
-            if(k==0){
-                break;
-            }
-            for(int i=0; i<k; i++){
-                arr[i]=Integer.parseInt(st.nextToken());
-            }
+  private static int[] arr;
+  private static int[] selected;
+  private static int N;
+  private static StringBuilder sb = new StringBuilder();
 
-            backtracking(0,0);
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String line = "";
+    while (!(line = br.readLine()).equals("0")) {
+      StringTokenizer st = new StringTokenizer(line);
+      N = Integer.parseInt(st.nextToken());
+      arr = new int[N];
+      selected = new int[6];
+      for (int i = 0; i < N; i++) {
+        arr[i] = Integer.parseInt(st.nextToken());
+      }
+
+      btk(0, 0);
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static void btk(int depth, int k) {
+    if (depth == 6) {
+      for(int num : selected) {
+        sb.append(num).append(" ");
+      }
+      sb.append("\n");
+      return;
     }
 
-    public static void backtracking(int cur,int depth){
-        if(depth==6){
-            for(int n: answer){
-                sb.append(n).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for(int i=cur; i<k; i++){
-            answer[depth]= arr[i];
-            backtracking(i+1,depth+1);
-        }
+    for(int i=k; i<N; i++){
+      selected[depth] = arr[i];
+      btk(depth+1, i+1);
     }
+  }
 }
