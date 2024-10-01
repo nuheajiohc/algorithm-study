dcc6fa2987a8feb28cb3cd8b65d68427f1ac5632
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-01 10:25:46 +0900

    [Silver I] Title: 스타트링크, Time: 200 ms, Memory: 36776 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,32 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int F = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        int G = Integer.parseInt(st.nextToken());
-        int U = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
 
-        int[] d = {U,-D};
-        int[] vis = new int[F+1];
-        Arrays.fill(vis,-1);
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(S);
-        vis[S]=0;
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
-            for(int dir: d){
-                int nx = cur+dir;
-                if(nx<1 || nx>F) continue;
-                if(vis[nx]>-1) continue;
-                vis[nx] = vis[cur]+1;
-                queue.offer(nx);
-            }
-        }
-        System.out.println(vis[G]>-1?vis[G]:"use the stairs");
+  private static int[] move;
+  private static int F,S,G,U,D;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    F = Integer.parseInt(st.nextToken());
+    S = Integer.parseInt(st.nextToken());
+    G = Integer.parseInt(st.nextToken());
+    U = Integer.parseInt(st.nextToken());
+    D = Integer.parseInt(st.nextToken());
+    move = new int[]{U,D*-1};
+    bfs();
+  }
+
+  public static void bfs(){
+    Queue<Integer> q = new ArrayDeque<>();
+    int[] vis = new int[F+1];
+    Arrays.fill(vis, -1);
+    q.offer(S);
+    vis[S] = 1;
+    while(!q.isEmpty()){
+      int cur = q.poll();
+      for(int dir=0; dir<2; dir++){
+        int nc = cur+move[dir];
+        if(nc<1 || nc>F) continue;
+        if(vis[nc]>0) continue;
+        vis[nc] = vis[cur]+1;
+        q.offer(nc);
+      }
     }
+    if(vis[G]>0){
+      System.out.println(vis[G]-1);
+    }else{
+      System.out.println("use the stairs");
+    }
+  }
 }
