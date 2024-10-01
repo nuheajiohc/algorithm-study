1a244770c3857865e86d37e73cab6f1e5abf9c9d
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-01 09:41:42 +0900

    [Silver II] Title: A → B, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,38 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
+  private static int A,B;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st= new StringTokenizer(br.readLine());
-    long A = Integer.parseInt(st.nextToken());
-    long B = Integer.parseInt(st.nextToken());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    A= Integer.parseInt(st.nextToken());
+    B= Integer.parseInt(st.nextToken());
 
-    Map<Long, Integer> map = new HashMap<>();
-    Queue<Long> q = new ArrayDeque<>();
-    q.offer(A);
-    map.put(A,1);
-    while(!q.isEmpty()){
-      long cur = q.poll();
-      if(cur*2<=B){
-        map.put(cur*2, map.get(cur)+1);
-        if(cur*2 ==B) break;
-        q.offer(cur*2);
-      }
+    int min = dfs(B,1);
+    System.out.println(min);
+  }
 
-      if(cur*10+1<=B){
-        map.put(cur*10+1, map.get(cur)+1);
-        if(cur*10+1 ==B) break;
-        q.offer(cur*10+1);
-      }
+  private static int dfs(int cur, int count){
+    if(cur<A) return -1;
+    if(A==cur){
+      return count;
     }
-    System.out.println(map.getOrDefault(B, -1));
+
+    if(cur%2==0){
+      return dfs(cur/2, count+1);
+    }else if(cur%10==1){
+      return dfs((cur-1)/10, count+1);
+    }else{
+      return -1;
+    }
   }
 }
