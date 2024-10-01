0405d1ce6ceefbcbcee946fdeabdfc8b4ee0e7a2
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-01 11:07:19 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int x = Integer.parseInt(st.nextToken());
+    int y = Integer.parseInt(st.nextToken());
+
+    int M = Integer.parseInt(br.readLine());
+    List<Integer>[] adj = new ArrayList[N+1];
+    for(int i=1; i<=N; i++){
+      adj[i] = new ArrayList<>();
+    }
+
+    while(M-->0){
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      adj[a].add(b);
+      adj[b].add(a);
+    }
+
+    int[] vis = new int[N+1];
+    Arrays.fill(vis, -1);
+    Queue<Integer> q = new ArrayDeque<>();
+    q.offer(x);
+    vis[x] = 0;
+    while(!q.isEmpty()){
+      int cur = q.poll();
+      for(int next : adj[cur]){
+        if(vis[next]>-1)continue;
+        vis[next] = vis[cur]+1;
+        q.offer(next);
+      }
+    }
+
+    System.out.println(vis[y]);
+  }
+}
