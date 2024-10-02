c2082e099c1b97ca4f75746ac2f166dd003fc329
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-02 11:21:25 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,45 +1,53 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int N;
-    private static int[][] paper;
-    private static int[] result = new int[2];
+  private static int N;
+  private static int[][] paper;
+  private static int white, blue;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        paper = new int[N][N];
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                paper[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        recursion(0,0,N);
-        System.out.println(result[0]+"\n"+result[1]);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    paper = new int[N][N];
+    for(int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int j = 0; j < N; j++) {
+        paper[i][j] = Integer.parseInt(st.nextToken());
+      }
     }
 
-    public static void recursion(int x, int y, int l){
-        if(isSame(x,y,l)){
-            result[paper[x][y]]++;
-            return;
-        }
+    recur(0,0,N);
+    System.out.println(white);
+    System.out.println(blue);
+  }
 
-        for(int i=0; i<2; i++){
-            for(int j=0; j<2; j++){
-                recursion(x+(i*l/2), y+(j*l/2),l/2);
-            }
+  public static void recur(int x, int y, int n){
+    if(n==0) return;
+    int w=0;
+    int b=0;
+    for(int i=x; i<x+n; i++){
+      for(int j=y; j<y+n; j++){
+        if(paper[i][j]==1){
+          b++;
+        }else{
+          w++;
         }
+      }
     }
 
-    public static boolean isSame(int x, int y, int l){
-        for(int i=x; i<x+l; i++){
-            for(int j=y; j<y+l; j++){
-                if(paper[i][j]!=paper[x][y]) return false;
-            }
-        }
-        return true;
+    if(w==0){
+      blue++;
+    }else if(b==0){
+      white++;
+    }else{
+      recur(x,y,n/2);
+      recur(x+n/2,y,n/2);
+      recur(x,y+n/2,n/2);
+      recur(x+n/2,y+n/2,n/2);
     }
+  }
 }
