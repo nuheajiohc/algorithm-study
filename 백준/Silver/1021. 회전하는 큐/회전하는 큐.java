7112d58345012ca2ef961fafaeaa56c64c80e2ca
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-02 23:32:13 +0900

    [Silver III] Title: 회전하는 큐, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -21,36 +21,14 @@ public class Main {
     int min=0;
     st = new StringTokenizer(br.readLine());
     while(st.hasMoreTokens()){
-      int cur = Integer.parseInt(st.nextToken());
-
-      Deque<Integer> repl1 = new ArrayDeque<>(deque);
-      int count1=0;
-      while(true){
-        if(cur==repl1.peekFirst()){
-          repl1.pollFirst();
-          break;
-        }
-        repl1.offerLast(repl1.pollFirst());
-        count1++;
+      int num = Integer.parseInt(st.nextToken());
+      int count=0;
+      while(deque.peekFirst()!=num){
+        deque.offerLast(deque.pollFirst());
+        count++;
       }
-
-      int count2 =0;
-      Deque<Integer> repl2 = new ArrayDeque<>(deque);
-      while(true){
-        if(cur==repl2.peekFirst()){
-          repl2.pollFirst();
-          break;
-        }
-        repl2.offerFirst(repl2.pollLast());
-        count2++;
-      }
-      if(count1 > count2){
-        deque = repl2;
-        min+=count2;
-      }else{
-        deque = repl1;
-        min+=count1;
-      }
+      deque.pollFirst();
+      min += Math.min(count, deque.size()+1-count);
     }
     System.out.println(min);
   }
