1b03dff7fd3fa8939b49fe350e26c527d3d3d7ec
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-02 09:25:38 +0900

    [Silver I] Title: 카드 구매하기, Time: 120 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,22 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] card = new int[N+1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++){
-            card[i] = Integer.parseInt(st.nextToken());
-        }
 
-        int[] dp = new int[N+1];
-        for(int i=1; i<=N; i++){
-            for(int j=i; j<=N; j++){
-                dp[j] = Math.max(dp[j-i]+card[i],dp[j]);
-            }
-        }
-        System.out.println(dp[N]);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] card = new int[N+1];
+    StringTokenizer st = new StringTokenizer(br.readLine());;
+    for(int i=1; i<=N; i++){
+      card[i] = Integer.parseInt(st.nextToken());
     }
+
+    int[] dp = new int[N+1];
+    dp[1]= card[1];
+    for(int i=2; i<=N; i++){
+      dp[i] = card[i];
+      for(int j=1; j<i; j++){
+        dp[i] = Math.max(dp[i],dp[j]+dp[i-j]);
+      }
+    }
+    System.out.println(dp[N]);
+  }
 }
