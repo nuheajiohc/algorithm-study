a7ff84ea426ab92a6490cea7f7015447ef690b28
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-03 10:33:54 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 112 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,26 +1,32 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        while(T-->0){
-            int N = Integer.parseInt(br.readLine());
-            Map<String, List<String>> map = new HashMap<>();
-            while(N-->0){
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                String item = st.nextToken();
-                String category = st.nextToken();
-                map.computeIfAbsent(category, k-> new ArrayList<>()).add(item);
-            }
-            int count=1;
-            for(List<String> items : map.values()){
-                count*=items.size()+1;
-            }
-            sb.append(count-1).append("\n");
-        }
-        System.out.println(sb.toString());
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    while(T-->0){
+      int N = Integer.parseInt(br.readLine());
+      Map<String, Integer> map = new HashMap<>();
+      while(N-->0){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b= st.nextToken();
+        map.put(b,map.getOrDefault(b,0)+1);
+      }
+
+      int result = 1;
+      for(int n : map.values()){
+        result*=(n+1);
+      }
+      sb.append(result-1).append("\n");
     }
+    System.out.println(sb);
+  }
 }
