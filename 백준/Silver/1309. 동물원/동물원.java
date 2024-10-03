dcde432d8b466e6a14c68ebfee620f2beaa6d90c
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-03 11:35:59 +0900

    [Silver I] Title: 동물원, Time: 112 ms, Memory: 17052 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,15 +1,20 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];
-        dp[0] = 1;
-        dp[1] = 3;
-        for(int i=2; i<=N; i++){
-            dp[i] = (dp[i-1]*2+dp[i-2])%9901;
-        }
-        System.out.println(dp[N]);
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    int[][] dp = new int[100001][2];
+    dp[1][0] = 2;
+    dp[1][1] = 1;
+    for(int i=2; i<=N; i++){
+      dp[i][0] = (dp[i-1][0]+dp[i-1][1]*2)%9901;
+      dp[i][1] = (dp[i-1][0]+dp[i-1][1])%9901;
     }
+    System.out.println((dp[N][0]+dp[N][1])%9901);
+  }
 }
