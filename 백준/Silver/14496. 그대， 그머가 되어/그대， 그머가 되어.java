ecf29a7c706d5c44a6d7836cdc671c2d37139aa9
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-04 09:18:41 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 160 ms, Memory: 19416 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.java
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.java
@@ -1,10 +1,16 @@
+import com.sun.jdi.ArrayReference;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+  private static List<Integer>[] adj;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,34 +23,42 @@ public class Main {
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
 
-    int[][] adj = new int[n + 1][n + 1];
+    adj = new ArrayList[n + 1];
     for (int i = 1; i <= n; i++) {
-      Arrays.fill(adj[i], 1000);
-      adj[i][i]=0;
+      adj[i] = new ArrayList<>();
     }
 
-    while(m-->0){
+    while (m-- > 0) {
       st = new StringTokenizer(br.readLine());
       int u = Integer.parseInt(st.nextToken());
       int v = Integer.parseInt(st.nextToken());
-      adj[u][v]=1;
-      adj[v][u]=1;
+      adj[u].add(v);
+      adj[v].add(u);
     }
+    System.out.println(bfs(a, b, n));
+  }
 
-    for(int k=1; k<=n; k++){
-      for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-          if(adj[i][j]>adj[i][k]+adj[k][j]){
-            adj[i][j]=adj[i][k]+adj[k][j];
-          }
+  private static int bfs(int a, int b, int n) {
+    if(a==b) return 0;
+    int[] vis = new int[n + 1];
+    Queue<Integer> q = new ArrayDeque<>();
+    q.offer(a);
+    vis[a] = 1;
+    while (!q.isEmpty()) {
+      int cur = q.poll();
+      for (int next : adj[cur]) {
+        if (vis[next] > 0) {
+          continue;
+        }
+        vis[next] = vis[cur] + 1;
+        q.offer(next);
+        if (next == b) {
+          return vis[next]-1;
         }
       }
     }
+    return -1;
 
-    if(adj[a][b]==1000){
-      System.out.println(-1);
-    }else{
-      System.out.println(adj[a][b]);
-    }
   }
 }
+
