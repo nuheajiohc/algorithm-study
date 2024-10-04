956706f22afc5e97e70704489192f54f125311ab
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-04 09:33:10 +0900

    [Silver III] Title: 파일 정리, Time: 644 ms, Memory: 59336 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    Map<String, Integer> map = new TreeMap<>();
+    while(N-->0){
+      String line = br.readLine();
+      StringTokenizer st = new StringTokenizer(line,".");
+      String name = st.nextToken();
+      String extension = st.nextToken();
+      map.put(extension, map.getOrDefault(extension,0)+1);
+    }
+
+    StringBuilder sb=  new StringBuilder();
+    for(String name : map.keySet()){
+      sb.append(name).append(" ").append(map.get(name)).append("\n");
+    }
+    System.out.println(sb);
+  }
+}
