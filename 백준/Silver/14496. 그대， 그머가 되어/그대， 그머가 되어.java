20b678c46e6fed7592cbb37f398210c20c0edde3
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-10-04 09:08:19 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 2868 ms, Memory: 27452 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.java
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.java
@@ -34,7 +34,6 @@ public class Main {
     for(int k=1; k<=n; k++){
       for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-          if(adj[i][j]==0) continue;
           if(adj[i][j]>adj[i][k]+adj[k][j]){
             adj[i][j]=adj[i][k]+adj[k][j];
           }
