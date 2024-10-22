e02fd4972e9a1ff40b0268f7052fb10ed63a7a75
jaehun <113998025+nuheajiohc@users.noreply.github.com>
2024-10-22 10:40:42 +0900

    [Bronze I] Title: 단어 공부, Time: 248 ms, Memory: 21136 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,32 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s=br.readLine().toUpperCase();
-        Map<Character,Integer> map = new HashMap<>();
-        for(int i=0; i<s.length();i++){
-            Character c = s.charAt(i);
-            if(!map.containsKey(c)){
-                map.put(c,1);
-            }else{
-                map.put(c,map.get(c)+1);
-            }
-        }
 
-        List<Map.Entry<Character,Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((a, b) -> b.getValue() - a.getValue());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line = br.readLine().toUpperCase();
 
-        if(list.size()==1){
-            System.out.println(list.get(0).getKey());
-            return;
-        }
-
-        if (Objects.equals(list.get(0).getValue(), list.get(1).getValue())){
-            System.out.println("?");
-        }else{
-            System.out.println(list.get(0).getKey());
-        }
+    int[] alpha = new int[26];
+    for(int i=0; i<line.length(); i++){
+      alpha[line.charAt(i)-'A']++;
     }
+
+    int maxCount=0;
+    int maxIndex=0;
+    for(int i=0; i<26; i++){
+      if(alpha[i]>alpha[maxIndex]){
+        maxCount = 1;
+        maxIndex = i;
+      }else if(alpha[i]==alpha[maxIndex]){
+        maxCount++;
+      }
+    }
+
+    if(maxCount>1){
+      System.out.println("?");
+    }else{
+      System.out.println((char)('A'+maxIndex));
+    }
+  }
 }
