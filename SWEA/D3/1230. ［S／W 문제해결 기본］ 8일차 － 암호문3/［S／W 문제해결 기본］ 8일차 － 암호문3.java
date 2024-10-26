b46e74edc5f58716a5e4a99ba1bf05df199f89b6
jaehun <113998025+nuheajiohc@users.noreply.github.com>
2024-10-26 14:39:08 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 180 ms, Memory: 32,044 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringBuilder sb = new StringBuilder();
+    for (int tc = 1; tc <= 10; tc++) {
+      int N = Integer.parseInt(br.readLine());
+      List<Integer> list = new LinkedList<>();
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < N; i++) {
+        list.add(Integer.parseInt(st.nextToken()));
+      }
+      int M = Integer.parseInt(br.readLine());
+      st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < M; i++) {
+        String I = st.nextToken();
+        if (I.equals("I")) {
+          int index = Integer.parseInt(st.nextToken());
+          int length = Integer.parseInt(st.nextToken());
+          List<Integer> nums = new LinkedList<>();
+          for (int j = 0; j < length; j++) {
+            nums.add(Integer.parseInt(st.nextToken()));
+          }
+          list.addAll(index, nums);
+        } else if (I.equals("D")) {
+          int index = Integer.parseInt(st.nextToken());
+          int length = Integer.parseInt(st.nextToken());
+          for (int j = 0; j < length; j++) {
+            list.remove(index);
+          }
+        } else {
+          int length = Integer.parseInt(st.nextToken());
+          for (int j = 0; j < length; j++) {
+            list.add(Integer.parseInt(st.nextToken()));
+          }
+        }
+      }
+
+      sb.append("#").append(tc).append(" ");
+
+      for (int i = 0; i < 10; i++) {
+        sb.append(list.get(i)).append(" ");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+}
