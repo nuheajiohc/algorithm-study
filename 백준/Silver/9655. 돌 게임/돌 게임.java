15a6236e20bacdf8315aba0631211f19c2b487ca
jaehun <113998025+nuheajiohc@users.noreply.github.com>
2024-10-28 10:25:32 +0900

    [Silver V] Title: 돌 게임, Time: 116 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -3,20 +3,8 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        recursion(N,0);
-    }
 
-    public static void recursion(int n, int count){
-        if(n==0 && count%2==0){
-            System.out.println("CY");
-            return;
-        }
-        if(n==0 && count%2==1){
-            System.out.println("SK");
-            return;
-        }
-        if(n>=3) recursion(n-3,count+1);
-        else recursion(n-1,count+1);
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N%2==1?"SK":"CY");
     }
 }
