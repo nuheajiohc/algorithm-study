22bd4f47d9cd4b41890079dbb53659a00fb2a792
jaehun <113998025+nuheajiohc@users.noreply.github.com>
2024-10-29 12:16:04 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -5,49 +5,42 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] switches = new int[N+1];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=N;i++){
-            switches[i] = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+1];
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int P = Integer.parseInt(br.readLine());
-        for(int i=0; i<P; i++){
+
+        int M = Integer.parseInt(br.readLine());
+        while(M-->0){
             st = new StringTokenizer(br.readLine());
             int gender = Integer.parseInt(st.nextToken());
             int pos = Integer.parseInt(st.nextToken());
-            if(gender == 1){
-                int count=1;
-                while(count*pos<=N){
-                    switches[count*pos] = switches[count*pos]==1 ? 0 :1;
-                    count++;
+            if(gender==1){
+                for(int i=pos; i<=N; i+=pos){
+                    arr[i] = (arr[i]+1)%2;
                 }
             }else{
-                int count=1;
-                switches[pos]=switches[pos]==1?0:1;
-                while(pos-count>0 && pos+count<=N){
-                    if(switches[pos-count]==switches[pos+count]){ 
-                        if(switches[pos-count]==1){
-                            switches[pos-count]=0;
-                            switches[pos+count]=0;
-                        }else{
-                            switches[pos-count]=1;
-                            switches[pos+count]=1;
-                        }
-                        count++;
-                    }else{
-                        break;
-                    }
+                arr[pos] = (arr[pos]+1)%2;
+                int temp=1;
+                while(pos+temp<=N && pos-temp>0 && arr[pos+temp]==arr[pos-temp]){
+                    arr[pos+temp] = (arr[pos+temp]+1)%2;
+                    arr[pos-temp] = (arr[pos-temp]+1)%2;
+                    temp++;
                 }
             }
         }
-
         StringBuilder sb = new StringBuilder();
-        for(int i=1;i<=N;i++){
-            sb.append(switches[i]).append(" ");
-            if(i%20==0){
+        int count=1;
+        for(int i=1; i<=N; i++){
+            if(count>20){
                 sb.append("\n");
-            }
+                count=1;
+            } 
+            sb.append(arr[i]).append(" ");
+            count++;
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
