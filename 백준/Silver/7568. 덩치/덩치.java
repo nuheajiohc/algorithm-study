d2394f8eab2b5c1c9bd4a9f385d4364ecf44b749
jaehun <113998025+nuheajiohc@users.noreply.github.com>
2024-10-29 09:41:23 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int[][] arr = new int[N][2];
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            arr[i] = new int[]{w,h};
+        }
+
+        for(int i=0; i<N; i++){
+            int rank=1;
+            for(int j=0; j<N; j++){
+                if(i==j) continue;
+                if(arr[i][0]<arr[j][0] && arr[i][1]<arr[j][1]){
+                    rank++;
+                }
+            }
+            sb.append(rank).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
