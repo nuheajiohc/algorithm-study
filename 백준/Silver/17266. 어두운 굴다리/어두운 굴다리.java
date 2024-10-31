e388a46e2c5ca4505e73ff9181051121b99f7261
jaehun <113998025+nuheajiohc@users.noreply.github.com>
2024-10-31 11:33:04 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 272 ms, Memory: 25112 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -1,38 +1,23 @@
 import java.io.*;
 import java.util.*;
 
-public class Main { 
-
-    private static List<Integer> list = new ArrayList<>();
-    private static int N,M;
+public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        M = Integer.parseInt(br.readLine());
+        int[] arr = new int[M];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<M; i++){
-            list.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int s = -1;
-        int e=100000;
-        while(s+1<e){
-            int mid = (s+e)/2;
-            if(!check(mid)) s=mid;
-            else e=mid;
+        int minHeight = Math.max(arr[0]-0, N-arr[M-1]);
+        for(int i=1; i<M; i++){
+            minHeight = Math.max((int)Math.ceil((arr[i]-arr[i-1])/(double)2),minHeight);
         }
-        System.out.println(e);
-    }
-
-    public static boolean check(int target){
-        if(list.get(0)-target>0) return false;
-        if(list.get(M-1)+target<N) return false;
-
-        for(int i=1; i<list.size()-1; i++){
-            if(list.get(i)-list.get(i-1)>target*2) return false;
-        }
-        return true;
+        System.out.println(minHeight);
     }
 }
