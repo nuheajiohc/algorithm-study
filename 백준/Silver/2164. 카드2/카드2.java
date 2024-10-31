e2c2c60f76269e81b9867767f09128db61e879e2
jaehun <113998025+nuheajiohc@users.noreply.github.com>
2024-10-31 11:44:27 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 28848 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,18 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> q = new ArrayDeque<>();
+
+        Queue<Integer> queue = new ArrayDeque<>();
         for(int i=1; i<=N; i++){
-            q.offerLast(i);
+            queue.offer(i);
         }
 
-        while(q.size()>1){
-            q.pollFirst();
-            q.offerLast(q.pollFirst());
+        while(queue.size()>1){
+            queue.poll();
+            queue.offer(queue.poll());
         }
-        System.out.println(q.poll());
+        System.out.println(queue.poll());
     }
 }
