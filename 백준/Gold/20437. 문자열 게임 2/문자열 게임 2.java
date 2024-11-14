6e55c4b8b72d05f3aa27a1766992587b77dbc7e0
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-11-14 14:56:26 +0900

    [Gold V] Title: 문자열 게임 2, Time: 512 ms, Memory: 20640 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -21,34 +21,25 @@ public class Main {
                 continue;
             }
 
-            if(!hasSequence(alpha, K)){
-                sb.append(-1);
-            }else{
-                for(int i=0; i<W.length(); i++){
-                    if(alpha[W.charAt(i)-'a']<K) continue;
-                    int count=1;
-                    for(int j=i+1; j<W.length(); j++){
-                        if(W.charAt(i)==W.charAt(j)){
-                            count++;
-                        }
-                        if(count==K){
-                            min = Math.min(min, j-i+1);
-                            max = Math.max(max, j-i+1);
-                            break;
-                        }
+            for(int i=0; i<W.length(); i++){
+                if(alpha[W.charAt(i)-'a']<K) continue;
+                int count=1;
+                for(int j=i+1; j<W.length(); j++){
+                    if(W.charAt(i)==W.charAt(j)){
+                        count++;
+                    }
+                    if(count==K){
+                        min = Math.min(min, j-i+1);
+                        max = Math.max(max, j-i+1);
+                        break;
                     }
                 }
-                sb.append(min).append(" ").append(max);
             }
-            sb.append("\n");
+            if(max!=-1){
+                sb.append(min).append(" ");
+            }
+            sb.append(max).append("\n");
         }
         System.out.println(sb);
-    }
-
-    public static boolean hasSequence(int[]alpha, int K){
-        for(int i=0; i<26; i++){
-            if(alpha[i]>=K) return true;
-        }
-        return false;
     }
 }
