b4fc3924e091820e0e0bfcb8964049d7e108987f
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-11-15 13:56:58 +0900

    [Gold V] Title: 택배 배송, Time: 368 ms, Memory: 39508 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        List<Node>[] adj = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adj[u].add(new Node(v, w));
+            adj[v].add(new Node(u, w));
+        }
+
+        int[] d = new int[V + 1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        d[1] = 0;
+        pq.add(new Node(1, 0));
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            if(d[cur.vertex] != cur.weight) continue;
+
+            for(Node next : adj[cur.vertex]){
+                if(d[next.vertex] <= d[cur.vertex]+next.weight) continue;
+                d[next.vertex]=d[cur.vertex]+next.weight;
+                pq.offer(new Node(next.vertex,d[next.vertex]));
+            }
+        }
+
+        System.out.println(d[V]);
+    }
+
+    public static class Node implements Comparable<Node> {
+        int vertex;
+        int weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(weight, o.weight);
+        }
+    }
+}
