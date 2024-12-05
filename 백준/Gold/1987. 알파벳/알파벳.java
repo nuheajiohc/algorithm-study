691b636df1791b990518eb4c6d227a56871b39c6
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2024-12-05 11:39:26 +0900

    [Gold IV] Title: 알파벳, Time: 916 ms, Memory: 15324 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,45 +3,46 @@ import java.util.*;
 
 public class Main {
 
-  private static int R,C;
+  private static int R, C;
   private static char[][] board;
   private static boolean[] used;
-  private static int[] dx = {0,0,1,-1};
-  private static int[] dy = {1,-1,0,0};
-  public static void main(String[] args) throws Exception{
+  private static int[] dx = {0, 0, 1, -1};
+  private static int[] dy = {1, -1, 0, 0};
+
+  public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     R = Integer.parseInt(st.nextToken());
     C = Integer.parseInt(st.nextToken());
 
     board = new char[R][C];
-    for(int i=0; i<R; i++){
+    for (int i = 0; i < R; i++) {
       board[i] = br.readLine().toCharArray();
     }
 
     used = new boolean[26];
-    int count= btk(0,0,0);
-    System.out.println(count);
-
+    int result = btk(0, 0);
+    System.out.println(result);
   }
 
-  public static int btk(int tempCount, int x, int y){
-    if(used[board[x][y]-'A']){
-      return tempCount;
+  public static int btk(int x, int y) {
+    used[board[x][y] - 'A'] = true;
+    int maxCount = 0;
+
+    for (int dir = 0; dir < 4; dir++) {
+      int nx = x + dx[dir];
+      int ny = y + dy[dir];
+
+      if (isInBounds(nx, ny) && !used[board[nx][ny] - 'A']) {
+        maxCount = Math.max(maxCount, btk(nx, ny));
+      }
     }
-    used[board[x][y]-'A']=true;
-    int count = tempCount+1;
-    for(int dir=0; dir<4; dir++){
-      int nx = x+dx[dir];
-      int ny = y+dy[dir];
-      if(isOutOfRange(nx,ny)) continue;
-      count = Math.max(count, btk(tempCount+1, nx, ny));
-    }
-    used[board[x][y]-'A']=false;
-    return count;
+
+    used[board[x][y] - 'A'] = false;
+    return maxCount + 1;
   }
 
-  public static boolean isOutOfRange(int x, int y){
-    return x<0 || x>=R || y<0 || y>=C;
+  public static boolean isInBounds(int x, int y) {
+    return x >= 0 && x < R && y >= 0 && y < C;
   }
 }
