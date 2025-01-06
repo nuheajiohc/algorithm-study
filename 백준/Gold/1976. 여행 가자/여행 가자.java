1263cfb4a1bfabc4ee446035dd6a0c8d9dd724e7
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2025-01-06 15:22:24 +0900

    [Gold IV] Title: 여행 가자, Time: 156 ms, Memory: 17920 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -2,27 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+  private static int[] parent;
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
     int M = Integer.parseInt(br.readLine());
-    int[][] conn = new int[N+1][N+1];
+
+
+    parent = new int[N+1];
+
+    for(int i=1; i<=N; i++){
+      parent[i]=i;
+    }
+    
     for(int i=1; i<=N; i++){
       StringTokenizer st = new StringTokenizer(br.readLine());
       for(int j=1; j<=N; j++){
-        conn[i][j] = Integer.parseInt(st.nextToken());
-      }
-    }
-    for(int i=1; i<=N; i++){
-      conn[i][i]=1;
-    }
-
-    for(int i=1; i<=N; i++){
-      for(int j=1; j<=N; j++){
-        for(int k=1; k<=N; k++){
-          if(conn[j][i]==1 && conn[i][k]==1){
-            conn[j][k]=1;
-          }
+        int connected = Integer.parseInt(st.nextToken());
+        if(connected==1){
+          union(i,j);
         }
       }
     }
@@ -33,18 +32,29 @@ public class Main {
       city[i] = Integer.parseInt(st.nextToken());
     }
 
-
-    boolean isPossible= true;
+    boolean isPossible = true;
+    int root = find(city[0]);
     for(int i=1; i<M; i++){
-      if(conn[city[i-1]][city[i]]!=1){
+      if(find(city[i]) != root){
         isPossible = false;
         break;
       }
     }
-    if(isPossible){
-      System.out.println("YES");
-    }else{
-      System.out.println("NO");
+    System.out.println(isPossible?"YES":"NO");
+  }
+
+  private static int find(int x){
+    if(parent[x] != x){
+      parent[x] = find(parent[x]);
+    }
+    return parent[x];
+  }
+
+  private static void union(int a, int b){
+    int rootA = find(a);
+    int rootB = find(b);
+    if(rootA!=rootB){
+      parent[rootB] = rootA;
     }
   }
 }
