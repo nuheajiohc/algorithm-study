930738266aae2399bbd5ed0df81431dfc85a89ee
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2025-02-19 19:54:53 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -5,21 +5,28 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N ;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        int[] arr = new int[N];
 
-        List<Integer> list = new LinkedList<>();
-        for(int i=N-1; i>=0; i--){
-            list.add(arr[i],i+1);
+        for(int i=0; i<N; i++){
+            int x = Integer.parseInt(st.nextToken());
+            for(int j=0; j<N; j++){
+                if(arr[j]==0){
+                    if(x==0){
+                        arr[j]= i+1;
+                        break;
+                    }else{
+                        x--;
+                    }
+                }
+            }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=0 ;i<N; i++){
-            sb.append(list.get(i)).append(" ");
+        for(int i =0; i<N; i++){
+            sb.append(arr[i]).append(" ");
         }
         System.out.println(sb);
+
     }
 }
