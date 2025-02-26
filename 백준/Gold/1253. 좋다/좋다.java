78641f0cfecbbec9f83104743d10de71cece009a
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2025-02-26 21:03:47 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -3,40 +3,42 @@ import java.util.*;
 
 public class Main {
 
-    public static int[] A;
-    public static int N;
-    public static int count=0;
-    public static void main(String[] args) throws Exception {
+    private static int[] arr;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        A = new int[N];
+        arr = new int[N];
         for(int i=0; i<N; i++){
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A);
-        for(int i=0; i<N; i++){
-            checkSum(i);
+        Arrays.sort(arr);
+        
+        int count=0;
+        
+        for(int t=0; t<N; t++){
+            int l = 0;
+            int r = N-1;
+            while(l<r){
+                if(l == t){
+                    l++;
+                    continue;
+                }
+                if(r==t){
+                    r--;
+                    continue;
+                }
+                if(arr[l]+arr[r]<arr[t]){
+                    l++;
+                }else if(arr[l]+arr[r]>arr[t]){
+                    r--;
+                }else{
+                    count++;
+                    break;
+                }
+            }
         }
         System.out.println(count);
-    }
-
-    public static void checkSum(int i){
-        for(int j=0; j<i; j++){
-            if(Arrays.binarySearch(A,j+1,i,A[i]-A[j])>=0){
-                count++;
-                return;
-            }
-            if(Arrays.binarySearch(A,i+1,N,A[i]-A[j])>=0){
-                count++;
-                return;
-            }
-        }
-        for(int j=i+1; j<N; j++){
-            if(Arrays.binarySearch(A,j+1,N,A[i]-A[j])>=0){
-                count++;
-                return;
-            }
-        }
-    }
+    } 
 }
