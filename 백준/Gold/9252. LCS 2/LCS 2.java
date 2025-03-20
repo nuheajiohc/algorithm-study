57a1147d15a631f9c51a933ecaffac37ac27d2c7
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2025-03-20 17:41:52 +0900

    [Gold IV] Title: LCS 2, Time: 132 ms, Memory: 18432 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,48 +1,46 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-
-    private static int maxValue;
-    private static int[][] dp;
-    private static String ss1,ss2;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ss1 = br.readLine();
-        ss2 = br.readLine();
+        
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+        
+        int[][] board = new int[str1.length+1][str2.length+1];
 
-        dp = new int[ss1.length()+1][ss2.length()+1];
-
-        maxValue = 0;
-        for(int i=1; i<=ss1.length(); i++){
-            for(int j=1; j<=ss2.length(); j++){
-                if(ss1.charAt(i-1) == ss2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-                maxValue = Math.max(dp[i][j],maxValue);
-            }
-        }
-        System.out.println(maxValue);
-        if(maxValue!=0){
-            int count = maxValue-1;
-            int x=ss1.length();
-            int y=ss2.length();
-            char[] arr = new char[maxValue];
-            while(x>0 && y>0){
-                if(ss1.charAt(x-1)==ss2.charAt(y-1)){
-                    arr[count] = ss1.charAt(x-1);
-                    count--;
-                    x--;
-                    y--;
-                }else if(dp[x][y] == dp[x-1][y]){
-                    x--;
-                }else{
-                    y--;
+        for(int i=1; i<=str1.length; i++){
+            for(int j=1; j<=str2.length; j++){
+                board[i][j] = Math.max(board[i-1][j], board[i][j-1]);
+                if(str1[i-1] == str2[j-1]){
+                    board[i][j] = Math.max(board[i][j],board[i-1][j-1]+1);
                 }
             }
-            System.out.println(String.valueOf(arr));
+        } 
+
+        StringBuilder sb = new StringBuilder();
+        
+        int max = board[str1.length][str2.length];
+        System.out.println(max);
+        if(max==0){
+            return;
         }
+
+        int x = str1.length-1;
+        int y = str2.length-1;
+
+        while(x>=0 && y>=0){
+            if(str1[x]==str2[y]){
+                sb.append(str1[x]);
+                x--;
+                y--;
+            } else if(board[x+1][y+1] == board[x][y+1]){
+                x--;
+            } else{
+                y--;
+            }
+        }
+        
+        System.out.println(sb.reverse());
     }
 }
