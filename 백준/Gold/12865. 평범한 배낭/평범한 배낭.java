ea5645d1665168e86d0047255c6748e9a2c3abdd
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2025-03-21 18:03:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 53792 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -2,24 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        long[][] dp = new long[N+1][K+1];
-        for(int i=0; i<N; i++){
+        Bag[] bags = new Bag[N+1];
+        for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
-            int W = Integer.parseInt(st.nextToken());
-            int V = Integer.parseInt(st.nextToken());
-
-            for(int j=1; j<K+1; j++){
-                if(W>j) dp[i+1][j] = dp[i][j];
-                else dp[i+1][j] = Math.max(dp[i][j],dp[i][j-W]+V);
-            }
+            bags[i] = new Bag(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        System.out.println(dp[N][K]);
+
+        int[][] board = new int[N+1][K+1];
+    
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=K; j++){
+                if(j>=bags[i].w) {
+                    board[i][j] = Math.max(board[i-1][j], board[i-1][j-bags[i].w]+bags[i].v);
+                }else{
+                    board[i][j] = board[i-1][j];
+                }
+            }
+        }
+        System.out.println(board[N][K]);
+    }
+
+    public static class Bag implements Comparable<Bag>{
+        int w;
+        int v;
+
+        public Bag(int w, int v){
+            this.w = w;
+            this.v = v;
+        }
+
+        public int compareTo(Bag o){
+            return o.w-this.w;
+        }   
     }
 }
