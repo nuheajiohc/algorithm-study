508c76cbe78cc9e8d77e3ffef761dd884d735ccb
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2025-04-30 16:29:30 +0900

    [level 2] Title: 튜플, Time: 93.10 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 97.1 MB, 시간: 76.04 ms
+메모리: 94.8 MB, 시간: 93.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 30일 16:10:46
+2025년 04월 30일 16:29:28
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,48 +1,31 @@
-// import java.util.*;
-
-// class Solution {
-//     public int[] solution(String s) {
-//         int[] count = new int[100001];
-        
-//         String[] splitted = s.substring(2,s.length()-2).split("\\},\\{");
-//         for(String str : splitted){
-//             StringTokenizer st = new StringTokenizer(str, ",");
-//             while(st.hasMoreTokens()){
-//                 count[Integer.valueOf(st.nextToken())]++;
-//             }
-//         }
-        
-//         List<int[]> list = new ArrayList<>();
-//         for(int i=1; i<100001; i++){
-//             if(count[i]>0){
-//                 list.add(new int[]{i, count[i]});
-//             }
-//         }
-        
-//         list.sort((a,b)->b[1]-a[1]);
-        
-//         int[] answer = new int[list.size()];
-//         for(int i=0; i<list.size(); i++){
-//             answer[i] = (list.get(i))[0];
-//         }
-        
-//         return answer;
-//     }
-// }
-
 import java.util.*;
+
 class Solution {
     public int[] solution(String s) {
-        Set<String> set = new HashSet<>();
-        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
-        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
-        int[] answer = new int[arr.length];
-        int idx = 0;
-        for(String s1 : arr) {
-            for(String s2 : s1.split(",")) {
-                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+        Map<Integer ,Integer> count = new HashMap<>();
+        
+        String[] splitted = s.substring(2,s.length()-2).split("\\},\\{");
+        for(String str : splitted){
+            StringTokenizer st = new StringTokenizer(str, ",");
+            while(st.hasMoreTokens()){
+                int value = Integer.valueOf(st.nextToken());
+                if(count.containsKey(value)){
+                    count.put(value, count.get(value)+1);
+                }else{
+                    count.put(value, 1);
+                }
             }
         }
+        
+        List<int[]> list = new ArrayList<>();
+        count.forEach((key, value)-> list.add(new int[]{key, value}));
+        list.sort((a,b)->b[1]-a[1]);
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = (list.get(i))[0];
+        }
+        
         return answer;
     }
 }
