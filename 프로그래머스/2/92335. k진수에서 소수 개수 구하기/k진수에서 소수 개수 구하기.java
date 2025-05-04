62b23285470e3bd0a72c33cf775c28269ab1c523
최재훈 <113998025+nuheajiohc@users.noreply.github.com>
2025-05-04 14:09:40 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 17.76 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        String converted = Integer.toString(n, k);
+        String[] arr = converted.split("0",-1);
+
+        for(String tmp : arr){
+            if(tmp.equals("")) continue;
+            Long number = Long.valueOf(tmp);
+            if(isPrime(number)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    private boolean isPrime(long number){
+        if(number==1) return false;
+        if(number==2) return true;
+        for(long i=2; i<=(long)Math.sqrt(number); i++){
+            if(number%i==0) return false;
+        }
+        return true;
+        
+    }
+}
