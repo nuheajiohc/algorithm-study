761e2557bc58e9e24147b45c771b7b89945f8110
JAEHUN <113998025+nuheajiohc@users.noreply.github.com>
2025-09-09 09:33:15 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,14 +1,16 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        long[] dp = new long[N+1];
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[91];
         dp[1] = 1;
-        for(int i =2; i<=N; i++){
+        dp[2] = 1;
+        for(int i=3; i<=n; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
-        System.out.println(dp[N]);
+        System.out.println(dp[n]);
     }
 }
