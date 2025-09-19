458ad481ebba894f8ca3852832678f9ee10b186c
JAEHUN <113998025+nuheajiohc@users.noreply.github.com>
2025-09-19 14:15:46 +0900

    [Gold V] Title: 부분 삼각 수열, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/1548. 부분 삼각 수열/부분 삼각 수열.java
+++ 백준/Gold/1548. 부분 삼각 수열/부분 삼각 수열.java
@@ -8,30 +8,29 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
         arr = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int ans = Math.min(N, 2);
-
         Arrays.sort(arr);
 
-        int r = 2;
-        for(int l=0; l<N; l++){
-            if(l+1>=N) break;
-            if(r<l+2) r = l+2;
-
-            int sum = arr[l] + arr[l+1];
-            while(r<N && sum > arr[r]){
-                r++;
-            }
-        
-            ans = Math.max(ans, r-l);
-
+        if(arr.length<3){
+            System.out.println(arr.length);
+            return;
         }
-        System.out.println(ans);
+
+        int maximum = 2;
+        for(int i=0; i<N; i++){
+            for(int j=i+2; j<N; j++){
+                if(arr[i]+arr[i+1]>arr[j]){
+                    maximum = Math.max(maximum, j-i+1);
+                }else{
+                    break;
+                }
+            }    
+        }
+        System.out.println(maximum);
     }
 }
