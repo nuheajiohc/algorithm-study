73af1ecf8248439234194bd19c8c9b08d6304359
JAEHUN <113998025+nuheajiohc@users.noreply.github.com>
2025-11-02 15:56:37 +0900

    [Silver III] Title: 블로그, Time: 308 ms, Memory: 34948 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -2,41 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
+
+        int[] visitors = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i =0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++){
+            visitors[i] = Integer.parseInt(st.nextToken());
         }
 
-        int temp=0;
-        int max=0;
-        int maxCount=1;
-        for(int i=-0; i<X; i++){
-            max += arr[i];
+        int cur = 0;
+        int max = 0;
+        int count = 1;
+
+        for(int i=0; i<X; i++){
+            cur += visitors[i];
         }
-        temp = max;
+        max = cur;
 
         for(int i=X; i<N; i++){
-            temp+=arr[i];
-            temp-=arr[i-X];
-            if(max<temp){
-                max=temp;
-                maxCount=1;
-            }else if(max==temp){
-                maxCount++;
+            cur += visitors[i];
+            cur -= visitors[i-X];
+
+            if(max < cur){
+                max = cur;
+                count = 1;
+            }else if(max == cur){
+                count++;
             }
+            
         }
 
         if(max==0){
             System.out.println("SAD");
         }else{
             System.out.println(max);
-            System.out.println(maxCount);
+            System.out.println(count);
         }
+
     }
 }
