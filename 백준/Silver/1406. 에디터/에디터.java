d3b3edf5aac39a9c96c2b7cfc95e288695fdf43c
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-01-22 20:20:19 +0900

    [Silver II] Title: 에디터, Time: 412 ms, Memory: 73648 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,54 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int M = Integer.parseInt(br.readLine());
+		
+		Deque<Character> stack1 = new ArrayDeque<>();
+		Deque<Character> stack2 = new ArrayDeque<>();
+		
+		for(int i=0; i<str.length(); i++) {
+			stack1.push(str.charAt(i));
+		}
+		
+		while(M-->0) {
+			String line = br.readLine();
+			
+			char command = line.charAt(0);
+			if(line.length()==1) {
+				switch(command) {
+					case 'L':
+						if(!stack1.isEmpty()) {
+							stack2.push(stack1.pop());
+						}
+						break;
+					case 'D':
+						if(!stack2.isEmpty()) {
+							stack1.push(stack2.pop());
+						}
+						break;
+					case 'B':
+						if(!stack1.isEmpty()) {
+							stack1.pop();
+						}
+						break;
+				}
+			} else {
+				char target = line.charAt(2);
+				stack1.push(target);
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		while(!stack1.isEmpty()) {
+			sb.append(stack1.pollLast());
+		}
+		while(!stack2.isEmpty()) {
+			sb.append(stack2.pollFirst());
+		}
+		System.out.println(sb);
+	}
 
-    Deque<String> deque1 = new ArrayDeque<>();
-    Deque<String> deque2 = new ArrayDeque<>();
-
-    String str = br.readLine();
-    for(int i = 0; i < str.length(); i++){
-      deque1.offer(String.valueOf(str.charAt(i)));
-    }
-
-    int M = Integer.parseInt(br.readLine());
-    while(M-->0){
-      String line = br.readLine();
-      StringTokenizer st = new StringTokenizer(line);
-      String command = st.nextToken();
-      if(command.equals("P")){
-        String alpha = st.nextToken();
-        deque1.offerLast(alpha);
-      }else{
-        if(command.equals("L")){
-          if(deque1.isEmpty()) continue;
-          String temp = deque1.pollLast();
-          deque2.offerFirst(temp);
-        }else if(command.equals("D")){
-          if(deque2.isEmpty()) continue;
-          String temp = deque2.pollFirst();
-          deque1.offerLast(temp);
-        }else{
-          if(deque1.isEmpty()) continue;
-          deque1.pollLast();
-        }
-      }
-    }
-
-    StringBuilder sb = new StringBuilder();
-    while(!deque1.isEmpty()){
-      sb.append(deque1.pollFirst());
-    }
-    while(!deque2.isEmpty()){
-      sb.append(deque2.pollFirst());
-    }
-    System.out.println(sb);
-  }
 }
