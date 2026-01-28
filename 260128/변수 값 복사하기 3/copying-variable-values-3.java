e2ac10c972f12e50df75b19e2fabb64eaf84199d
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-01-28 22:09:57 +0900

    Title: 변수 값 복사하기 3, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260128/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        int c = 3;
+
+        a = c;
+        a = a+c;
+        b = b-c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
