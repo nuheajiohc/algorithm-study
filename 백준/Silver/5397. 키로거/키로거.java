a3a38f539ae676ade17a115cf11eea767c1f14ed
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-01-28 16:41:13 +0900

    [Silver II] Title: 키로거, Time: 596 ms, Memory: 100664 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,47 +1,50 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int T = Integer.parseInt(br.readLine());
-
-    StringBuilder sb = new StringBuilder();
-    while(T-->0){
-      String line = br.readLine();
-      Deque<Character> queue1 = new ArrayDeque<>();
-      Deque<Character> queue2 = new ArrayDeque<>();
-      for(int i = 0; i < line.length(); i++){
-        char c = line.charAt(i);
-        if(c == '<'){
-          if(!queue1.isEmpty()){
-            queue2.offerFirst(queue1.pollLast());
-          }
-        }else if(c== '>'){
-          if(!queue2.isEmpty()){
-            queue1.offerLast(queue2.pollFirst());
-          }
-        }else if(c=='-'){
-          queue1.pollLast();
-        }else{
-          queue1.offerLast(line.charAt(i));
-        }
-      }
-
-      while(!queue1.isEmpty()){
-        sb.append(queue1.pollFirst());
-      }
-      while(!queue2.isEmpty()){
-        sb.append(queue2.pollFirst());
-      }
-      sb.append('\n');
-    }
-    System.out.println(sb);
-  }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(N-->0) {
+			String command = br.readLine();
+			
+			Deque<Character> deque1 = new ArrayDeque<>();
+			Deque<Character> deque2 = new ArrayDeque<>();
+			
+			for(char c : command.toCharArray()) {
+				switch(c) {
+					case '<':
+						if(!deque1.isEmpty()) {
+							deque2.offerFirst(deque1.pollLast());
+						}
+						break;
+					case '>':
+						if(!deque2.isEmpty()) {
+							deque1.offerLast(deque2.pollFirst());
+						}
+						break;
+					case '-':
+						if(!deque1.isEmpty()) {
+							deque1.pollLast();
+						}
+						break;
+					default:
+						deque1.offerLast(c);
+				}
+			}
+			for(char c : deque1) {
+				sb.append(c);
+			}
+			
+			for(char c : deque2) {
+				sb.append(c);
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
