62381815342f75679d4252c525341d2e971a39a4
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-01-29 13:45:47 +0900

    [D3] Title: 두 수의 덧셈, Time: 101 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/3260. 두 수의 덧셈/두 수의 덧셈.java
+++ SWEA/D3/3260. 두 수의 덧셈/두 수의 덧셈.java
@@ -1,44 +1,20 @@
 import java.io.*;
+import java.math.*;
 import java.util.*;
 
 public class Solution {
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		StringBuilder sb = new StringBuilder();
-		for(int t=1; t<=T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String a = st.nextToken();
-			String b = st.nextToken();
-			
-			StringBuilder result = new StringBuilder();
-			
-			int length = Math.max(a.length(), b.length());
-			
-			int next = 0;
-			for(int i=0; i<length; i++) {
-				int aa=0;
-				int bb=0;
-				if(a.length()>i) {
-					aa = a.charAt(a.length()-1-i)-'0';
-				}
-				
-				if(b.length()>i) {
-					bb = b.charAt(b.length()-1-i)-'0';
-				}
-				
-				int sum = aa+bb;
-				result.append((sum+next)%10);
-				next = (sum+next)/10;
-			}
-			if(next>0) {
-				result.append(next);
-			}
-			sb.append("#").append(t).append(" ").append(result.reverse()).append("\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+    	
+        StringBuilder sb = new StringBuilder();
+        for(int t=1; t<=T; t++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	BigInteger a = new BigInteger(st.nextToken());
+        	BigInteger b = new BigInteger(st.nextToken());
+        	
+        	sb.append("#").append(t).append(" ").append(a.add(b)).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
