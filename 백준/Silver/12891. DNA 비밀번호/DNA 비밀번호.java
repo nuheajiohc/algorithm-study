1e1a794d0f2081adf6bae76fc68d29c50d632549
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-02 13:49:23 +0900

    [Silver II] Title: DNA 비밀번호, Time: 252 ms, Memory: 22028 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -3,37 +3,46 @@ import java.util.*;
 
 public class Main {
 
-    private static int[] alpha;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        int count=0;
-        String str = br.readLine();
-        int[] alphabet = new int[26];
-        for(int i=0; i<P; i++){
-            alphabet[str.charAt(i)-'A']++;
-        }
-        st =new StringTokenizer(br.readLine());
-        int[] partStr = new int[4];
-        for(int i=0; i<4; i++){
-            partStr[i] = Integer.parseInt(st.nextToken());
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int start = 0;
-        int end = P-1;
-        while(end<S){
-            if(start>0){
-                alphabet[str.charAt(start-1)-'A']--;
-                alphabet[str.charAt(end)-'A']++;
-            }
-            if(alphabet['A'-'A']>=partStr[0] && alphabet['C'-'A']>=partStr[1] && alphabet['G'-'A']>=partStr[2] && alphabet['T'-'A']>=partStr[3]){
-                count++;
-            }
-            start++;
-            end++;
-        }
-        System.out.println(count);
-    }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int S = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		
+		char[] DNA = br.readLine().toCharArray();
+	
+		st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int G = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+		
+		for(int i=0; i<P-1; i++) {
+			if(DNA[i]=='A') A--;
+			else if(DNA[i]=='C') C--;
+			else if(DNA[i]=='G') G--;
+			else T--;
+		}
+		
+		int count=0;
+		
+		for(int i=0; i<=S-P; i++) {
+			if(DNA[i+P-1]=='A') A--;
+			else if(DNA[i+P-1]=='C') C--;
+			else if(DNA[i+P-1]=='G') G--;
+			else T--;
+			
+			if(A<=0 && C<=0 && G<=0 && T<=0) {
+				count++;
+			}
+			
+			if(DNA[i]=='A') A++;
+			else if(DNA[i]=='C') C++;
+			else if(DNA[i]=='G') G++;
+			else T++;
+		}
+		System.out.println(count);
+	}
 }
