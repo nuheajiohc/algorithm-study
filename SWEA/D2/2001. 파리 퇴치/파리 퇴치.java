be8a23815560cee52c91b7e05292a8d6a5fd94c9
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-02 12:15:14 +0900

    [D2] Title: 파리 퇴치, Time: 79 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,44 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int t=1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int[][] board = new int[N][N];
+			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0; j<N; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			int max = 0;
+			
+			for(int i=0; i<=N-M; i++) {
+				for(int j=0; j<=N-M; j++) {
+					int tmp = 0;
+					for(int k=i; k<i+M; k++) {
+						for(int l=j; l<j+M; l++) {
+							tmp += board[k][l];
+						}
+					}
+					max = Math.max(max, tmp);
+				}
+			}
+			
+			sb.append("#").append(t).append(" ").append(max).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 
-    StringBuilder sb = new StringBuilder();
-    for(int t=1; t<=T; t++) {
-      sb.append("#").append(t).append(" ");
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int N = Integer.parseInt(st.nextToken());
-      int M = Integer.parseInt(st.nextToken());
-
-      int[][] arr = new int[N][N];
-      for(int i=0; i<N; i++){
-        st = new StringTokenizer(br.readLine());
-        for(int j=0; j<N; j++) {
-          arr[i][j] = Integer.parseInt(st.nextToken());
-        }
-      }
-      int max=0;
-      for(int i=0; i<=N-M; i++){
-        for(int j=0; j<=N-M; j++){
-          int temp=0;
-          for(int k=i; k<i+M; k++){
-            for(int l=j; l<j+M; l++){
-              temp+= arr[k][l];
-            }
-          }
-          if(temp>max){
-            max=temp;
-          }
-        }
-      }
-      sb.append(max).append("\n");
-    }
-    System.out.println(sb);
-  }
 }
