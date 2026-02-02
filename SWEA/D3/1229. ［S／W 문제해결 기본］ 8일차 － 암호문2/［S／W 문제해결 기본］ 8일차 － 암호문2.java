0f1094c806f41e17c2679daa4da7d605bb690495
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-02 11:58:01 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 76 ms, Memory: 24,832 KB -BaekjoonHub

--- SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -3,50 +3,47 @@ import java.util.*;
 
 public class Solution {
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    StringBuilder sb = new StringBuilder();
-    for (int tc = 1; tc <= 10; tc++) {
-      int N = Integer.parseInt(br.readLine());
-      List<Integer> list = new LinkedList<>();
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < N; i++) {
-        list.add(Integer.parseInt(st.nextToken()));
-      }
-      int M = Integer.parseInt(br.readLine());
-      st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < M; i++) {
-        String I = st.nextToken();
-        if (I.equals("I")) {
-          int index = Integer.parseInt(st.nextToken());
-          int length = Integer.parseInt(st.nextToken());
-          List<Integer> nums = new LinkedList<>();
-          for (int j = 0; j < length; j++) {
-            nums.add(Integer.parseInt(st.nextToken()));
-          }
-          list.addAll(index, nums);
-        } else if (I.equals("D")) {
-          int index = Integer.parseInt(st.nextToken());
-          int length = Integer.parseInt(st.nextToken());
-          for (int j = 0; j < length; j++) {
-            list.remove(index);
-          }
-        } else {
-          int length = Integer.parseInt(st.nextToken());
-          for (int j = 0; j < length; j++) {
-            list.add(Integer.parseInt(st.nextToken()));
-          }
-        }
-      }
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<=10; i++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			List<String> list = new ArrayList<>();
+			while(N-->0){
+				list.add(st.nextToken());
+			}
+			
+			int C = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			while(C-->0) {
+				String command = st.nextToken();
+				
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				if(command.equals("I")) {
+					for(int j=0; j<y; j++) {
+						list.add(x+j,st.nextToken());
+					}
+				}else {
+					while(y-->0) {
+						list.remove(x);
+					}
+				}
+			}
+			
+			sb.append("#").append(i).append(" ");
+			
+			for(int j=0; j<10; j++) {
+				sb.append(list.get(j)).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 
-      sb.append("#").append(tc).append(" ");
-
-      for (int i = 0; i < 10; i++) {
-        sb.append(list.get(i)).append(" ");
-      }
-      sb.append("\n");
-    }
-    System.out.println(sb);
-  }
 }
