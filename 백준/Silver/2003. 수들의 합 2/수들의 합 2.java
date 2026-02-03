fdfaff512c8574d0913693ec83171d063dbf32d0
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-03 14:41:18 +0900

    [Silver IV] Title: 수들의 합 2, Time: 128 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,31 +1,39 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new  StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] sequence = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            sequence[i] = Integer.parseInt(st.nextToken());
-        }
 
-        int totalSum =sequence[0];
-        int count=0;
-        int end=0;
-        for(int start=0; start<N; start++){
-            while(end<N-1 && totalSum<M){
-                end++;
-                totalSum+=sequence[end];
-            }
-            if(totalSum==M) count++;
-            if(end==N) break;
-            totalSum-=sequence[start];
-        }
-
-        System.out.println(count);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int count = 0;
+		int sum = 0;
+		int s = 0;
+		int e = 0;
+		while(true) {
+			if(sum>=M) {
+				sum-=arr[s];
+				s++;
+			} else if(e==N) {
+				break;
+			} else {
+				sum+=arr[e];
+				e++;
+			}
+			if(sum==M) count++;
+		}
+		System.out.println(count);
+	}
 }
