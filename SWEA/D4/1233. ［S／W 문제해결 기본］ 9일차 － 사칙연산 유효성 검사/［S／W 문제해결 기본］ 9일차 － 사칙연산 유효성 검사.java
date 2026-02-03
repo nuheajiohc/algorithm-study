2afe7138ebb0495a0d1e6d8aceb953816281cabf
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-03 13:35:05 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 97 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -5,61 +5,36 @@ public class Solution {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringBuilder sb = new StringBuilder();
 		
 		for(int t=1; t<=10; t++) {
-			
 			sb.append("#").append(t).append(" ");
 			int N = Integer.parseInt(br.readLine());
 			
-			boolean[] arr = new boolean[N+1];
+			int op = 0;
+			int num = 0;
 			for(int i=0; i<N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				int vertex = Integer.parseInt(st.nextToken());
-				String first = st.nextToken();
-				
-				while(st.hasMoreTokens()) {
-					st.nextToken();
-				}
-				
-				if(isNum(first)) {
-					arr[vertex] = true;
+				st.nextToken();
+				if(!isNum(st.nextToken())) {
+					op++;
+				}else if(op==N/2) {
+					num++;
 				}
 			}
-			
-			if(N%2==0) {
-				sb.append(0).append("\n");
-				continue;
-			}
-			
-			if(possible(N, arr)) {
+		
+			if(op<num && op+num==N) {
 				sb.append(1);
 			}else {
 				sb.append(0);
 			}
+			
 			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	public static boolean possible(int N, boolean[] arr) {
-		for(int i=1; i<=N/2; i++) {
-			if(arr[i]) {
-				return false;
-			}
-		}
-		
-		for(int i=(N/2)+1; i<=N; i++) {
-				if(!arr[i]) {
-					return false;
-				}
-		}
-		return true;
-	}
-	
 	public static boolean isNum(String target) {
 		return !(target.equals("*") || target.equals("/") || target.equals("+") || target.equals("-"));
 	}
-
 }
