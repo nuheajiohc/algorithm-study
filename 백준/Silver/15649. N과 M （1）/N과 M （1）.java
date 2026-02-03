99555cf0e93c71c20adc96ad07727c64b941213c
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-03 16:35:56 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 22332 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	
+	private static int N,M;
+	private static StringBuilder sb = new StringBuilder();
+	private static int[] arr;
+	private static boolean[] used;
+	
 
-  private static int N, M;
-  private static int[] arr;
-  private static boolean[] used;
-  private static StringBuilder sb = new StringBuilder();
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-
-    arr = new int[M];
-    used = new boolean[N+1];
-    btk(0);
-    System.out.println(sb);
-  }
-
-  public static void btk(int depth){
-    if(depth == M){
-      for(int i : arr){
-        sb.append(i).append(" ");
-      }
-      sb.append("\n");
-      return;
-    }
-
-    for(int i=1; i<=N; i++){
-      if(used[i]) continue;
-      used[i] = true;
-      arr[depth]= i;
-      btk(depth+1);
-      used[i] = false;
-    }
-  }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		used = new boolean[N+1];
+		btk(0);
+		System.out.println(sb);
+	}
+	
+	public static void btk(int cur) {
+		if(cur>=M) {
+			for(int v : arr) {
+				sb.append(v).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			if(used[i]) continue;
+			arr[cur] = i;
+			used[i] = true;
+			btk(cur+1);
+			used[i] = false;
+		}
+	}
 }
