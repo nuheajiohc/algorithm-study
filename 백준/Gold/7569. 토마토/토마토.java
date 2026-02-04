6b7ea0545fcb370ccc0e34bc3f8536a83e375b21
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-04 09:23:51 +0900

    [Gold V] Title: 토마토, Time: 552 ms, Memory: 105460 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -2,61 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	private static int[] dx = {0,0,1,-1,0,0}, dy = {1,-1,0,0,0,0}, dz = {0,0,0,0,1,-1};
+	private static int M,N,H;
+	private static int[][][] board;
+	private static int remainder;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    private static int[][] d = {{1,0,0},{-1,0,0},{0,1,0},{0,0,1},{0,-1,0},{0,0,-1}};
-    
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        int H = Integer.parseInt(st.nextToken());
-
-        Deque<int[]> q = new ArrayDeque<>();
-
-        int[][][] warehouse = new int[H][N][M];
-        for(int i=0; i<H; i++){
-            for(int j=0; j<N; j++){
-                st = new StringTokenizer(br.readLine());
-                for(int k=0; k<M; k++){
-                    warehouse[i][j][k] = Integer.parseInt(st.nextToken());
-                    if(warehouse[i][j][k]==1){
-                        q.offer(new int[]{i,j,k});
-                    }
-                }
-            }
-        }
-
-        while(!q.isEmpty()){
-            int[] cur=q.poll();
-            for(int[] dir : d){
-                int nz=cur[0]+dir[0];
-                int nx=cur[1]+dir[1];
-                int ny=cur[2]+dir[2];
-                if(nz<0 || nz>=H || nx<0 || nx>=N || ny<0 || ny>=M) continue;
-                if(warehouse[nz][nx][ny]!=0) continue;
-                warehouse[nz][nx][ny] = warehouse[cur[0]][cur[1]][cur[2]] + 1;
-                q.offer(new int[]{nz,nx,ny});
-            }
-        }
-        int result=-1;
-        for(int i=0; i<H; i++){
-            for(int j=0; j<N; j++){
-                for(int k=0; k<M; k++){
-                    if(warehouse[i][j][k]==0){
-                        System.out.println(-1);
-                        return;
-                    }
-                    result = Math.max(result,warehouse[i][j][k]);
-                }
-            }
-        }
-
-        if(result==1){
-            System.out.println(0);
-        }else{
-            System.out.println(result-1);
-        }
-    }
-    
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		
+		Queue<int[]> queue = new ArrayDeque<>();
+		
+		board = new int[H][N][M];
+		for(int i=0; i<H; i++) {
+			for(int j=0; j<N; j++){
+				st = new StringTokenizer(br.readLine());
+				for(int k=0; k<M; k++) {
+					board[i][j][k] = Integer.parseInt(st.nextToken());
+					if(board[i][j][k]==1) {
+						queue.offer(new int[] {i,j,k});
+					}
+					if(board[i][j][k]==0) remainder++;
+				}
+			}
+		}
+		
+		if(remainder==0) {
+			System.out.println(0);
+			return;
+		}
+		
+		int day = 0;
+		while(true) {
+			int size = queue.size();
+			
+			while(size-->0) {
+				int[] cur = queue.poll();
+				for(int dir=0; dir<6; dir++) {
+					int nz = cur[0] + dz[dir];
+					int nx = cur[1] + dx[dir];
+					int ny = cur[2] + dy[dir];
+					if(nx<0 || nx>=N || ny<0 || ny>=M || nz<0 || nz>=H) continue;
+					if(board[nz][nx][ny]!=0) continue;
+					board[nz][nx][ny] = 1;
+					queue.offer(new int[] {nz, nx ,ny});
+					remainder--;
+				}	
+			}
+			if(queue.isEmpty()) break;
+			day++;
+		}
+		
+		if(remainder>0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(day);
+		}
+	}
 }
