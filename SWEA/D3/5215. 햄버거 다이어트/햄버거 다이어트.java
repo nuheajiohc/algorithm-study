8bbd86fe4bec8347af1267e2127c7c802138d050
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-05 14:46:20 +0900

    [D3] Title: 햄버거 다이어트, Time: 713 ms, Memory: 27,392 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,53 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-    private static Food[] foods;
-    private static int N, L;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-        for(int t=1; t<=T; t++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            L = Integer.parseInt(st.nextToken());
-            foods = new Food[N];
-            for(int i=0; i<N; i++){
-                st = new StringTokenizer(br.readLine());
-                int score = Integer.parseInt(st.nextToken());
-                int kcal = Integer.parseInt(st.nextToken());
-                foods[i] = new Food(score, kcal);
-            }
-            int result = btk(0,0,0);
-
-            sb.append("#").append(t).append(" ").append(result).append("\n");
-        }
-
-        System.out.println(sb);
-    }
-
-    public static int btk(int depth, int totalScore, int totalKcal){
-        if(depth==N){
-            if(totalKcal<=L){
-                return totalScore;
-            }else
-                return 0;
-        }
-        int max = btk(depth+1, totalScore+foods[depth].score, totalKcal+foods[depth].kcal);
-        max = Math.max(max, btk(depth+1, totalScore, totalKcal));
-        return max;
-    }
-
-    public static class Food{
-        int score;
-        int kcal;
-
-        public Food(int score, int kcal) {
-            this.score = score;
-            this.kcal = kcal;
-        }
-    }
+	
+	private static int N, L;
+	private static int[][] arr;
+	private static boolean[] used;
+	private static int highScore;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int t=1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			
+			highScore = 0;
+			arr = new int[N][2];
+			used = new boolean[N];
+			
+			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[i] = new int[] {a,b};
+			}
+			
+			btk(0);
+			
+			sb.append("#").append(t).append(" ").append(highScore).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	public static void btk(int depth) {
+		if(depth==N) {
+			int score = 0;
+			int kcal = 0;
+			for(int i=0; i<N; i++) {
+				if(used[i]) {
+					score += arr[i][0];
+					kcal += arr[i][1];
+				}
+			}
+			
+			if(kcal<=L) {
+				highScore = Math.max(highScore, score);
+			}
+			return;
+		}
+		
+		used[depth] = true;
+		btk(depth+1);
+		used[depth] = false;
+		btk(depth+1);
+	}
 }
