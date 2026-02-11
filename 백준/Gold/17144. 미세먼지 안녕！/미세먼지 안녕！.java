d6550c55e108aebf2ee3a21f9cb2628716d2aaf7
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-11 21:08:48 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 268 ms, Memory: 17344 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -9,7 +10,7 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	static int R,C,T;
-	static int[][] room;
+	static int[][] room, tmp;
 	static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
 	static List<Integer> machine = new ArrayList<>();
 	
@@ -22,7 +23,8 @@ public class Main {
 		T = Integer.parseInt(st.nextToken());
 		
 		room = new int[R][C];
-		
+		tmp = new int[R][C];
+
 		for(int i=0; i<R; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<C; j++) {
@@ -51,31 +53,35 @@ public class Main {
 	}
 	
 	static void spread() {
-		Queue<int[]> queue = new ArrayDeque<>();
-		
-		for(int i=0; i<R; i++) {
-			for(int j=0; j<C; j++) {
-				if(room[i][j]>0) queue.offer(new int[] {i,j, room[i][j]});
+		for(int i=0; i<R; i++){
+			Arrays.fill(tmp[i], 0);
+		}
+
+		for(int i=0; i<R; i++){
+			for(int j=0; j<C; j++){
+				if(room[i][j]<5) continue;
+
+				int amount = room[i][j]/5;
+				int cnt = 0;
+
+				for(int dir=0; dir<4; dir++) {
+					int nx = i + dx[dir];
+					int ny = j + dy[dir];
+					if(nx<0 || nx>=R || ny<0 || ny>=C) continue;
+					if(room[nx][ny]==-1) continue;
+					tmp[nx][ny] += amount;
+					cnt++;
+				}
+				room[i][j] -= amount * cnt;
 			}
 		}
-		
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int x = cur[0];
-			int y = cur[1];
-			int quantity = cur[2];
-			if(room[x][y]<5) continue;
-			
-			int cnt = 0;
-			for(int dir=0; dir<4; dir++) {
-				int nx = x + dx[dir];
-				int ny = y + dy[dir];
-				if(nx<0 || nx>=R || ny<0 || ny>=C) continue;
-				if(room[nx][ny]==-1) continue;
-				room[nx][ny] += quantity/5;
-				cnt++;
+
+		for(int i=0; i<R; i++){
+			for(int j=0; j<C; j++){
+				if(tmp[i][j] > 0){
+					room[i][j] += tmp[i][j];
+				}
 			}
-			room[x][y] -= (quantity/5)*cnt;
 		}
 	}
 	
@@ -83,14 +89,10 @@ public class Main {
 		int top = machine.get(0);
 		int bottom = machine.get(1);
 
-		// 1. 위쪽 공기청정기 (반시계 방향 회전)
-    	// 당겨오는 방향: 위 -> 오른쪽 -> 아래 -> 왼쪽
     	int[] dxTop = {-1, 0, 1, 0};
     	int[] dyTop = {0, 1, 0, -1};
     	rotate(top, 0, top, dxTop, dyTop);
 
-    	// 2. 아래쪽 공기청정기 (시계 방향 회전)
-    	// 당겨오는 방향: 아래 -> 오른쪽 -> 위 -> 왼쪽
     	int[] dxBottom = {1, 0, -1, 0};
     	int[] dyBottom = {0, 1, 0, -1};
     	rotate(bottom, bottom, R-1, dxBottom, dyBottom);
