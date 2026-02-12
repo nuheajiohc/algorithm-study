318dd2de7b610d7d30f114c6726b6fd03b36838a
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-12 13:49:12 +0900

    [D2] Title: 나무 높이, Time: 96 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,52 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int TC = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int TC = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1; tc<=TC; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int[] arr = new int[N];
+			int max = 0;
+			for(int i=0; i<N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+				max = Math.max(max, arr[i]);
+			}
+			
+			int one = 0;
+			int two = 0;
+			for(int h : arr) {
+				one += (max-h)%2;
+				two += (max-h)/2;
+			}
 
-        StringBuilder sb = new StringBuilder();
-        for(int tc=1; tc<=TC; tc++){
-            int N = Integer.parseInt(br.readLine());
-            int[] arr = new int[N];
+			int day=0;
+			if(one>two) {
+				day = one * 2 - 1;
+			}else if(one==two) {
+				day = one*2;
+			}else {
+				day = one*2 + (two-one+1) +  ((two-one+2)/3)-1;
+			}
+			
+			sb.append("#").append(tc).append(" ").append(day).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-            int max = 0;
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++){
-                arr[i] = Integer.parseInt(st.nextToken());
-                max = Math.max(arr[i], max);
-            }
-
-            int day1 = 0;
-            int day2 = 0;
-
-            for(int h : arr){
-                int diff = max - h;
-                day2 += diff/2;
-                day1 += diff%2;
-            }
-
-            if(day1+1 < day2){
-                int diff = day2 - day1;
-
-                int k = (diff + 1) / 3;
-                day2 -= k;
-                day1 += 2 * k;
-            }
-
-            int answer = 0;
-
-            if(day1 > day2){
-                answer = day1*2 -1;
-            } else{
-                answer = day2 * 2;
-            }
-
-            sb.append("#").append(tc).append(" ").append(answer).append("\n");
-        }
-        System.out.println(sb);
-    }
 }
