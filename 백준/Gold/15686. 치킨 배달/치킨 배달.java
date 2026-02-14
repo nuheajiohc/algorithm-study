1dede8cfd8a3b5d13235d20697d083efa5d8212d
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-14 14:41:25 +0900

    [Gold V] Title: 치킨 배달, Time: 132 ms, Memory: 14956 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,69 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int N,M,size;
-	static int[][] city, chicken;
-	static boolean[] used;
-	static int min = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		chicken = new int[13][2];
-		city = new int[N][N];
-		used = new boolean[13];
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				city[i][j] = Integer.parseInt(st.nextToken());
-				
-				if(city[i][j]==2) {
-					chicken[size++] = new int[] {i,j};
-				}
-			}
-		}
-		
-		permutation(0,0);
-		System.out.println(min);
-	}
-	
-	static void permutation(int depth, int k) {
-		if(depth==M) {
-			findMin();
-			return;
-		}
-		
-		for(int i=k; i<size; i++) {
-			used[i] = true;
-			permutation(depth+1, i+1);
-			used[i] = false;
-		}
-	}
-	
-	static void findMin() {
-		int value = 0;
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(city[i][j]==1) {
-					value += getMinDistance(i,j);
-				}
-			}
-		}
-		min = Math.min(min, value);
-	}
-	
-	static int getMinDistance(int x, int y) {
-		int tmp = Integer.MAX_VALUE;
-		for(int i=0; i<size; i++) {
-			if(!used[i]) continue;
-			tmp = Math.min(tmp, Math.abs(x-chicken[i][0])+ Math.abs(y-chicken[i][1]));
-		}
-		return tmp;
-	}
+    static int N,M, houseLen, chickenLen;
+    static boolean[] used;
+    static int[] house = new int[100];
+    static int[] chicken = new int[13];
+    static int[][] dist;
+    static int minSum = Integer.MAX_VALUE;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dist = new int[2*N][13];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                int num = Integer.parseInt(st.nextToken());
+                if(num==1){
+                    house[houseLen++] = i*N+j;
+                }else if(num==2){
+                    chicken[chickenLen++] = i*N+j;;
+                }
+            }
+        }
+
+        for(int i=0; i<houseLen; i++){
+            for(int j=0; j<chickenLen; j++){
+                int hx = house[i]/N;
+                int hy = house[i]%N;
+                int cx = chicken[j]/N;
+                int cy = chicken[j]%N;
+                dist[i][j] = Math.abs(hx-cx) + Math.abs(hy-cy);
+            }
+        }
+
+        used = new boolean[chickenLen];
+        subset(0, 0);
+
+        System.out.println(minSum);
+    }
+
+    static void subset(int depth, int k){
+        if(depth == M){
+            findMinSum();
+            return;
+        }
+
+        for(int i=k; i<chickenLen; i++){
+            used[i] = true;
+            subset(depth+1, i+1);
+            used[i] = false;
+        }
+    }
+
+    static void findMinSum(){
+        int sum = 0;
+        for(int i=0; i<houseLen; i++){
+            int tmp = Integer.MAX_VALUE;
+            for(int j=0; j<chickenLen; j++){
+                if(!used[j]) continue;
+                tmp = Math.min(tmp, dist[i][j]);
+            }
+            sum += tmp;
+        }
+
+        minSum = Math.min(minSum, sum);
+    }
 }
