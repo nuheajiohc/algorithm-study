3ee0d588145684008d91f3718dc77331f9dcbc91
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-14 22:04:19 +0900

    [Unrated] Title: [모의 SW 역량테스트] 원자 소멸 시뮬레이션, Time: 1,460 ms, Memory: 182,632 KB -BaekjoonHub

--- SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
+++ SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
@@ -1,88 +1,77 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	
-	static int[] dx = {0, 0, -1, 1}, dy = {1, -1, 0, 0};
-	static int[][] map = new int[4001][4001];
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int TC = Integer.parseInt(br.readLine());
-		
-		StringBuilder sb = new StringBuilder();
-		for(int tc=1; tc<=TC; tc++) {
-			int N = Integer.parseInt(br.readLine());
+    static int[] dx = {0, 0, -1, 1}, dy = {1, -1, 0, 0};
+    static int[][] board = new int[4001][4001];
+    static List<Atom> atoms;
+    static int atomLen;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			List<Atom> atoms = new ArrayList<>();
-			for(int i=0; i<N; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				int x = (Integer.parseInt(st.nextToken())+1000) * 2;
-				int y = (Integer.parseInt(st.nextToken())+1000) * 2;
-				int d = Integer.parseInt(st.nextToken());
-				int k = Integer.parseInt(st.nextToken());
-				atoms.add(new Atom(x, y, d, k));
-				map[x][y] = k;
-			}
-			
-			int totalSum = 0;
-			
-			while(!atoms.isEmpty()) {
-				for(Atom atom : atoms) {
-					map[atom.x][atom.y]=0;
-					atom.move();
-					if(atom.x<0 || atom.x>4000 || atom.y<0 || atom.y>4000) {
-						atom.isDead = true;
-						continue;
-					}
-					
-					map[atom.x][atom.y]+=atom.k;
-				}
-				
-				for(Atom atom : atoms) {
-					if(atom.isDead) continue;
-					if(map[atom.x][atom.y]>atom.k) {
-						totalSum += atom.k;
-						atom.isDead = true;
-					}
-				}
-				
-				for(Atom atom : atoms) {
-					if(atom.x<0 || atom.x>4000 || atom.y<0 || atom.y>4000) continue;
-					if(atom.isDead) {
-						map[atom.x][atom.y] = 0;
-					}
-				}
-				
-				atoms.removeIf(a-> a.isDead);
-			}
-			
-			sb.append("#").append(tc).append(" ").append(totalSum).append("\n");
-		}
-		System.out.println(sb);
-	}
+        int TC = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int tc=1; tc<=TC; tc++){
+            int atomLen = Integer.parseInt(br.readLine());
 
-	static class Atom {
-		int x;
-		int y;
-		int d;
-		int k;
-		boolean isDead;
-		
-		Atom(int x, int y, int d, int k){
-			this.x=x;
-			this.y=y;
-			this.d=d;
-			this.k=k;
-		}
-		
-		void move(){
-			this.x = x+dx[d];
-			this.y = y+dy[d];
-		}
-	}
+            atoms = new LinkedList<>();
+            for(int i=0; i<atomLen; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int x = (Integer.parseInt(st.nextToken())+1000)*2;
+                int y = (Integer.parseInt(st.nextToken())+1000)*2;
+                int dir = Integer.parseInt(st.nextToken());
+                int K = Integer.parseInt(st.nextToken());
+                atoms.add(new Atom(x, y, dir, K));
+            }
+
+            int total=0;
+            while(!atoms.isEmpty()){
+                Iterator<Atom> iter = atoms.iterator();
+                while(iter.hasNext()){
+                    Atom atom = iter.next();
+                    board[atom.x][atom.y] = 0;
+                    atom.x = atom.x + dx[atom.dir];
+                    atom.y = atom.y + dy[atom.dir];
+                    if(atom.x<0 || atom.x>4000 || atom.y<0 || atom.y>4000){
+                        iter.remove();
+                        continue;
+                    }
+                    board[atom.x][atom.y] += atom.K;
+                }
+
+                for(Atom atom : atoms){
+                    if(board[atom.x][atom.y]>atom.K){
+                        atom.mark = true;
+                        total += atom.K;
+                    }
+                }
+
+                iter = atoms.iterator();
+                while(iter.hasNext()){
+                    Atom atom = iter.next();
+                    if(atom.mark){
+                        iter.remove();
+                        board[atom.x][atom.y] = 0;
+                    }
+                }
+            }
+
+            sb.append("#").append(tc).append(" ").append(total).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static class Atom{
+        int x; int y;
+        int dir; int K;
+        boolean mark;
+
+        Atom(int x, int y, int dir, int K){
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+            this.K = K;
+        }
+    }
 }
