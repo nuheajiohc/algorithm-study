d572212db10c3ffe9a6e87bc1c9a8aa25e58112c
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-14 10:42:14 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 100 ms, Memory: 28,800 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -2,60 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-    static int V,E;
-    static List<Integer>[] adj;
-    static boolean[] used; 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringBuilder sb = new StringBuilder();
         for(int tc=1; tc<=10; tc++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
+            sb.append("#").append(tc).append(" ");
 
-            List<Integer> answer = new ArrayList<>();
-            adj = new ArrayList[V+1];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            List<Integer>[] adj = new ArrayList[V+1];
             for(int i=1; i<=V; i++) adj[i] = new ArrayList<>();
+
+            int[] degree = new int[V+1];
+
             st = new StringTokenizer(br.readLine());
             while(st.hasMoreTokens()){
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
                 adj[a].add(b);
+                degree[b]++;
             }
 
-            used = new boolean[V+1];
-
-            int cnt = 0;
-            while(cnt<V){
-                for(int i=1; i<=V; i++){
-                    if(used[i]) continue;
-                    if(!hasSubNode(i)){
-                        used[i] = true;
-                        answer.add(i);
-                        cnt++;
-                    }
+            Queue<Integer> queue = new ArrayDeque<>();
+            for(int i=1; i<=V; i++){
+                if(degree[i]==0){
+                    queue.offer(i);
+                    sb.append(i).append(" ");
                 }
             }
-            
-
-            sb.append("#").append(tc).append(" ");
-            for(int i=V-1; i>=0; i--){
-                sb.append(answer.get(i)).append(" ");
+            while(!queue.isEmpty()){
+                int cur = queue.poll();
+                for(int next : adj[cur]){
+                    degree[next]--;
+                    if(degree[next]!=0) continue;
+                    queue.offer(next);
+                    sb.append(next).append(" ");
+                }
             }
             sb.append("\n");
         }
-
         System.out.println(sb);
-    }
-
-    static boolean hasSubNode(int v){
-        int cnt =0;
-        for(int u : adj[v]){
-            if(used[u]) continue;
-            cnt++;
-        }
-        return cnt>0;
     }
 }
