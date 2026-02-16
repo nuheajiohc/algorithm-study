728435213ed81aa4b6dffe3cd2a279f3c4612f27
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-16 16:25:36 +0900

    [D4] Title: 준환이의 양팔저울, Time: 517 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
@@ -3,9 +3,12 @@ import java.util.*;
 
 public class Solution {
 
-    static int N, cnt;
+    static int N;
     static int[] arr = new int[9];
     static boolean[] used = new boolean[9];
+
+    static int[] fact = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+    static int[] pow = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,30 +17,36 @@ public class Solution {
         for(int tc=1; tc<=TC; tc++){
             N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int totalWeight = 0;
             for(int i=0; i<N; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
+                totalWeight += arr[i];
             }
-            cnt = 0;
-            recursion(0, 0 ,0);
+            int cnt = recursion(0, 0 ,0, totalWeight);
             sb.append("#").append(tc).append(" ").append(cnt).append("\n");
         }
         System.out.println(sb);
     }
     
-    static void recursion(int depth, int leftSum,  int rightSum){
-        if(leftSum<rightSum) return;
+    static int recursion(int depth, int leftSum,  int rightSum, int remain){
+        if(leftSum<rightSum) return 0;
 
-        if(depth==N){
-            cnt++;
-            return;
+        if(leftSum>=rightSum+remain){
+            return fact[N-depth] * pow[N-depth];
         }
 
+        if(depth==N){
+            return 1;
+        }
+
+        int cnt = 0;
         for(int i=0; i<N; i++){
             if(used[i]) continue;
             used[i] = true;
-            recursion(depth+1, leftSum+arr[i], rightSum);
-            recursion(depth+1, leftSum, rightSum+arr[i]);
+            cnt += recursion(depth+1, leftSum+arr[i], rightSum, remain-arr[i]);
+            cnt += recursion(depth+1, leftSum, rightSum+arr[i], remain-arr[i]);
             used[i] = false;
         }
+        return cnt;
     }
 }
