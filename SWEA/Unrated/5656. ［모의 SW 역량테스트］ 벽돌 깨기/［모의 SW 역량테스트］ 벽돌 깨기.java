504b461463a0403c37bc078ddbaa696e19ffb6d0
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-17 12:27:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 164 ms, Memory: 62,704 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -3,11 +3,8 @@ import java.util.*;
 
 public class Solution {
 
-    static int N,H,W;
-    static int[][] map, copyMap;
+    static int N, H, W;
     static int[] dx = {0,0,1,-1}, dy = {1,-1,0,0};
-    static int[] tmp, used;
-    static int minCnt;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,95 +15,87 @@ public class Solution {
             N = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
             H = Integer.parseInt(st.nextToken());
-            map = new int[H][W];
-            copyMap = new int[H][W];
-            used = new int[N];
-            tmp = new int[H];
-            minCnt = Integer.MAX_VALUE;
+
+            int totalBrick = 0;
+            int[][] map = new int[H][W];
             for(int i=0; i<H; i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j=0; j<W; j++){
                     map[i][j] = Integer.parseInt(st.nextToken());
+                    if(map[i][j]>0) totalBrick++;
                 }
             }
 
-            btk(0);
+            int minCnt = btk(0, map, totalBrick);
             
             sb.append("#").append(tc).append(" ").append(minCnt).append("\n");
         }
         System.out.println(sb);
     }
 
-    static void btk(int depth){
+    static int btk(int depth, int[][] map, int remain){
+        if(remain==0) return 0;
+
         if(depth==N){
-            initMap();
-            for(int i=0; i<N; i++){
-                breakBrick(used[i]);
-                afterProcess();
-            }
-            countBrick();
-            return;
+            return remain;
         }
 
+        int minCnt = Integer.MAX_VALUE;
         for(int w=0; w<W; w++){
-            used[depth] = w;
-            btk(depth+1);
+            int r = 0;
+            while(r<H && map[r][w]==0) r++;
+            if(r==H) continue;
+
+            int[][] nextMap = initMap(map);
+            int broken = breakBrick(nextMap, r, w);
+            applyGravity(nextMap);
+            minCnt = Math.min(minCnt, btk(depth+1, nextMap, remain-broken));
         }
+        return minCnt;
     }
 
-    static void initMap(){
+    static int[][] initMap(int[][] map){
+        int[][] tmp = new int[H][W];
         for(int i=0; i<H; i++){
             for(int j=0; j<W; j++){
-                copyMap[i][j] = map[i][j]; 
+                tmp[i][j] = map[i][j]; 
             }
         }
+        return tmp;
     }
 
-    static void countBrick(){
-        int cnt = 0;
-        for(int i=0; i<H; i++){
-            for(int j=0; j<W; j++){
-                if(copyMap[i][j]>0) cnt++;
-            }
-        }
-        minCnt = Math.min(minCnt, cnt);
-    }
-
-    static void breakBrick(int w){
+    static int breakBrick(int[][] map, int r, int c){
         Queue<int[]> q = new ArrayDeque<>();
-        for(int h=0; h<H; h++){
-            if(copyMap[h][w]>0){
-                q.offer(new int[]{h,w});
-                break;
-            }
-        }
-
+        q.offer(new int[]{r, c, map[r][c]});
+        map[r][c] = 0;  
+        int cnt = 1;
         while(!q.isEmpty()){
             int[] cur = q.poll();
             int x = cur[0]; int y = cur[1];
-            int dist = copyMap[x][y];
-            copyMap[x][y]=0;
+            int dist = cur[2];
             for(int d=1; d<dist; d++){
                 for(int dir=0; dir<4; dir++){
                     int nx = x + dx[dir]*d;
                     int ny = y + dy[dir]*d;
                     if(nx<0 || nx>=H || ny<0 || ny>=W) continue;
-                    if(copyMap[nx][ny]==0) continue;
-                    q.offer(new int[]{nx, ny});
+                    if(map[nx][ny]==0) continue;
+                    q.offer(new int[]{nx, ny, map[nx][ny]});
+                    map[nx][ny]=0;
+                    cnt++;
                 }
             }
         }
+        return cnt;
     }
 
-    static void afterProcess(){
+    static void applyGravity(int[][] map){
         for(int w=0; w<W; w++){
-            Arrays.fill(tmp, 0);
-            int idx = 0;
+            int idx = H-1;
             for(int h=H-1; h>=0; h--){
-                if(copyMap[h][w]>0) tmp[idx++] = copyMap[h][w];
-            }
-            for(int h=H-1; h>=0; h--){
-                copyMap[h][w] = tmp[H-h-1];
+                if(map[h][w]==0) continue;
+                int tmp = map[h][w];
+                map[h][w] = 0;
+                map[idx--][w] = tmp;
             }
         }
     }
