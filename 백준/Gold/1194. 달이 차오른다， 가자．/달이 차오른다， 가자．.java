dab285f3ca4a10a047cca7917c2ce68698aa95fb
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-19 17:17:21 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 112 ms, Memory: 16896 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -2,73 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	static int N, M;
+	static char[][] board;
+	static int[][][] visited;
+	static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
 
-    static int N,M;
-    static char[][] maze;
-    static int[][][] visited;
-    static int[] dx = {0,0,1,-1}, dy = {1,-1,0,0};
-    static int[] arrived;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		board = new char[N][M];
+		visited = new int[N][M][64];
+		Queue<int[]> queue = new ArrayDeque<>();
+		for(int i=0; i<N; i++) {
+			String line = br.readLine();
+			for(int j=0; j<M; j++) {
+				board[i][j] = line.charAt(j);
+				if(board[i][j]=='0') {
+					board[i][j] = '.';
+					queue.offer(new int[] {i,j,0});
+					visited[i][j][0] = 1;
+				}
+			}
+		}
+		
+		int min = bfs(queue);
+		System.out.println(min);
+	}
+	
+	static int bfs(Queue<int[]> queue) {
+		while(!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			int x = cur[0];
+			int y = cur[1];
+			int bit = cur[2];
+			for(int dir=0; dir<4; dir++) {
+				int nx = x + dx[dir];
+				int ny = y + dy[dir];
+				if(nx<0|| nx>=N || ny<0 || ny>=M) continue;
+				if(board[nx][ny]=='#') continue;
+				
+				if(board[nx][ny]=='1') {
+					return visited[x][y][bit];
+				}
+				
+				int nextBit = bit;
+				if(board[nx][ny]>='a' && board[nx][ny]<='z') {
+					int key = 1<<(board[nx][ny]-'a');
+					nextBit = bit | key;
+				}else if(board[nx][ny]>='A' && board[nx][ny]<='Z') {
+					int key = 1<<(board[nx][ny]-'A');
+					if((nextBit & key) == 0) continue;
+				}
+				
+				if(visited[nx][ny][nextBit]!=0)continue;
+				visited[nx][ny][nextBit] = visited[x][y][bit] + 1;
+				queue.offer(new int[] {nx, ny, nextBit});
+			}
+		}
+		return -1;
+	}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        int ix = 0;
-        int iy = 0;
-
-        maze = new char[N][M];
-        for(int i=0; i<N; i++){
-            String line = br.readLine();
-            for(int j=0; j<M; j++){
-                maze[i][j] = line.charAt(j);
-                if(maze[i][j] == '0'){
-                    ix = i;
-                    iy = j;
-                }
-            }
-        }
-        visited = new int[64][N][M];
-
-        bfs(ix, iy);
-        if(arrived == null){
-            System.out.println(-1);
-        }else{
-            System.out.println(visited[arrived[0]][arrived[1]][arrived[2]]-1);
-        }
-    }
-
-    static void bfs(int ix, int iy){
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{0, ix, iy});
-        visited[0][ix][iy] = 1;
-
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            for(int dir=0; dir<4; dir++){
-                int nx = cur[1] + dx[dir];
-                int ny = cur[2] + dy[dir];
-                int status = cur[0];
-                if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
-                if(visited[status][nx][ny]>0) continue;
-                if(maze[nx][ny]=='#') continue;
-                if(maze[nx][ny]>='A'&& maze[nx][ny]<='F'){
-                    if((status & 1<<(maze[nx][ny]-'A'))==0){
-                        continue;
-                    }
-                } else if(maze[nx][ny]>='a' && maze[nx][ny]<='f'){
-                    status |= 1<<(maze[nx][ny]-'a');
-                }
-
-                visited[status][nx][ny] = visited[cur[0]][cur[1]][cur[2]]+1;
-                queue.offer(new int[]{status, nx, ny});
-
-                if(maze[nx][ny] =='1'){
-                    arrived = new int[]{status, nx, ny};
-                    return;
-                }
-            }
-        }
-    }
 }
