c0911dda082ea7ab4ef1ec02e83d6d94863d704f
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-23 21:48:20 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -13,13 +13,7 @@ public class Main {
 
         int[][] dp = new int[N+1][3];
         dp[1][1] = arr[1];
-        if(N>1){
-            dp[2][0] = arr[1];
-            dp[2][1] = arr[2];
-            dp[2][2] = arr[1] + arr[2];
-        }
-        
-        for(int i=3; i<=N; i++){
+        for(int i=2; i<=N; i++){
             dp[i][0] = Math.max(dp[i-1][1], dp[i-1][2]);
             dp[i][1] = arr[i] + dp[i-1][0];
             dp[i][2] = arr[i] + dp[i-1][1];
