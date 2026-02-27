d94a73a3a7346b319d66cbee75f0e24225aa2a2e
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-27 11:38:58 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 656 ms, Memory: 109832 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,60 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	static int N, M;
+	static char[][] map;
+	static int[][][] visited;
+	static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
 
-    private static int N,M;
-    private static int[][] board;
-    private static int[][][] vis;
-    private static int[][] d = {{1,0},{-1,0},{0,1},{0,-1}};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new char[N][M];
+		for(int i=0; i<N; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		
+		visited = new int[N][M][2];
+		
+		int minDistance = bfs();
+		System.out.println(minDistance);
+	}
+	
+	static int bfs() {
+		Queue<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {0, 0, 0});
+		visited[0][0][0] = 1;
+		
+		if(N==1 && M==1) return 1;
+		
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int x = cur[0];
+			int y = cur[1];
+			int wall = cur[2];
+			for(int dir=0; dir<4; dir++) {
+				int nx = cur[0] + dx[dir];
+				int ny = cur[1] + dy[dir];
+				if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        board = new int[N][M];
-        vis = new int[N][M][2];
-        for(int k=0; k<2; k++){
-            for(int i=0; i<N; i++){
-                for(int j=0; j<M; j++){
-                    vis[i][j][k]=-1;
-                }
-            }
-        }
-        vis[0][0][0]=1;
+				if(nx==N-1 && ny==M-1) {
+					return visited[x][y][wall]+1;
+				}
+				
+				if(map[nx][ny]=='0') {
+					if(visited[nx][ny][wall]>0) continue;
+					visited[nx][ny][wall] = visited[x][y][wall] + 1;
+					q.offer(new int[] {nx, ny, wall});
+				}else {
+					if(wall==0) {
+						if(visited[nx][ny][wall+1]>0) continue;
+						visited[nx][ny][wall+1] = visited[x][y][wall] + 1;
+						q.offer(new int[] {nx, ny, wall+1});
+					}
+				}
 
-        for(int i=0; i<N; i++){
-            String s = br.readLine();
-            for(int j=0; j<M; j++){
-                board[i][j] = s.charAt(j)-'0';
-            }
-        }
-        bfs();
-    }
+			}
+		}
+		
+		return -1;
+	}
 
-    public static void bfs(){
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0,0,0});
-        while(!q.isEmpty()){
-            int[] cur=q.poll();
-            if(cur[0]==N-1 && cur[1]==M-1){
-                System.out.println(vis[cur[0]][cur[1]][cur[2]]);
-                return;
-            }
-
-            for(int[] dir : d){
-                int nx=cur[0]+dir[0];
-                int ny=cur[1]+dir[1];
-
-                if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
-                if(board[nx][ny]==1 && cur[2]==1) continue;
-
-                int state = board[nx][ny] | cur[2];
-                if(vis[nx][ny][state]!=-1) continue;
-                vis[nx][ny][state] = vis[cur[0]][cur[1]][cur[2]]+1;
-                q.offer(new int[]{nx,ny,state});
-            }
-        }
-        System.out.println(-1);
-    }
 }
