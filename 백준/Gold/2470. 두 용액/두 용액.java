873832a36cea9380d7f8fa90b147d9dcda7bbf8f
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-28 15:45:34 +0900

    [Gold V] Title: 두 용액, Time: 332 ms, Memory: 28688 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,39 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		int s = 0;
-		int e = N-1;
-		int sum = arr[s]+arr[e];
-		int t1 = s;
-		int t2 = e;
-		while(s<e) {
-			if(Math.abs(arr[s]+arr[e])<Math.abs(sum)) {
-				t1 = s;
-				t2 = e;
-				sum = arr[s]+arr[e];
-			}
-			
-			if(arr[s]+arr[e]<0) {
-				s++;
-			}else {
-				e--;
-			}
-		}
-		System.out.println(arr[t1] + " " + arr[t2]);
-	}
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
+        Arrays.sort(arr);
+
+        int ansL=arr[0];
+        int ansR=arr[N-1];
+
+        int s = 0; 
+        int e = N-1;
+        while(s+1<e){
+            if(arr[s]+arr[e]<0){
+                s++;
+            }else if(arr[s]+arr[e]>0){
+                e--;
+            }else{
+                ansL = arr[s];
+                ansR = arr[e];
+                break;
+            }
+            
+            if(Math.abs(ansL+ansR) > Math.abs(arr[s]+arr[e])){
+                ansL = arr[s];
+                ansR = arr[e];
+            }
+        }
+
+        System.out.println(ansL + " " + ansR);
+    }
 }
