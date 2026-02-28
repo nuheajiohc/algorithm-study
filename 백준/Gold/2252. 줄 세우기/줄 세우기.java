71bcb80dd364bfd11b0ecc609766546694386bd4
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-02-28 16:18:32 +0900

    [Gold III] Title: 줄 세우기, Time: 416 ms, Memory: 47172 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,42 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int N, M;
+    static List<Integer>[] adj;
+    static int[] degree;
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        List<Integer>[] adj = new ArrayList[N+1];
-        for(int i=1;i <=N; i++){
-            adj[i] = new ArrayList<>();
-        }
+        adj = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) adj[i] = new ArrayList<>();
 
-        int[] deg = new int[N+1];
+        degree = new int[N+1];
 
         while(M-->0){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             adj[a].add(b);
-            deg[b]++;
+            degree[b]++;
         }
+
+        List<Integer> ans = new ArrayList<>();
 
         Queue<Integer> queue = new ArrayDeque<>();
         for(int i=1; i<=N; i++){
-            if(deg[i]==0) queue.offer(i);
+            if(degree[i]!=0) continue;
+            queue.offer(i);
+            ans.add(i);
         }
 
-        StringBuilder sb= new StringBuilder();
         while(!queue.isEmpty()){
             int cur = queue.poll();
-            if(deg[cur]==0) sb.append(cur).append(" ");
-            for(int next: adj[cur]){
-                deg[next]--;
-                if(deg[next]==0) queue.offer(next);
+            for(int next : adj[cur]){
+                if(--degree[next]!=0) continue;
+                queue.offer(next);
+                ans.add(next);
             }
         }
 
-        System.out.println(sb.toString());
+        StringBuilder sb = new StringBuilder();
+        for(int num : ans){
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
     }
 }
