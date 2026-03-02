fcd073347d0a18676181e0d321290f0f4eb512ba
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-02 17:24:44 +0900

    [Gold IV] Title: Puyo Puyo, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -3,97 +3,84 @@ import java.util.*;
 
 public class Main {
 
-    private static int[] dx = {1,-1,0,0};
-    private static int[] dy = {0,0,1,-1};
-    private static char[][] board = new char[12][6];
-    private static boolean[][] vis;
-    private static int count;
+    static char[][] field = new char[12][6];
+    static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         for(int i=0; i<12; i++){
-            String line = br.readLine();
-            for(int j=0; j<line.length(); j++){
-                board[i][j]= line.charAt(j);
-            }
+            field[i] = br.readLine().toCharArray();
         }
-        int result=0;
-        while(removePuyo()){
-            downPuyo();
-            result++;
+
+        int cnt = 0;
+        while(true){
+            boolean isBomb = bomb();
+            if(!isBomb) break;
+            applyGravity();
+            cnt ++;
         }
-        System.out.println(result);
+
+        System.out.println(cnt);
 
     }
 
-
-    public static void downPuyo(){
-        for(int i=0; i<6; i++){
-            Queue<Character> queue = new ArrayDeque<>();
-            for(int j=11; j>=0; j--){
-                if(board[j][i]!='.') queue.offer(board[j][i]);
-            }
-
-            for(int j=11; j>=0; j--){
-                if(!queue.isEmpty()){
-                    board[j][i]=queue.poll();
-                }else{
-                    board[j][i]='.';
-                }
-            }
-            
-        }
-    }
-
-    public static boolean removePuyo(){
-        vis = new boolean[12][6];
-        boolean canRemove = false;
+    static boolean bomb(){
+        boolean possible = false;
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[12][6];
         for(int i=0; i<12; i++){
             for(int j=0; j<6; j++){
-                if(vis[i][j] || board[i][j]=='.') continue;
-                char curPuyo = board[i][j];
-                count=1;
-                bfs(curPuyo,i,j);
-                if(count>=4) canRemove =true;
-            }
-        }
-        return canRemove;
-    }
+                if(field[i][j]=='.') continue;
+                if(visited[i][j]) continue;
 
-    public static void bfs(int curPuyo, int x, int y){
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{x,y});
-        vis[x][y] = true;
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            for(int dir=0; dir<4; dir++){
-                int nx = cur[0]+dx[dir];
-                int ny = cur[1]+dy[dir];
-                if(isOutOfRange(nx,ny)) continue;
-                if(vis[nx][ny]) continue;
-                if(board[nx][ny]!=curPuyo) continue;
-                vis[nx][ny] = true;
-                queue.offer(new int[]{nx,ny});
-                count++;
-            }
-        }
+                char color = field[i][j];
 
-        if(count>=4){
-            queue.offer(new int[]{x,y});
-            while(!queue.isEmpty()){
-                int[] cur= queue.poll();
-                for(int dir=0; dir<4; dir++){
-                    int nx = cur[0]+dx[dir];
-                int ny = cur[1]+dy[dir];
-                if(isOutOfRange(nx,ny)) continue;
-                if(board[nx][ny]!=curPuyo) continue;
-                queue.offer(new int[]{nx,ny});
-                board[nx][ny]='.';
+                visited[i][j] = true;
+                field[i][j] = '.';
+                List<int[]> cand = new ArrayList<>();
+                cand.add(new int[]{i, j});
+                queue.offer(new int[]{i, j});
+
+                while(!queue.isEmpty()) {
+                    int[] cur = queue.poll();
+                    int x = cur[0]; int y = cur[1];
+
+                    for(int dir=0; dir<4; dir++){
+                        int nx = x + dx[dir];
+                        int ny = y + dy[dir];
+                        if(nx<0 || nx>=12 || ny<0 || ny>=6) continue;
+                        if(field[nx][ny]=='.' || field[nx][ny]!=color) continue;
+                        visited[nx][ny] = true;
+                        cand.add(new int[]{nx, ny});
+                        field[nx][ny]= '.';
+                        queue.offer(new int[]{nx, ny});
+                    }
                 }
+
+                if(cand.size()<4){
+                    for(int[] coord : cand){
+                        int x = coord[0];
+                        int y = coord[1];
+                        field[x][y] = color;
+                    }
+                    continue;
+                }
+                possible = true;
             }
         }
+        return possible;
     }
 
-    public static boolean isOutOfRange(int x, int y){
-        return x<0 || x>=12 || y<0 || y>=6;
+    static void applyGravity(){
+        for(int j=0; j<6; j++){
+            int idx = 12;
+            for(int i=11; i>=0; i--){
+                if(field[i][j]=='.') continue;
+                char existed = field[i][j];
+                field[i][j] = '.';
+                field[--idx][j] = existed;
+            }
+        }
     }
 }
