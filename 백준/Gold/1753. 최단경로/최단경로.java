4de60473c10364925c9e99e19ac0d1e4dd78f2ce
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-03 15:46:57 +0900

    [Gold IV] Title: 최단경로, Time: 656 ms, Memory: 107876 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,69 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	
+	static int V, E, start;
+	static List<int[]>[] adj;
+	static int[] distance;
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		start = Integer.parseInt(br.readLine());
+		
+		distance = new int[V+1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		
+		adj = new ArrayList[V+1];
+		for(int i=1; i<=V; i++) adj[i] = new ArrayList<>();
+		
+		while(E-->0) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			adj[u].add(new int[] {v, w});
+		}
+		
+		distance[start] = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->a[1]-b[1]);
+		pq.offer(new int[] {start, 0});
+		
+		while(!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			int v = cur[0];
+			int w = cur[1];
+			
+			if(distance[v] != w) continue;
+			
+			for(int[] next : adj[v]) {
+				int nv = next[0];
+				int nw = next[1];
+				
+				if(distance[nv] <= distance[v] + nw) continue;
+				distance[nv] = distance[v] + nw;
+				pq.offer(new int[] {nv, distance[nv]});
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int v=1; v<=V; v++) {
+			if(distance[v]==Integer.MAX_VALUE) {
+				sb.append("INF");
+			}else {
+				sb.append(distance[v]);
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
 
-        int start = Integer.parseInt(br.readLine());
-
-        List<Node>[] adj = new ArrayList[V+1];
-        for(int i=0; i<=V; i++){
-            adj[i] = new ArrayList<>();
-        }
-
-        for(int i=0; i<E; i++){
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            adj[u].add(new Node(v, w));
-        }
-
-        int[] d = new int[V+1];
-        Arrays.fill(d, Integer.MAX_VALUE);
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        d[start]=0;
-        pq.add(new Node(start,0));
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
-            if(d[cur.vertex]!= cur.weight) continue;
-
-            for(Node next : adj[cur.vertex]){
-                if(d[next.vertex] <= d[cur.vertex]+next.weight) continue;
-                d[next.vertex]=d[cur.vertex]+next.weight;
-                pq.offer(new Node(next.vertex,d[next.vertex]));
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=V; i++){
-            if(d[i]==Integer.MAX_VALUE){
-                sb.append("INF");                
-            }else{
-                sb.append(d[i]);
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    public static class Node implements Comparable<Node>{
-        int vertex;
-        int weight;
-
-        public Node(int vertex, int weight) {
-            this.vertex = vertex;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return weight-o.weight;
-        }
-    }
 }
