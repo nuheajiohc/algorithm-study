8e97b11e0b3a4acab83c65e6e45c0595a1ba0a94
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-09 11:26:02 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 106 ms, Memory: 28,416 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,51 +1,53 @@
-import java.io.*;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
+	
+	static int N, M;
+	static List<Integer>[] adj;
+	static boolean[] visited;
 
-    static int N, M, cnt;
-    static List<Integer>[] adj;
-    static boolean[] vis;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int TC = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1; tc<=TC; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			visited = new boolean[N+1];
+			adj = new ArrayList[N+1];
+			for(int i=1; i<=N; i++) adj[i] = new ArrayList<>();
+			
+			while(M-->0) {
+				st = new StringTokenizer(br.readLine());
+				int u = Integer.parseInt(st.nextToken());
+				int v = Integer.parseInt(st.nextToken());
+				
+				adj[u].add(v);
+				adj[v].add(u);
+			}
+			
+			int cnt = 0;
+			for(int i=1; i<=N; i++) {
+				if(visited[i]) continue;
+				dfs(i);
+				cnt ++;
+			}
+			sb.append("#").append(tc).append(" ").append(cnt).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static void dfs(int cur) {
+		visited[cur] = true;
+		for(int next : adj[cur]) {
+			if(visited[next]) continue;
+			dfs(next);
+		}
+	}
 
-        int TC = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int tc=1; tc<=TC; tc++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-
-            cnt = 0;
-            vis = new boolean[N+1];
-            adj = new ArrayList[N+1];
-            for(int i=1; i<=N; i++) adj[i] = new ArrayList<>();
-
-            while(M-->0){
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                adj[a].add(b);
-                adj[b].add(a);
-            }
-
-            for(int i=1; i<=N; i++){
-                if(vis[i]) continue;
-                cnt ++;
-                dfs(i);
-            }
-
-            sb.append("#").append(tc).append(" ").append(cnt).append("\n");
-
-        }
-        System.out.println(sb);
-    }
-
-    static void dfs(int idx){
-        vis[idx] = true;
-        for(int next : adj[idx]){
-            if(vis[next]) continue;
-            dfs(next);
-        }
-    }
 }
