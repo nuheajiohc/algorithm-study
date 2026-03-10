cdd1539431df24805794c9994b766d22b200a6b1
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-10 13:32:20 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 420 ms, Memory: 32912 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -3,86 +3,93 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static List<int[]>[] adj;
-	static int[] dist1, dist2, dist3;
-	static PriorityQueue<int[]> pq;
-	static int INF = Integer.MAX_VALUE;
+    static int N, M;
+    static List<int[]>[] adj;
+    static int[] dist1, dist2, dist3;
+    static PriorityQueue<int[]> pq;
+    static int INF = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        st.nextToken();
+        N = (int) st.nval;
 
-		pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        st.nextToken();
+        M = (int) st.nval;
 
-		adj = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++)
-			adj[i] = new ArrayList<>();
+        pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
-		while (M-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++)
+            adj[i] = new ArrayList<>();
 
-			adj[u].add(new int[] { v, e });
-			adj[v].add(new int[] { u, e });
-		}
+        while (M-- > 0) {
+            st.nextToken();
+            int u = (int) st.nval;
 
-		st = new StringTokenizer(br.readLine());
-		int mid1 = Integer.parseInt(st.nextToken());
-		int mid2 = Integer.parseInt(st.nextToken());
+            st.nextToken();
+            int v = (int) st.nval;
 
-		dist1 = new int[N + 1];
-		Arrays.fill(dist1, INF);
-		
-		dijkstra(1, N, dist1);
-		
-		if(dist1[mid1]==INF || dist1[mid2]==INF || dist1[N]==INF) {
-			System.out.println(-1);
-			return;
-		}
-		
-		dist2 = new int[N + 1];
-		Arrays.fill(dist2, INF);
-		dijkstra(mid1, N, dist2);
-		
-		
-		dist3 = new int[N + 1];
-		Arrays.fill(dist3, INF);
-		dijkstra(mid2, N, dist3);
-		
-		int cand1 = dist1[mid1] + dist2[mid2] + dist3[N];
-		int cand2 = dist1[mid2] + dist3[mid1] + dist2[N];
+            st.nextToken();
+            int e = (int) st.nval;
 
-		System.out.println(Math.min(cand1, cand2));
-	}
+            adj[u].add(new int[]{v, e});
+            adj[v].add(new int[]{u, e});
+        }
 
-	static void dijkstra(int start, int end, int[] dist) {
-		pq.clear();
-		pq.offer(new int[] { start, 0 });
-		dist[start] = 0;
+        st.nextToken();
+        int mid1 = (int) st.nval;
 
-		while (!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			int u = cur[0];
-			int e = cur[1];
-			
-			if (dist[u] != e) continue;
-			for (int[] next : adj[u]) {
-				int nu = next[0];
-				int ne = next[1];
-				if (dist[nu] <= dist[u] + ne) continue;
-				dist[nu] = dist[u] + ne;
-				pq.offer(new int[] { nu, dist[nu] });
-			}
-		}
+        st.nextToken();
+        int mid2 = (int) st.nval;
 
-		return;
-	}
+        dist1 = new int[N + 1];
+        Arrays.fill(dist1, INF);
 
+        dijkstra(1, N, dist1);
+
+        if (dist1[mid1] == INF || dist1[mid2] == INF || dist1[N] == INF) {
+            System.out.println(-1);
+            return;
+        }
+
+        dist2 = new int[N + 1];
+        Arrays.fill(dist2, INF);
+        dijkstra(mid1, N, dist2);
+
+        dist3 = new int[N + 1];
+        Arrays.fill(dist3, INF);
+        dijkstra(mid2, N, dist3);
+
+        int cand1 = dist1[mid1] + dist2[mid2] + dist3[N];
+        int cand2 = dist1[mid2] + dist3[mid1] + dist2[N];
+
+        System.out.println(Math.min(cand1, cand2));
+    }
+
+    static void dijkstra(int start, int end, int[] dist) {
+        pq.clear();
+        pq.offer(new int[]{start, 0});
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int u = cur[0];
+            int e = cur[1];
+
+            if (dist[u] != e) continue;
+
+            for (int[] next : adj[u]) {
+                int nu = next[0];
+                int ne = next[1];
+
+                if (dist[nu] <= dist[u] + ne) continue;
+
+                dist[nu] = dist[u] + ne;
+                pq.offer(new int[]{nu, dist[nu]});
+            }
+        }
+    }
 }
