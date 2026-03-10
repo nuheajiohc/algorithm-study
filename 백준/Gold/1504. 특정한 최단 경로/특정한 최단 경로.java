e7baf7e4789dbb9e6ed58f08c7be315ea4f305bb
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-10 10:10:55 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 556 ms, Memory: 68684 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N, M;
+	static List<int[]>[] adj;
+	static int[] dist1, dist2, dist3;
+	static PriorityQueue<int[]> pq;
+	static int INF = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+		adj = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++)
+			adj[i] = new ArrayList<>();
+
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+
+			adj[u].add(new int[] { v, e });
+			adj[v].add(new int[] { u, e });
+		}
+
+		st = new StringTokenizer(br.readLine());
+		int mid1 = Integer.parseInt(st.nextToken());
+		int mid2 = Integer.parseInt(st.nextToken());
+
+		dist1 = new int[N + 1];
+		Arrays.fill(dist1, INF);
+		
+		dijkstra(1, N, dist1);
+		
+		if(dist1[mid1]==INF || dist1[mid2]==INF || dist1[N]==INF) {
+			System.out.println(-1);
+			return;
+		}
+		
+		dist2 = new int[N + 1];
+		Arrays.fill(dist2, INF);
+		dijkstra(mid1, N, dist2);
+		
+		
+		dist3 = new int[N + 1];
+		Arrays.fill(dist3, INF);
+		dijkstra(mid2, N, dist3);
+		
+		int cand1 = dist1[mid1] + dist2[mid2] + dist3[N];
+		int cand2 = dist1[mid2] + dist3[mid1] + dist2[N];
+
+		System.out.println(Math.min(cand1, cand2));
+	}
+
+	static void dijkstra(int start, int end, int[] dist) {
+		pq.clear();
+		pq.offer(new int[] { start, 0 });
+		dist[start] = 0;
+
+		while (!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			int u = cur[0];
+			int e = cur[1];
+			
+			if (dist[u] != e) continue;
+			for (int[] next : adj[u]) {
+				int nu = next[0];
+				int ne = next[1];
+				if (dist[nu] <= dist[u] + ne) continue;
+				dist[nu] = dist[u] + ne;
+				pq.offer(new int[] { nu, dist[nu] });
+			}
+		}
+
+		return;
+	}
+
+}
