b56b439270f0577196919c01300ba6b949520eb6
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-12 11:24:13 +0900

    [Gold IV] Title: 최단경로, Time: 700 ms, Memory: 110324 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -3,9 +3,10 @@ import java.util.*;
 
 public class Main {
 	
-	static int V, E, start;
+	static int V, E;
 	static List<int[]>[] adj;
-	static int[] distance;
+	static int[] dist;
+	static int INF = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,10 +15,7 @@ public class Main {
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		
-		start = Integer.parseInt(br.readLine());
-		
-		distance = new int[V+1];
-		Arrays.fill(distance, Integer.MAX_VALUE);
+		int start = Integer.parseInt(br.readLine());
 		
 		adj = new ArrayList[V+1];
 		for(int i=1; i<=V; i++) adj[i] = new ArrayList<>();
@@ -27,40 +25,45 @@ public class Main {
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
+			
 			adj[u].add(new int[] {v, w});
 		}
 		
-		distance[start] = 0;
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->a[1]-b[1]);
-		pq.offer(new int[] {start, 0});
+		dist = new int[V+1];
+		Arrays.fill(dist, INF);
 		
-		while(!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			int v = cur[0];
-			int w = cur[1];
-			
-			if(distance[v] != w) continue;
-			
-			for(int[] next : adj[v]) {
-				int nv = next[0];
-				int nw = next[1];
-				
-				if(distance[nv] <= distance[v] + nw) continue;
-				distance[nv] = distance[v] + nw;
-				pq.offer(new int[] {nv, distance[nv]});
-			}
-		}
+		dijkstra(start);
 		
 		StringBuilder sb = new StringBuilder();
-		for(int v=1; v<=V; v++) {
-			if(distance[v]==Integer.MAX_VALUE) {
-				sb.append("INF");
-			}else {
-				sb.append(distance[v]);
-			}
-			sb.append("\n");
+		for(int i=1; i<=V; i++) {
+			sb.append(dist[i]==INF ? "INF" : dist[i]).append("\n");
 		}
 		System.out.println(sb);
+	}
+	
+	
+	static void dijkstra(int start) {
+		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[1]));
+		
+		pq.offer(new int[] {start, 0});
+		dist[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			int[] now = pq.poll();
+			int u = now[0];
+			int w = now[1];
+			
+			if(dist[u] != w) continue;
+			
+			for(int[] next : adj[u]) {
+				int nu = next[0];
+				int nw = next[1];
+				
+				if(dist[nu] <= dist[u] + nw) continue;
+				dist[nu] = dist[u] + nw;
+				pq.offer(new int[] {nu, dist[nu]});
+			}
+		}
 	}
 
 }
