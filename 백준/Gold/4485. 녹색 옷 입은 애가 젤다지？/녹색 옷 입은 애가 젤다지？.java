2b6c78c4867e6b842a8ed7fb822626b5e2f9371c
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-12 11:43:08 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 352 ms, Memory: 30152 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -3,64 +3,67 @@ import java.util.*;
 
 public class Main {
 
-    static int[] dx = {0,0, 1, -1}, dy = {1, -1, 0, 0};
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static int[][] map;
+	static int[][] dist;
+	static int[] dx = { 0, 0, 1, -1 }, dy = { 1, -1, 0, 0 };
 
-        StringBuilder sb = new StringBuilder();
-        int tc=1;
-        while(true){
-            int N = Integer.parseInt(br.readLine());
-            if(N == 0) break;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		int tc= 1;
+		while(true) {
+			N = Integer.parseInt(br.readLine());
+			if(N==0) break;
+			map = new int [N][N];
+			dist = new int [N][N];
+			for(int i=0; i<N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int j=0; j<N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			dijkstra();
+			
+			sb.append("Problem ").append(tc).append(": ").append(dist[N-1][N-1]).append("\n");
+			tc++;
+		}
+		System.out.println(sb);
+	}
 
-            int[][] board = new int[N][N];
-            for(int i=0; i<N; i++){
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for(int j=0; j<N; j++){
-                    board[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+	static void dijkstra() {
+		for(int i=0; i<N; i++) {
+			Arrays.fill(dist[i], Integer.MAX_VALUE);
+		}
+		
+		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[1]));
+		dist[0][0] = map[0][0];
+		pq.offer(new int[] {0, 0, dist[0][0]});
+		
+		while(!pq.isEmpty()) {
+			int[] now = pq.poll();
+			int x = now[0];
+			int y = now[1];
+			int w = now[2];
+			
+			if(dist[x][y] != w) continue;
+		
+			
+			for(int dir=0; dir<4; dir++) {
+				int nx = x + dx[dir];
+				int ny = y + dy[dir];
+				
+				if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
+				
+				int nw = map[nx][ny];
+				
+				if(dist[nx][ny] <= dist[x][y]+ nw) continue;
+				dist[nx][ny] = dist[x][y] + nw;
+				pq.offer(new int[] {nx, ny, dist[nx][ny]});
+			}
+		}
+	}
 
-            int[][] d = new int[N][N];
-            for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
-                    d[i][j] = Integer.MAX_VALUE;
-                }
-            }
-            PriorityQueue<Node> pq = new PriorityQueue<>((a,b)->a.e-b.e);
-
-            d[0][0] = board[0][0];
-            pq.add(new Node(0,0,0));
-            while(!pq.isEmpty()){
-                Node cur = pq.poll();
-                if(d[cur.x][cur.y] < cur.e) continue;
-                for(int dir=0; dir<4; dir++){
-                    int nx = cur.x + dx[dir];
-                    int ny = cur.y + dy[dir];
-
-                    if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
-                    if(d[nx][ny] <= d[cur.x][cur.y]+board[nx][ny]) continue;
-                    d[nx][ny] = d[cur.x][cur.y] + board[nx][ny];
-                    pq.offer(new Node(nx, ny, d[nx][ny]));
-                }
-            }
-
-            sb.append("Problem ").append(tc).append(": ").append(d[N-1][N-1]).append("\n");
-            tc++;
-        }
-
-        System.out.println(sb);
-    }
-
-    static class Node{
-        int x;
-        int y;
-        int e;
-
-        Node(int x, int y, int e){
-            this.x = x;
-            this.y = y;
-            this.e = e;
-        }
-    }
 }
