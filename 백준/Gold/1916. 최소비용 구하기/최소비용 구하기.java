afa29502e890e9dc6bb1d71c9a630e57f2e79a39
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-03-13 11:38:38 +0900

    [Gold V] Title: 최소비용 구하기, Time: 408 ms, Memory: 50444 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,74 +3,83 @@ import java.util.*;
 
 public class Main {
 
-    static int N, M;
-    static List<int[]>[] adj;
-    static int[] dist;
+	static int N, M;
+	static List<Edge>[] adj;
+	static int INF = Integer.MAX_VALUE;
 
-    static StreamTokenizer st;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int nextInt() throws Exception {
-        st.nextToken();
-        return (int) st.nval;
-    }
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 
-    public static void main(String[] args) throws Exception {
+		adj = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++)
+			adj[i] = new ArrayList<>();
 
-        st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+		while (M-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			adj[u].add(new Edge(v, w));
+		}
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		
+		System.out.println(dijkstra(start, end));
+	}
+	
+	static int dijkstra(int start, int end) {
+		int[] dist = new int[N+1];
+		Arrays.fill(dist, INF);
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(start, 0));
+		dist[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if(cur.v == end) break;
+			
+			if(cur.dist != dist[cur.v]) continue;
+			
+			for(Edge next : adj[cur.v]) {
+				if(dist[next.to] <= dist[cur.v] + next.weight) continue;
+				dist[next.to] = dist[cur.v] + next.weight;
+				
+				pq.offer(new Node(next.to, dist[next.to]));
+			}
+		}
+		
+		return dist[end];
+	}
 
-        N = nextInt();
-        M = nextInt();
+	static class Edge {
+		int to;
+		int weight;
 
-        adj = new ArrayList[N + 1];
+		Edge(int to, int weight) {
+			this.to = to;
+			this.weight = weight;
+		}
+	}
 
-        for (int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
+	static class Node implements Comparable<Node> {
+		int v;
+		int dist;
 
-        while (M-- > 0) {
-            int u = nextInt();
-            int v = nextInt();
-            int e = nextInt();
-            adj[u].add(new int[]{v, e});
-        }
+		Node(int v, int dist) {
+			this.v = v;
+			this.dist = dist;
+		}
 
-        int start = nextInt();
-        int end = nextInt();
+		public int compareTo(Node o) {
+			return Integer.compare(dist, o.dist);
+		}
+	}
 
-        int minCost = dijkstra(start, end);
-
-        System.out.println(minCost);
-    }
-
-    static int dijkstra(int s, int e) {
-
-        dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        dist[s] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.offer(new int[]{s, 0});
-
-        while (!pq.isEmpty()) {
-
-            int[] cur = pq.poll();
-            int u = cur[0];
-            int w = cur[1];
-
-            if (u == e) break;
-
-            if (dist[u] != w) continue;
-
-            for (int[] next : adj[u]) {
-                int nu = next[0];
-                int nw = next[1];
-
-                if (dist[nu] <= dist[u] + nw) continue;
-
-                dist[nu] = dist[u] + nw;
-                pq.offer(new int[]{nu, dist[nu]});
-            }
-        }
-
-        return dist[e];
-    }
 }
