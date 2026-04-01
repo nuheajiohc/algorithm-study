b7116cafd98c4970cb7d70a304303b9f89312098
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-01 15:04:16 +0900

    Title: N개의 정수 중 최대 2개, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/N개의 정수 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine().trim());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[N-1]+" "+arr[N-2]);
+    }
+}
