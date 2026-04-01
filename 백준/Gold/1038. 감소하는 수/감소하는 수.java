3b5ee42388cc5a442f49d2f1f08289bc0faf8265
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-01 13:51:47 +0900

    [Gold V] Title: 감소하는 수, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -2,29 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	static int N;
+	static List<Long> nums = new ArrayList<>();
 
-    static List<Long> nums = new ArrayList<>();
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        for(int i=0; i<10; i++){
-            recursion(i, i);
-        }
-        int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<10; i++) {
+			recursion(i, i);
+		}
 
-        Collections.sort(nums);
-        if(nums.size()>N){
-            System.out.println(nums.get(N));
-        }else{
-            System.out.println(-1);
-        }
-    }
-
-    static void recursion(int k, long num){
-        nums.add(num);
-
-        for(int i=0; i<k; i++){
-            recursion(i, num*10 + i);
-        }
-    }
+		Collections.sort(nums);
+		
+		if(N >= nums.size()) {
+			System.out.println(-1);
+		}else {
+			System.out.println(nums.get(N));
+		}
+	}
+	
+	static void recursion(int k, long num) {
+		nums.add(num);
+		
+		for(int i=0; i<k; i++) {
+			recursion(i, num*10 + i);
+		}
+	}
 }
