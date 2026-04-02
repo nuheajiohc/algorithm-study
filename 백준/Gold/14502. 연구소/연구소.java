976defafe6622ba8701757379451288a55369c6e
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-02 14:24:49 +0900

    [Gold IV] Title: 연구소, Time: 456 ms, Memory: 77896 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -3,85 +3,104 @@ import java.util.*;
 
 public class Main {
 
-    private static int N,M;
-    private static int[][] board;
-    private static int max;
-    private static int[] dx = {0,0,1,-1};
-    private static int[] dy = {1,-1,0,0};
-    private static List<int[]> virus = new ArrayList<>();
+	static int N, M;
+	static int[][] board;
+	static List<int[]> virus = new ArrayList<>();
+	static List<int[]> blank = new ArrayList<>();
+	static int[][] copiedBoard;
+	static boolean[] used;
+	static int maxSafeZone = 0;
+	static int[] dx = { 0, 0, 1, -1 }, dy = { 1, -1, 0, 0 };
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        board = new int[N][M];
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++){
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j]==2){
-                    virus.add(new int[]{i,j});
-                }
-            }
-        }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        backtracking(0,0);
-        System.out.println(max);
-    }
+		board = new int[N][M];
+		copiedBoard = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				if (board[i][j] == 2) {
+					virus.add(new int[] { i, j });
+				}
 
-    public static void backtracking(int pos, int count){
-        if(count==3){
-            check();
-            return;
-        }
+				if (board[i][j] == 0) {
+					blank.add(new int[] { i, j });
+				}
+			}
+		}
 
-        if(pos==N*M){
-            return;
-        }
+		used = new boolean[blank.size()];
+		combination(0, 0);
+		System.out.println(maxSafeZone);
+	}
 
-        for(int i=pos; i<N*M; i++){
-            int x = i/M;
-            int y = i%M;
-            if(board[x][y]==0){
-                board[x][y]=1;
-                backtracking(i+1, count+1);
-                board[x][y]=0;
-            }
-        }
-    }
+	static void combination(int depth, int k) {
+		if (depth == 3) {
+			int safeZone = bfs();
+			maxSafeZone = Math.max(maxSafeZone, safeZone);
+			return;
+		}
 
-    public static void check(){
-        Deque<int[]> q = new ArrayDeque<>();
-        boolean[][] vis = new boolean[N][M];
-        for(int[] pos : virus){
-            q.offer(pos);
-            vis[pos[0]][pos[1]] = true;
+		for (int i = k; i < blank.size(); i++) {
+			used[i] = true;
+			combination(depth + 1, i + 1);
+			used[i] = false;
+		}
+	}
 
-        }
+	static int bfs() {
+		Queue<int[]> queue = new ArrayDeque<>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				copiedBoard[i][j] = board[i][j];
+			}
+		}
 
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            for(int dir=0; dir<4; dir++){
-                int nx = cur[0]+dx[dir];
-                int ny = cur[1]+dy[dir];
-                if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
-                if(board[nx][ny]!=0) continue;
-                if(vis[nx][ny]) continue;
-                vis[nx][ny]=true;
-                q.offer(new int[]{nx,ny});
-            }
-        }
+		for (int[] v : virus) {
+			queue.offer(v);
+		}
 
-        int count=0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(!vis[i][j] && board[i][j]!=1){
-                    count++;
-                }
-            }
-        }
-        max = Math.max(max,count);
-    }
+		for (int i = 0; i < used.length; i++) {
+			if (used[i]) {
+				int[] p = blank.get(i);
+				copiedBoard[p[0]][p[1]] = 1;
+			}
+		}
+
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			int x = cur[0];
+			int y = cur[1];
+
+			for (int dir = 0; dir < 4; dir++) {
+				int nx = x + dx[dir];
+				int ny = y + dy[dir];
+
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+					continue;
+
+				if (copiedBoard[nx][ny] != 0)
+					continue;
+				copiedBoard[nx][ny] = 2;
+				queue.offer(new int[] { nx, ny });
+			}
+		}
+
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (copiedBoard[i][j] == 0) {
+					cnt++;
+				}
+			}
+		}
+
+		return cnt;
+	}
 }
