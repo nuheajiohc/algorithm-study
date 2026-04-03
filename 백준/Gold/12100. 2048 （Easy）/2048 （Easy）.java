249ba64d8585f0f371bee08a773a46489649490b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-03 14:05:04 +0900

    [Gold I] Title: 2048 (Easy), Time: 444 ms, Memory: 38368 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -3,202 +3,126 @@ import java.util.*;
 
 public class Main {
 
-    private static int N,maxBlock;
-    private static int[][] board;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+	static int N;
+	static int[][] board, copied;
+	static int[] direction = new int[5];
+	static int[] dx = { -1, 0, 1, 0 }; //상좌하우
+	static int[] dy = { 0, -1, 0, 1 };
+	
+	static int maxBlockValue = 0;
 
-        board = new int[N][N];
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        backtracking(0,board);
-        System.out.println(maxBlock);
-    }
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+		copied = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		permutation(0);
+		System.out.println(maxBlockValue);
+	}
 
-    public static void backtracking(int depth, int[][] board){
-        if(depth == 5){
-            maxBlock = Math.max(maxBlock,getMaxValue(board));
-            return;
-        }
-
-        int[][] replica = getReplica(board);
-        moveUp(replica);
-        backtracking(depth+1, replica);
-        
-        replica = getReplica(board);
-        moveDown(replica);
-        backtracking(depth+1, replica);
-
-        replica = getReplica(board);
-        moveLeft(replica);
-        backtracking(depth+1, replica);
-
-        replica = getReplica(board);
-        moveRight(replica);
-        backtracking(depth+1, replica);
-    }
-
-    public static void moveUp(int[][] replica){
-        Deque<Integer> queue1 = new ArrayDeque<>();
-        Deque<Integer> queue2 = new ArrayDeque<>();
-        for(int y=0; y<N; y++){
-            for(int x=0; x<N; x++){
-                if(replica[x][y]==0) continue;
-                queue1.offer(replica[x][y]);
-            }
-
-            while(!queue1.isEmpty()){
-                int a =queue1.poll();
-                if(queue1.isEmpty()){
-                    queue2.offer(a);
-                    break;
-                }
-                int b = queue1.poll();
-                if(a==b){
-                    queue2.offer(a+b);
-                }else{
-                    queue2.offer(a);
-                    queue1.offerFirst(b);
-                }
-            }
-
-            for(int x=0; x<N; x++){
-                if(!queue2.isEmpty()){
-                    replica[x][y] = queue2.poll();
-                }else{
-                    replica[x][y]=0;
-                }
-            }
-        }
-    }
-
-    public static void moveDown(int[][] replica){
-        Deque<Integer> queue1 = new ArrayDeque<>();
-        Deque<Integer> queue2 = new ArrayDeque<>();
-        for(int y=0; y<N; y++){
-            for(int x=N-1; x>=0; x--){
-                if(replica[x][y]==0) continue;
-                queue1.offer(replica[x][y]);
-            }
-
-            while(!queue1.isEmpty()){
-                int a =queue1.poll();
-                if(queue1.isEmpty()){
-                    queue2.offer(a);
-                    break;
-                }
-                int b = queue1.poll();
-                if(a==b){
-                    queue2.offer(a+b);
-                }else{
-                    queue2.offer(a);
-                    queue1.offerFirst(b);
-                }
-            }
-
-            for(int x=N-1; x>=0; x--){
-                if(!queue2.isEmpty()){
-                    replica[x][y] = queue2.poll();
-                }else{
-                    replica[x][y]=0;
-                }
-            }
-        }
-    }
-
-    public static void moveLeft(int[][] replica){
-        Deque<Integer> queue1 = new ArrayDeque<>();
-        Deque<Integer> queue2 = new ArrayDeque<>();
-        for(int x=0; x<N; x++){
-            for(int y=0; y<N; y++){
-                if(replica[x][y]==0) continue;
-                queue1.offer(replica[x][y]);
-            }
-
-            while(!queue1.isEmpty()){
-                int a =queue1.poll();
-                if(queue1.isEmpty()){
-                    queue2.offer(a);
-                    break;
-                }
-                int b = queue1.poll();
-                if(a==b){
-                    queue2.offer(a+b);
-                }else{
-                    queue2.offer(a);
-                    queue1.offerFirst(b);
-                }
-            }
-
-            for(int y=0; y<N; y++){
-                if(!queue2.isEmpty()){
-                    replica[x][y]= queue2.poll();
-                }else{
-                    replica[x][y]=0;
-                }
-            }
-        }
-    }
-
-    public static void moveRight(int[][] replica){
-        Deque<Integer> queue1 = new ArrayDeque<>();
-        Deque<Integer> queue2 = new ArrayDeque<>();
-        for(int x=0; x<N; x++){
-            for(int y=N-1; y>=0; y--){
-                if(replica[x][y]==0) continue;
-                queue1.offer(replica[x][y]);
-            }
-
-            while(!queue1.isEmpty()){
-                int a =queue1.poll();
-                if(queue1.isEmpty()){
-                    queue2.offer(a);
-                    break;
-                }
-                int b = queue1.poll();
-                if(a==b){
-                    queue2.offer(a+b);
-                }else{
-                    queue2.offer(a);
-                    queue1.offerFirst(b);
-                }
-            }
-
-            for(int y=0; y<N; y++){
-                if(!queue2.isEmpty()){
-                    replica[x][y]= queue2.poll();
-                }else{
-                    replica[x][y]=0;
-                }
-            }
-        }
-    }
-
-    public static int getMaxValue(int[][] replica){
-        int maxValue=0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(replica[i][j]>maxValue){
-                    maxValue = replica[i][j];
-                }
-            }
-        }
-        return maxValue;
-    }
-
-    public static int[][] getReplica(int[][] board){
-        int[][] replica = new int[N][N];
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                replica[i][j] = board[i][j];
-            }
-        }
-        return replica;
-    }
+	static void permutation(int depth) {
+		if(depth==5) {
+			int blockValue = moveBlock();
+			maxBlockValue = Math.max(maxBlockValue, blockValue);
+			return;
+		}
+		
+		for(int dir=0; dir<4; dir++) {
+			direction[depth] = dir;
+			permutation(depth+1);
+		}
+	}
+	
+	static int moveBlock() {
+		for(int i=0; i<N; i++) {
+			copied[i] = board[i].clone();
+		}
+		
+		
+		for(int c=0; c<5; c++) {
+			int dir = direction[c];
+			
+			if (dir == 0) { // 상
+	            for (int j = 0; j < N; j++) {
+	                int[] temp = new int[N];
+	                int idx = 0;
+	                for (int i = 0; i < N; i++) {
+	                    if (copied[i][j] == 0) continue;
+	                    if (temp[idx] == 0) {
+	                        temp[idx] = copied[i][j];
+	                    } else if (temp[idx] == copied[i][j]) {
+	                        temp[idx] *= 2;
+	                        idx++;
+	                    } else {
+	                        idx++;
+	                        temp[idx] = copied[i][j];
+	                    }
+	                }
+	                for (int i = 0; i < N; i++) copied[i][j] = temp[i];
+	            }
+	        } else if (dir == 1) { // 좌
+	            for (int i = 0; i < N; i++) {
+	                int[] temp = new int[N];
+	                int idx = 0;
+	                for (int j = 0; j < N; j++) {
+	                    if (copied[i][j] == 0) continue;
+	                    if (temp[idx] == 0) temp[idx] = copied[i][j];
+	                    else if (temp[idx] == copied[i][j]) temp[idx++] *= 2;
+	                    else temp[++idx] = copied[i][j];
+	                }
+	                for (int j = 0; j < N; j++) copied[i][j] = temp[j];
+	            }
+	        } else if (dir == 2) { // 하
+	            for (int j = 0; j < N; j++) {
+	                int[] temp = new int[N];
+	                int idx = N - 1;
+	                for (int i = N - 1; i >= 0; i--) {
+	                    if (copied[i][j] == 0) continue;
+	                    if (temp[idx] == 0) temp[idx] = copied[i][j];
+	                    else if (temp[idx] == copied[i][j]) {
+	                        temp[idx] *= 2;
+	                        idx--;
+	                    } else {
+	                        idx--;
+	                        temp[idx] = copied[i][j];
+	                    }
+	                }
+	                for (int i = 0; i < N; i++) copied[i][j] = temp[i];
+	            }
+	        } else if (dir == 3) { // 우
+	            for (int i = 0; i < N; i++) {
+	                int[] temp = new int[N];
+	                int idx = N - 1;
+	                for (int j = N - 1; j >= 0; j--) {
+	                    if (copied[i][j] == 0) continue;
+	                    if (temp[idx] == 0) temp[idx] = copied[i][j];
+	                    else if (temp[idx] == copied[i][j]) {
+	                        temp[idx] *= 2;
+	                        idx--;
+	                    } else {
+	                        idx--;
+	                        temp[idx] = copied[i][j];
+	                    }
+	                }
+	                for (int j = 0; j < N; j++) copied[i][j] = temp[j];
+	            }
+	        }
+		}
+		
+		int value = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				value = Math.max(value, copied[i][j]);
+			}
+		}
+		return value;
+	}
 }
