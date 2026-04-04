bb46f2991509f25f0fc68412aceed384632e6f7a
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-04 22:16:08 +0900

    [Gold II] Title: 집들이, Time: 200 ms, Memory: 21272 KB -BaekjoonHub

--- 백준/Gold/3050. 집들이/집들이.java
+++ 백준/Gold/3050. 집들이/집들이.java
@@ -4,53 +4,50 @@ import java.util.*;
 public class Main {
 
     static int R, C;
-    static char[][] area;
-    static int[][] sum;
+    static int[][] area;
+    static int maxPerson = 0;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        area = new char[R][C];
+        int[] height = new int[C];
 
         for(int i=0; i<R; i++){
-            area[i] = br.readLine().toCharArray();
-        }
-
-        sum = new int[R][C];
-        for(int j=0; j<C; j++){
-            if(area[0][j]=='.'){
-                sum[0][j] = 1;
-            }
-        }
-
-        for(int i=0; i<C; i++){
-            for(int j=1; j<R; j++){
-                if(area[j][i] == '.'){
-                    sum[j][i] = 1 + sum[j-1][i];
+            String line = br.readLine();
+            for(int j=0; j<C; j++){
+                if(line.charAt(j)=='.'){
+                    height[j]++;
                 }else{
-                    sum[j][i] = 0;
+                    height[j]=0;
                 }
             }
-        }
 
-        int maxPerson = 0;
-        for(int i=0; i<R; i++){
-            for(int j=i; j<R; j++){
-                int width = 0;
-                int height = j-i+1;
-                for(int k=0; k<C; k++){
-                    if(sum[j][k]>=height) {
-                        width += 1;
-                        maxPerson = Math.max(maxPerson, height*2 + width*2 - 1);
-                    }else{
-                        width = 0;
-                    }
-                }
-            }
+            maxPerson = Math.max(maxPerson, cal(height));
         }
+        
         System.out.println(maxPerson);
+    }   
+
+    static int cal(int[] height){
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int person = 0;
+        for(int i=0; i<=C; i++){
+            int currentH = (i==C) ? 0 : height[i];
+
+            while(!stack.isEmpty() && height[stack.peekLast()] > currentH){
+                int hIdx = stack.pollLast();
+                int h = height[hIdx];
+
+                int w = stack.isEmpty() ? i : i - stack.peekLast() - 1;
+
+                person = Math.max(person, w*2 + h*2 -1);
+            }
+            stack.offerLast(i);
+        }
+        return person;
     }
 }
