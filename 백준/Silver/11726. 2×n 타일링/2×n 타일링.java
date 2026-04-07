0be3101890c4c186bc458d2fbff2d97394253a29
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-07 11:28:28 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,18 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[N+1];
+		
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		for(int i=2; i<=N; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%10007;
+		}
+		System.out.println(dp[N]);
+	}
 
-    int[] dp = new int[1001];
-    dp[1] = 1;
-    dp[2] = 2;
-    for(int i=3; i<=N; i++){
-      dp[i] = (dp[i-1] + dp[i-2])%10007;
-    }
-    System.out.println(dp[N]);
-  }
 }
