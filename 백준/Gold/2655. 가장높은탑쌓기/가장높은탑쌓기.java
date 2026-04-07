d384b156bf746c041efd4c6a1e89a00858bac786
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-07 17:22:33 +0900

    [Gold III] Title: 가장높은탑쌓기, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.java
+++ 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.java
@@ -2,73 +2,72 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int[][] tower;
-	static List<Integer>[] adj, reversed;
-	static int[] dp;
-	static int cnt;
-	static StringBuilder sb = new StringBuilder();
+
+	static int N;
+	static Brick[] bricks;
+	static int[] dp, prev;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		tower = new int[N][3];
+
+		N = Integer.parseInt(br.readLine());
+		bricks = new Brick[N];
 		dp = new int[N];
-		for(int i=0; i<N; i++) {
+		prev = new int[N];
+		Arrays.fill(prev, -1);
+		
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<3; j++) {
-				tower[i][j] = Integer.parseInt(st.nextToken());
-			}
+			int area = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			bricks[i] = new Brick(i + 1, area, height, weight);
 		}
-		
-		adj = new ArrayList[N];
-		for(int i=0;i<N; i++) adj[i] = new ArrayList<>();
-		
-		reversed = new ArrayList[N];
-		for(int i=0;i<N; i++) reversed[i] = new ArrayList<>();
-		
-		for(int i=0; i<N; i++) {
-			dp[i] = tower[i][1];
-			for(int j=0; j<N; j++) {
-				if(i==j) continue;
-				if(tower[i][0] > tower[j][0] && tower[i][2] > tower[j][2]) {
-					adj[i].add(j);
-					reversed[j].add(i);
+
+		Arrays.sort(bricks, (a, b) -> Integer.compare(b.area, a.area));
+
+		int maxIdx = 0;
+		for (int i = 0; i < N; i++) {
+			dp[i] = bricks[i].height;
+			for (int j = 0; j < i; j++) {
+				if (bricks[i].weight > bricks[j].weight) continue;
+
+				if(dp[j] + bricks[i].height > dp[i]) {
+					dp[i] = dp[j] + bricks[i].height;
+					prev[i] = j;
 				}
 			}
-		}
-		for(int k=0; k<N; k++) {
-			for(int i=0; i<N; i++) {
-				for(int j : adj[i]) {
-					dp[j] = Math.max(dp[j], dp[i] + tower[j][1]);
-				}
-			}	
-		}
-		
-		int maxValue = 0;
-		int maxIdx = -1;
-		for(int i=0; i<N; i++) {
-			if(maxValue < dp[i]) {
-				maxValue = dp[i];
+			
+			if(dp[maxIdx] < dp[i]) {
 				maxIdx = i;
 			}
 		}
 		
-		dfs(maxIdx);
+		Deque<Integer> stack = new ArrayDeque<>();
+		int cur = maxIdx;
+		while(cur!=-1) {
+			stack.offerLast(bricks[cur].idx);
+			cur = prev[cur];
+		}
 		
-		System.out.println(cnt);
+		StringBuilder sb = new StringBuilder();
+		sb.append(stack.size()).append("\n");
+		while(!stack.isEmpty()) {
+			sb.append(stack.pollFirst()).append("\n");
+		}
 		System.out.println(sb);
+
 	}
-	
-	static void dfs(int v) {
-		sb.append(v+1).append("\n");
-		cnt++;
-		for(int next : reversed[v]) {
-			if(dp[next] == dp[v]-tower[v][1]) {
-				dfs(next);
-			}
+
+	static class Brick {
+		int idx, area, height, weight;
+
+		Brick(int idx, int area, int height, int weight) {
+			this.idx = idx;
+			this.area = area;
+			this.height = height;
+			this.weight = weight;
 		}
 	}
+
 }
