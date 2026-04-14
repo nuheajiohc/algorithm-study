75f7d2f27eb609eae78f262a6f1739cba3067724
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-14 13:53:40 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,055 ms, Memory: 101,600 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,63 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+	
+	static int N, M;
+	static int[][] dist;
+	static int[] record;
 
-    static int N,M;
-    static List<Integer>[] adj;
-    static boolean[] vis;
-    static int[] arr;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=TC; tc++) {
+			N = Integer.parseInt(br.readLine());
+			M = Integer.parseInt(br.readLine());
+			
+			dist = new int[N+1][N+1];
+			for(int i=1; i<=N; i++) {
+				Arrays.fill(dist[i], 501);
+			}
+			
+			while(M-->0) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				dist[a][b] = 1;
+			}
 
-        int TC = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int tc=1; tc<=TC; tc++){
-            N = Integer.parseInt(br.readLine());
-            M = Integer.parseInt(br.readLine());
-
-            adj = new ArrayList[N+1];
-            for(int i=1; i<=N; i++) adj[i] = new ArrayList<>();
-
-            while(M-->0){
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                adj[a].add(b);
-            }
-
-            arr = new int[N];
-            for(int i=1; i<=N; i++){
-                vis = new boolean[N+1];
-                dfs(i);
-
-                int cnt=0;
-                for(int j=1; j<=N; j++){
-                    if(vis[j]) cnt++;
-                }
-                arr[i-1] = cnt;
-            }
-
-            Arrays.sort(arr);
-            int ans = 0;
-            for(int i=0; i<N; i++){
-                int cnt = 0;
-                for(int j=0; j<N;j++){
-                    if(arr[i]>=arr[j]) cnt++;
-                }
-
-                if(cnt==arr[i]+1) ans++;
-            }
-            sb.append("#").append(tc).append(" ").append(ans).append("\n");
-        }
-
-        System.out.println(sb);
-    }
-    
-    static void dfs(int idx){
-        for(int v : adj[idx]){
-            if(vis[v]) continue;
-            vis[v] = true;
-            dfs(v);
-        }
-    }
+			for(int k=1; k<=N; k++) {
+				for(int i=1; i<=N; i++) {
+					for(int j=1; j<=N; j++) {
+						dist[i][j] = Math.min(dist[i][j], dist[i][k]+dist[k][j]);
+					}
+				}
+			}
+			
+			int result = 0;
+			for(int i=1; i<=N; i++) {
+				int cnt = 0;
+				for(int j=1; j<=N; j++) {
+					if(i==j) continue;
+					if(dist[i][j]<501 || dist[j][i]<501) cnt++;
+				}
+				if(cnt==N-1) result++;
+			}
+			
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
