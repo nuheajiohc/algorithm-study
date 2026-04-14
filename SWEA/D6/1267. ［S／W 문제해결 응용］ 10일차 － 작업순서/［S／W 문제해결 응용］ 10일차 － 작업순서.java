800ba27f5f40b8b0cb2d161052df5c512f8036ba
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-14 17:52:13 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 113 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -2,48 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int V, E;
+	static List<Integer>[] adj;
+	static int[] degree;
 
-        StringBuilder sb = new StringBuilder();
-        for(int tc=1; tc<=10; tc++){
-            sb.append("#").append(tc).append(" ");
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int V = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-
-            List<Integer>[] adj = new ArrayList[V+1];
-            for(int i=1; i<=V; i++) adj[i] = new ArrayList<>();
-
-            int[] degree = new int[V+1];
-
-            st = new StringTokenizer(br.readLine());
-            while(st.hasMoreTokens()){
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                adj[a].add(b);
-                degree[b]++;
-            }
-
-            Queue<Integer> queue = new ArrayDeque<>();
-            for(int i=1; i<=V; i++){
-                if(degree[i]==0){
-                    queue.offer(i);
-                    sb.append(i).append(" ");
-                }
-            }
-            while(!queue.isEmpty()){
-                int cur = queue.poll();
-                for(int next : adj[cur]){
-                    degree[next]--;
-                    if(degree[next]!=0) continue;
-                    queue.offer(next);
-                    sb.append(next).append(" ");
-                }
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= 10; tc++) {
+			sb.append("#").append(tc).append(" ");
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			
+			adj = new ArrayList[V+1];
+			for(int i=1; i<=V; i++) adj[i] = new ArrayList<>();
+			
+			degree = new int[V+1];
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<E; i++) {
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				adj[a].add(b);
+				degree[b]++;
+			}
+			
+			Queue<Integer> q = new ArrayDeque<>();
+			for(int i=1; i<=V; i++) {
+				if(degree[i]==0) q.offer(i); 
+			}
+			
+			
+			while(!q.isEmpty()) {
+				int cur = q.poll();
+				sb.append(cur).append(" ");
+				for(int next : adj[cur]) {
+					if(--degree[next]>0) continue;
+					q.offer(next);
+				}
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
