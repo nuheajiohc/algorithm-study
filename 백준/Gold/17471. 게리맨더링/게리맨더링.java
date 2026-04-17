94b6a2a6dc7f7c97bdfe1709f838dfb5c0c7f1fd
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-17 15:54:18 +0900

    [Gold III] Title: 게리맨더링, Time: 116 ms, Memory: 15124 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -2,21 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	
 	static int N;
 	static List<Integer>[] adj;
-	static int[] person;
 	static boolean[] used;
-	static int minDiff;
+	static int[] person;
+	static int[] parent;
+	static int minDiff = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		N = Integer.parseInt(br.readLine());
-		
-		minDiff = Integer.MAX_VALUE;
+	
 		used= new boolean[N+1];
 		person = new int[N+1];
+		parent = new int[N+1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++) {
 			person[i] = Integer.parseInt(st.nextToken());
@@ -31,62 +32,46 @@ public class Main {
 			while(st.hasMoreTokens()) {
 				int conn = Integer.parseInt(st.nextToken());
 				adj[i].add(conn);
-				adj[conn].add(i);
 			}
 		}
 		
 		subset(1);
-		
 		System.out.println(minDiff==Integer.MAX_VALUE ? -1 : minDiff);
 	}
 	
-	static int bfs(boolean area) {
-		boolean[] visited = new boolean[N+1];
-		Queue<Integer> q = new ArrayDeque<>();
+	static int calParent(){
+		Arrays.fill(parent, -1);
+		
 		for(int i=1; i<=N; i++) {
-			if(used[i]==area) {
-				q.offer(i);
-				visited[i] = true;
-				break;
+			for(int next : adj[i]) {
+				if(used[i] == used[next]) {
+					union(i, next);
+				}
 			}
 		}
 		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			
-			for(int next : adj[cur]) {
-				if(visited[next]) continue;
-				if(used[next]!=area) continue;
-				visited[next] = true;
-				q.offer(next);
+		int rootCount = 0;
+		for(int i=1; i<=N; i++) {
+			if(parent[i]<0) {
+				rootCount++;
 			}
 		}
-		
-		int cnt = 0;
-		for(int i=1; i<=N; i++) {
-			if(visited[i]) cnt++; 
-		}
-		return cnt;
-	}
-	
-	static int calDiff() {
-		int diff = 0;
-		for(int i=1; i<=N; i++) {
-			if(used[i]) {
-				diff+=person[i];
-			}else {
-				diff-=person[i];
-			}
-		}
-		return Math.abs(diff);
+		return rootCount;
 	}
 	
 	static void subset(int depth) {
 		if(depth==N+1) {
-			int A = bfs(true);
-			int B = bfs(false);
-			if(A+B == N) {
-				minDiff = Math.min(minDiff, calDiff());
+			int parentCount = calParent();
+			if(parentCount==2) {
+				int aSum = 0;
+				int bSum = 0;
+				
+				for(int i=1; i<=N; i++) {
+					if(used[i]) aSum +=person[i];
+					else bSum += person[i];
+				}
+				
+				minDiff = Math.min(minDiff, Math.abs(aSum - bSum));
 			}
 			return;
 		}
@@ -96,6 +81,33 @@ public class Main {
 		
 		used[depth] = false;
 		subset(depth+1);
+	}
+
+	
+	static int find(int u) {
+		if(parent[u]<0) return u;
+		
+		return parent[u] = find(parent[u]);
+	}
+	
+	static boolean union(int u, int v) {
+		u = find(u);
+		v = find(v);
+		
+		if(u==v) {
+			return false;
+		}
+		
+		if(parent[u] > parent[v]) {
+			int temp = u;
+			u = v;
+			v = temp;
+		}
+		
+		parent[u] += parent[v];
+		parent[v] = u;
+		
+		return true;
 	}
 
 }
