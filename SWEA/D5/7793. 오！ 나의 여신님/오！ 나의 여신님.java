c1856da88d145c783a96b601252af81eb1588aba
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-04-21 17:50:50 +0900

    [D5] Title: 오! 나의 여신님, Time: 85 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
+++ SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
@@ -4,95 +4,103 @@ import java.util.*;
 public class Solution {
 
 	static int N, M;
-	static char[][] board;
-	static int[][] visited;
-	static int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
-	
+	static int[] dx = { 0, 0, 1, -1 }, dy = { 1, -1, 0, 0 };
+	static char[][] map;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int TC = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		for(int tc=1; tc<=TC; tc++) {
+
+		for (int tc = 1; tc <= TC; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			
-			int sx = 0;
-			int sy = 0;
-			Queue<int[]> queue = new ArrayDeque<>();
-			board = new char[N][M];
-			visited = new int[N][M];
-			
-			for(int i=0; i<N; i++) Arrays.fill(visited[i], -1);
-			
-			for(int i=0; i<N; i++) {
+
+			map = new char[N][M];
+
+			Queue<int[]> devilQ = new ArrayDeque<>();
+			Queue<int[]> syQ = new ArrayDeque<>();
+
+			for (int i = 0; i < N; i++) {
 				String line = br.readLine();
-				
-				for(int j=0; j<M; j++) {
-					board[i][j] = line.charAt(j);
-					
-					if(board[i][j]=='S') {
-						sx = i;
-						sy = j;
-					}
-					if(board[i][j] == '*') {
-						queue.offer(new int[] {i, j});
-						visited[i][j] = 0;
+				for (int j = 0; j < M; j++) {
+					map[i][j] = line.charAt(j);
+
+					if (map[i][j] == '*') {
+						devilQ.offer(new int[] { i, j });
+					} else if (map[i][j] == 'S') {
+						syQ.offer(new int[] { i, j, 0 });
 					}
 				}
 			}
-			
-			devilMove(queue);
-			queue.offer(new int[] {sx, sy});
-			visited[sx][sy] = 0;
-			int answer = suMove(queue); 
-			
-			sb.append("#").append(tc).append(" ").append(answer==-1 ? "GAME OVER" : answer).append("\n");
+
+			int result = bfs(devilQ, syQ);
+
+			sb.append("#").append(tc).append(" ");
+			if (result == -1) {
+				sb.append("GAME OVER\n");
+			} else {
+				sb.append(result).append("\n");
+			}
 		}
+
 		System.out.println(sb);
 	}
-	
-	static void devilMove(Queue<int[]> q) {
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int x = cur[0];
-			int y = cur[1];
-			
-			for(int dir=0; dir<4; dir++) {
-				int nx = x + dx[dir];
-				int ny = y + dy[dir];
-				
-				if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
-				if(board[nx][ny]=='X' || board[nx][ny]=='D') continue;
-				if(visited[nx][ny]==-1) {
-					visited[nx][ny] = visited[x][y] + 1;
-					q.offer(new int[] {nx, ny});
+
+	private static int bfs(Queue<int[]> devilQ, Queue<int[]> syQ) {
+
+		while (!syQ.isEmpty()) {
+			int devilSize = devilQ.size();
+			while (devilSize-- > 0) {
+				int[] cur = devilQ.poll();
+				int x = cur[0];
+				int y = cur[1];
+
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = x + dx[dir];
+					int ny = y + dy[dir];
+
+					if (isOutOfRange(nx, ny))
+						continue;
+					if (map[nx][ny] == 'D' || map[nx][ny] == 'X' || map[nx][ny] == '*')
+						continue;
+					map[nx][ny] = '*';
+					devilQ.offer(new int[] { nx, ny });
 				}
 			}
-		}
-	}
-	
-	static int suMove(Queue<int[]> q) {
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int x = cur[0];
-			int y = cur[1];
-			
-			for(int dir=0; dir<4; dir++) {
-				int nx = x + dx[dir];
-				int ny = y + dy[dir];
-				
-				if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
-				if(board[nx][ny]=='X') continue;
-				if(board[nx][ny]=='D') return visited[x][y] + 1;
-				
-				if(visited[nx][ny]==-1 || visited[nx][ny] > visited[x][y] +1) {
-					visited[nx][ny] = visited[x][y] + 1;
-					q.offer(new int[] {nx, ny});
+
+			int sySize = syQ.size();
+			while (sySize-- > 0) {
+				int[] cur = syQ.poll();
+				int x = cur[0];
+				int y = cur[1];
+				int t = cur[2];
+
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = x + dx[dir];
+					int ny = y + dy[dir];
+
+					if (isOutOfRange(nx, ny))
+						continue;
+					if (map[nx][ny] == 'X' || map[nx][ny] == '*' || map[nx][ny] == 'S')
+						continue;
+					
+					if (map[nx][ny] == 'D') {
+						return t + 1;
+					}
+					
+					map[nx][ny] = 'S';
+					syQ.offer(new int[] { nx, ny, t + 1 });
 				}
 			}
 		}
 		return -1;
 	}
+
+	static boolean isOutOfRange(int x, int y) {
+		return x < 0 || x >= N || y < 0 || y >= M;
+	}
+
 }
