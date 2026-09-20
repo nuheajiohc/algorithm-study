bfe496cc1235a4131f817d67a4b6ae130af88e10
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2026-09-20 14:29:57 +0900

    [Passed] 변수 선언하기 (86ms, 9MB)

---
+++ 260920/변수 선언하기/declaring-variables.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        char b = 'C';
+        System.out.println(a+"\n"+b);
+    }
+}
